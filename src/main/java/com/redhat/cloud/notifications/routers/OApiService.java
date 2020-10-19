05aec826e9e6ce9b079cd07a1d6ee380914faf13
Heiko W. Rupp <hwr@pilhuhn.de>
2020-10-19 21:54:40 +0200

    Rewrite

--- src/main/java/com/redhat/cloud/notifications/routers/OApiService.java
+++ src/main/java/com/redhat/cloud/notifications/routers/OApiService.java
@@ -1,16 +1,14 @@
 package com.redhat.cloud.notifications.routers;
 
+import io.smallrye.mutiny.Uni;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
 
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
@@ -23,20 +21,22 @@ public class OApiService {
     @GET
     @Path("/{what}/v1.0/openapi.json")
     @Produces(MediaType.APPLICATION_JSON)
-    public StreamingOutput serveOpenAPI(@PathParam("what") String what) {
+    public Uni<String> serveOpenAPI(@PathParam("what") String what) {
 
         String resourceName = "/openapi." + what + ".json";
-        URL target = getClass().getResource(resourceName);
-        if (target == null) {
-            throw new WebApplicationException(resourceName, NOT_FOUND);
-        }
 
-        return new StreamingOutput() {
-            @Override
-            public void write(OutputStream output) throws IOException, WebApplicationException {
-                InputStream is = getClass().getResourceAsStream(resourceName);
-                is.transferTo(output);
-            }
-        };
+        return Uni.createFrom().item(resourceName)
+                .onItem().transform(n ->
+                        getClass().getResourceAsStream(n)
+                )
+                .onItem().ifNull().failWith(new WebApplicationException(resourceName, NOT_FOUND))
+                .onItem().transform(j -> {
+                    try {
+                        return j.readAllBytes();
+                    } catch (IOException e) {
+                        return new byte[1];
+                    }
+                })
+                .onItem().transform(String::new);
     }
 }
