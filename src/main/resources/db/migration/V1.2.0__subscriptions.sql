400653c57bff42d1feca3d4cc8ca4f1859763918
Josejulio Martínez <jmartine@redhat.com>
2020-11-11 12:04:18 -0600

    Update migration, table already existed, only updating the name.

--- src/main/resources/db/migration/V1.2.0__subscriptions.sql
+++ src/main/resources/db/migration/V1.2.0__subscriptions.sql
@@ -1,9 +1,3 @@
 -- Table: public.email_subscribers
 
-CREATE TABLE public.endpoint_email_subscriptions
-(
-    account_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    user_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    subscription_type character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT application_event_type_pkey PRIMARY KEY (account_id, user_id, subscription_type)
-)
+ALTER TABLE public.endpoint_email_subscriptions RENAME COLUMN event_type TO subscription_type;
