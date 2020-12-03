39f9c408c075ffb46fee46a0c3dfcf5d3da3f1f5
Josejulio Martínez <jmartine@redhat.com>
2020-12-03 10:33:01 -0600

    NOTIF-71 Silence db warning about "Duplicate key value for email subs… (#77)
    * NOTIF-71 Silence db warning about "Duplicate key value for email subscription"
    
    * Fix lint error

--- src/main/java/com/redhat/cloud/notifications/db/EndpointEmailSubscriptionResources.java
+++ src/main/java/com/redhat/cloud/notifications/db/EndpointEmailSubscriptionResources.java
@@ -4,7 +4,6 @@ import com.redhat.cloud.notifications.models.EmailSubscription;
 import com.redhat.cloud.notifications.models.EmailSubscription.EmailSubscriptionType;
 import io.r2dbc.postgresql.api.PostgresqlConnection;
 import io.r2dbc.postgresql.api.PostgresqlResult;
-import io.r2dbc.spi.R2dbcDataIntegrityViolationException;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import reactor.core.publisher.Flux;
@@ -20,7 +19,8 @@ public class EndpointEmailSubscriptionResources extends DatasourceProvider {
     Provider<Uni<PostgresqlConnection>> connectionPublisherUni;
 
     public Uni<Boolean> subscribe(String accountNumber, String username, EmailSubscriptionType type) {
-        String query = "INSERT INTO public.endpoint_email_subscriptions(account_id, user_id, subscription_type) VALUES($1, $2, $3)";
+        String query = "INSERT INTO public.endpoint_email_subscriptions(account_id, user_id, subscription_type) VALUES($1, $2, $3) " +
+                "ON CONFLICT (account_id, user_id, subscription_type) DO NOTHING"; // The value is already on the database, this is OK
         return connectionPublisherUni.get().onItem()
                 .transformToMulti(c -> Multi.createFrom().resource(() -> c,
                         c2 -> {
@@ -30,13 +30,11 @@ public class EndpointEmailSubscriptionResources extends DatasourceProvider {
                                     .bind("$3", type.toString())
                                     .execute();
                             return execute.flatMap(PostgresqlResult::getRowsUpdated)
-                                    .map(i -> i > 0).next();
+                                    .map(i -> true).next();
                         })
                         .withFinalizer(postgresqlConnection -> {
                             postgresqlConnection.close().subscribe();
                         })
-                        // The value is already on the database, this is OK
-                        .onFailure(R2dbcDataIntegrityViolationException.class).recoverWithItem(true)
                 ).toUni();
     }
 
