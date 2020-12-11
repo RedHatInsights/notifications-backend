e75110d2fdc120a7bd10af25a9c1d0d06c9aa618
Heiko W. Rupp <hwr@pilhuhn.de>
2020-12-11 09:45:47 +0100

    Add a test for (not) leaking db-connections.

--- src/test/java/com/redhat/cloud/notifications/routers/HealthCheckTest.java
+++ src/test/java/com/redhat/cloud/notifications/routers/HealthCheckTest.java
@@ -1,12 +1,13 @@
 package com.redhat.cloud.notifications.routers;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
 import static io.restassured.RestAssured.with;
 import static org.hamcrest.Matchers.in;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test Health checks and admin-down from admin-interface
@@ -16,13 +17,18 @@ public class HealthCheckTest {
 
     @Test
     void testNormalHealth() {
+        normalHealthCheck();
+    }
+
+    private String normalHealthCheck() {
         String body =
                 when()
                         .get("/health")
                         .then()
                         .statusCode(in(new Integer[]{200, 503})) // may be 503 as there is no Kafka we can talk to
                         .extract().asString();
-        Assertions.assertFalse(body.contains("admin-down"));
+        assertFalse(body.contains("admin-down"));
+        return body;
     }
 
     @Test
@@ -44,7 +50,7 @@ public class HealthCheckTest {
                             .then()
                             .statusCode(503)
                             .extract().asString();
-            Assertions.assertTrue(body.contains("admin-down"));
+            assertTrue(body.contains("admin-down"));
         } finally {
             with()
                     .queryParam("status", "ok")
@@ -54,6 +60,15 @@ public class HealthCheckTest {
                     .post("/admin/status")
                     .then()
                     .statusCode(200);
+        }
+    }
+
+    // Make sure we don't leak connections here
+    @Test
+    void testRepeatedHealth() {
+        for (int i = 0; i < 150; i++) {
+            String body = normalHealthCheck();
+            assertTrue(body.contains("\"reactive-db-check\": true"));
         }
     }
 }
