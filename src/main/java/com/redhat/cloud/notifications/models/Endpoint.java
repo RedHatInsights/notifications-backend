f5edb5397d81b4488fb3b1a8afd66228a8615bf0
Josejulio Martínez <jmartine@redhat.com>
2021-01-07 08:53:02 -0600

    Add comment on top of EndpointType enum explaining that the order must remain constant

--- src/main/java/com/redhat/cloud/notifications/models/Endpoint.java
+++ src/main/java/com/redhat/cloud/notifications/models/Endpoint.java
@@ -15,6 +15,9 @@ import java.util.UUID;
 
 public class Endpoint {
 
+    // Add new values to the bottom of the enum
+    // The ordinal order must remain the same as is used internally
+    // Update test com.redhat.cloud.notifications.models.TestEndpointType to reflect any new enum value
     @Schema(enumeration = { "webhook", "email_subscription", "default" })
     public enum EndpointType {
         @JsonProperty("webhook")
