c965e576a09b6843b479b0d5f9c6592d3595a3a5
Josejulio Martínez <jmartine@redhat.com>
2021-01-08 15:26:03 -0600

    Fix linting

--- src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
+++ src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
@@ -29,7 +29,6 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
