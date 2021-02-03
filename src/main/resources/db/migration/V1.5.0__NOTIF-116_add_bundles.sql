5ca31a0845e756e67e8785af2a5067268d5e2597
Heiko W. Rupp <hwr@pilhuhn.de>
2021-02-03 21:06:45 +0100

    Migrate existing 'Policies' application over

--- src/main/resources/db/migration/V1.5.0__NOTIF-116_add_bundles.sql
+++ src/main/resources/db/migration/V1.5.0__NOTIF-116_add_bundles.sql
@@ -5,11 +5,22 @@ create table bundles (id uuid primary key DEFAULT public.gen_random_uuid() NOT N
     updated timestamp with time zone
 );
 
-alter table applications add column bundle_id uuid NOT NULL;
+insert into bundles (name, display_name) VALUES ('insights', 'Insights');
+
+-- we can not set this to NOT NULL here, as we first need to link the bundle
+-- to the existing 'Policies' app (see v1.1.1)
+ALTER TABLE applications add column bundle_id uuid;
+
+update applications set bundle_id = (
+select id from bundles where name = 'insights'
+);
+
 
 alter table applications add constraint app_bundle_fk
   foreign key (bundle_id) references bundles(id)
   on delete cascade;
+
+
 
 create unique index app_bundle_idx ON public.applications(name, bundle_id);
 
