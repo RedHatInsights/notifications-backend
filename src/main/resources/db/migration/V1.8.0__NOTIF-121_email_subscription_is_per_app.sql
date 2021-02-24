8c9cd4feda3bfbf826d20f5ba42421f1f4e3f13e
Josejulio Martínez <jmartine@redhat.com>
2021-02-24 10:21:55 -0600

    Add ON UPDATE/DELETE CASCADE to previous defined FK

--- src/main/resources/db/migration/V1.8.0__NOTIF-121_email_subscription_is_per_app.sql
+++ src/main/resources/db/migration/V1.8.0__NOTIF-121_email_subscription_is_per_app.sql
@@ -13,3 +13,12 @@ ALTER TABLE ONLY public.endpoint_email_subscriptions DROP CONSTRAINT endpoint_em
 -- Add new PKEY
 ALTER TABLE ONLY public.endpoint_email_subscriptions
   ADD CONSTRAINT endpoint_email_subscriptions_pkey PRIMARY KEY (account_id, user_id, subscription_type, application, bundle);
+
+
+-- Add cascade to missing FK
+alter table endpoint_targets drop CONSTRAINT "FK_event_type_id_target";
+alter table endpoint_targets add CONSTRAINT "FK_event_type_id_target"
+  foreign key (event_type_id)
+  references event_type(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
