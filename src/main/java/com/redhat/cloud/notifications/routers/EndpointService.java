4091fdc9f8a2e2218003c2e7473167be13ee658c
Josejulio Martínez <jmartine@redhat.com>
2021-03-09 10:32:41 -0600

    NOTIF-142 openapi fixes - Add description to 204 contents - Remove some types that were strange (JsonObject as array) - Remove internal types

--- src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
+++ src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
@@ -135,7 +135,7 @@ public class EndpointService {
     @DELETE
     @Path("/{id}")
     @RolesAllowed(RbacIdentityProvider.RBAC_WRITE_INTEGRATIONS_ENDPOINTS)
-    @APIResponse(responseCode = "204", content = @Content(schema = @Schema(type = SchemaType.STRING)))
+    @APIResponse(responseCode = "204", description = "No Content", content = @Content(schema = @Schema(type = SchemaType.STRING)))
     public Uni<Response> deleteEndpoint(@Context SecurityContext sec, @PathParam("id") UUID id) {
         RhIdPrincipal principal = (RhIdPrincipal) sec.getUserPrincipal();
         return resources.deleteEndpoint(principal.getAccount(), id)
@@ -156,7 +156,7 @@ public class EndpointService {
     @DELETE
     @Path("/{id}/enable")
     @RolesAllowed(RbacIdentityProvider.RBAC_WRITE_INTEGRATIONS_ENDPOINTS)
-    @APIResponse(responseCode = "204", content = @Content(schema = @Schema(type = SchemaType.STRING)))
+    @APIResponse(responseCode = "204", description = "No Content", content = @Content(schema = @Schema(type = SchemaType.STRING)))
     public Uni<Response> disableEndpoint(@Context SecurityContext sec, @PathParam("id") UUID id) {
         RhIdPrincipal principal = (RhIdPrincipal) sec.getUserPrincipal();
         return resources.disableEndpoint(principal.getAccount(), id)
