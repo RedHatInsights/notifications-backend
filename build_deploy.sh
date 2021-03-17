4513da97584698a734a26e462b7a52632db1a981
Yuval Lahav <51315998+lahavyuv86@users.noreply.github.com>
2021-03-17 14:50:22 +0200

    add latest tag to build_deploy.sh (#138)

--- build_deploy.sh
+++ build_deploy.sh
@@ -24,3 +24,5 @@ docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f src/main/do
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
 docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
 docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
