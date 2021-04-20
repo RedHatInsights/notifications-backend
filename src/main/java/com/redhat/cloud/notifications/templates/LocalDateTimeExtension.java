d182305b8d7f79e8253efb3fc58b517d800e9847
Josejulio Martínez <jmartine@redhat.com>
2021-04-20 10:01:44 -0500

    Introduces timeAgo() function on templates. (#175)
    * Introduces timeAgo() function on templates.
     - Transforms dates(or date-strings) to a "1 year ago" format
     - Fixes on Advisor templates
    
    * Update TimeAgoFormatter based on insights
    
    https://github.com/RedHatInsights/frontend-components/blob/86b02e3c206663d640d30b32087061c04db1595d/packages/components/src/DateFormat/helper.js#L4-L19

--- src/main/java/com/redhat/cloud/notifications/templates/LocalDateTimeExtension.java
+++ src/main/java/com/redhat/cloud/notifications/templates/LocalDateTimeExtension.java
@@ -1,8 +1,10 @@
 package com.redhat.cloud.notifications.templates;
 
+import com.redhat.cloud.notifications.utils.TimeAgoFormatter;
 import io.quarkus.qute.TemplateExtension;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -11,6 +13,7 @@ public class LocalDateTimeExtension {
 
     private static final DateTimeFormatter utcDateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm 'UTC'").withLocale(Locale.US);
     private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy").withLocale(Locale.US);
+    private static final TimeAgoFormatter timeAgoFormatter = new TimeAgoFormatter();
 
     public static String toUtcFormat(LocalDateTime date) {
         return date.format(utcDateTimeFormatter);
@@ -26,6 +29,14 @@ public class LocalDateTimeExtension {
 
     public static String toStringFormat(String date) {
         return toStringFormat(fromIsoLocalDateTime(date));
+    }
+
+    public static String toTimeAgo(LocalDateTime date) {
+        return timeAgoFormatter.format(LocalDateTime.now(ZoneOffset.UTC), date);
+    }
+
+    public static String toTimeAgo(String date) {
+        return toTimeAgo(fromIsoLocalDateTime(date));
     }
 
     public static LocalDateTime fromIsoLocalDateTime(String date) {
