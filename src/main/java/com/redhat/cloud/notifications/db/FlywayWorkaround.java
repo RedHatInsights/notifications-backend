a7c08aaf4afd659d9eac4196dc6541af79fe47f7
Gwenneg Lepage <gwenneg@gmail.com>
2021-04-26 16:58:25 +0200

    Disable the flyway_schema_history repair on startup

--- src/main/java/com/redhat/cloud/notifications/db/FlywayWorkaround.java
+++ src/main/java/com/redhat/cloud/notifications/db/FlywayWorkaround.java
@@ -29,7 +29,6 @@ public class FlywayWorkaround {
     public void runFlywayMigration(@Observes StartupEvent event) {
         LOGGER.warn("Starting Flyway workaround... remove it ASAP!");
         Flyway flyway = Flyway.configure().dataSource("jdbc:" + datasourceUrl, datasourceUsername, datasourcePassword).load();
-        flyway.repair();
         flyway.migrate();
     }
 }
