6dc939d5310a4b0647e28bfe9d46462164d93363
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-06-10 11:44:20 +0200

    NOTIF-255 Return 400 when request body JSON parsing fails (#302)

---
+++ src/test/java/com/redhat/cloud/notifications/routers/JsonParseExceptionMapperTest.java
@@ -0,0 +1,25 @@
+package com.redhat.cloud.notifications.routers;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+public class JsonParseExceptionMapperTest {
+
+    @Test
+    public void testCreateInvalidEventType() {
+        String responseBody = given()
+                .contentType(JSON)
+                .body("I should be a valid event type JSON!")
+                .when()
+                .post("/internal/eventTypes")
+                .then()
+                .statusCode(400)
+                .extract().body().asString();
+        assertTrue(responseBody.contains("Unrecognized token 'I'"));
+    }
+}
