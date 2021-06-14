fd3866ae81088acffd70bcb4326e2c3dee2f9fb9
Bennet Schulz <mail@bennet-schulz.de>
2021-06-14 14:14:28 +0200

    Fix checkstyle issues

--- src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
+++ src/main/java/com/redhat/cloud/notifications/routers/EndpointService.java
@@ -206,19 +206,19 @@ public class EndpointService {
     @Path("/{id}/history")
     @Produces(APPLICATION_JSON)
     @Parameters({
-            @Parameter(
+        @Parameter(
                     name = "limit",
                     in = ParameterIn.QUERY,
                     description = "Number of items per page, if not specified or 0 is used, returns all elements",
                     schema = @Schema(type = SchemaType.INTEGER)
             ),
-            @Parameter(
+        @Parameter(
                     name = "pageNumber",
                     in = ParameterIn.QUERY,
                     description = "Page number. Starts at first page (0), if not specified starts at first page.",
                     schema = @Schema(type = SchemaType.INTEGER)
             ),
-            @Parameter(
+        @Parameter(
                     name = "includeDetail",
                     description = "Include the detail in the reply",
                     schema = @Schema(type = SchemaType.BOOLEAN)
