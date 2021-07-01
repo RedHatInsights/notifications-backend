a98f20a206ffb6e675cb029d61332786e60e45c6
Josejulio Martínez <jmartine@redhat.com>
2021-07-01 14:19:10 -0500

    NOTIF-153 Adds options to email endpoints  - Send to admins  - Send to RBAC group  - Force-send (skip preferences)

--- backend/src/main/java/com/redhat/cloud/notifications/models/EmailSubscriptionProperties.java
+++ backend/src/main/java/com/redhat/cloud/notifications/models/EmailSubscriptionProperties.java
@@ -1,6 +1,37 @@
 package com.redhat.cloud.notifications.models;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class EmailSubscriptionProperties extends EndpointProperties {
+
+    private Boolean onlyAdmins;
+    private Boolean ignorePreferences;
+    private UUID groupId;
+
+    public Boolean getOnlyAdmins() {
+        return onlyAdmins;
+    }
+
+    public void setOnlyAdmins(Boolean onlyAdmins) {
+        this.onlyAdmins = onlyAdmins;
+    }
+
+    public Boolean getIgnorePreferences() {
+        return ignorePreferences;
+    }
+
+    public void setIgnorePreferences(Boolean ignorePreferences) {
+        this.ignorePreferences = ignorePreferences;
+    }
+
+    public UUID getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(UUID groupId) {
+        this.groupId = groupId;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -12,11 +43,14 @@ public class EmailSubscriptionProperties extends EndpointProperties {
             return false;
         }
 
-        return true;
+        EmailSubscriptionProperties props = (EmailSubscriptionProperties) o;
+
+        return Objects.equals(onlyAdmins, props.onlyAdmins) && Objects.equals(ignorePreferences, props.ignorePreferences)
+                && Objects.equals(groupId, props.groupId);
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(onlyAdmins, ignorePreferences, groupId);
     }
 }
