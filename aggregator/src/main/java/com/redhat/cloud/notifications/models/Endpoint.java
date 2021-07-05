fabcd5e62af3bb2569deec399747f5f9ed46f606
Bennet Schulz <mail@bennet-schulz.de>
2021-07-05 09:37:12 +0200

    Remove WebhookProperties

--- aggregator/src/main/java/com/redhat/cloud/notifications/models/Endpoint.java
+++ aggregator/src/main/java/com/redhat/cloud/notifications/models/Endpoint.java
@@ -2,11 +2,8 @@ package com.redhat.cloud.notifications.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.redhat.cloud.notifications.db.converters.EndpointTypeConverter;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -14,8 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Objects;
@@ -52,19 +47,6 @@ public class Endpoint extends CreationUpdateTimestamped {
     @Column(name = "endpoint_type")
     @Convert(converter = EndpointTypeConverter.class)
     private EndpointType type;
-
-    @Schema(oneOf = { WebhookProperties.class, EmailSubscriptionProperties.class })
-    @JsonTypeInfo(
-            use = JsonTypeInfo.Id.NAME,
-            property = "type",
-            include = JsonTypeInfo.As.EXTERNAL_PROPERTY)
-    @JsonSubTypes({
-        @JsonSubTypes.Type(value = WebhookProperties.class, name = "webhook"),
-        @JsonSubTypes.Type(value = EmailSubscriptionProperties.class, name = "email_subscription")
-    })
-    @Valid
-    @Transient
-    private EndpointProperties properties;
 
     public UUID getId() {
         return id;
@@ -114,23 +96,6 @@ public class Endpoint extends CreationUpdateTimestamped {
         this.type = type;
     }
 
-    public EndpointProperties getProperties() {
-        return properties;
-    }
-
-    public <T extends EndpointProperties> T getProperties(Class<T> propertiesClass) {
-        if (!propertiesClass.isInstance(properties)) {
-            throw new IllegalStateException("Endpoint properties type mismatch, expected: " + propertiesClass.getName() +
-                    ", actual: " + properties.getClass().getName());
-        } else {
-            return propertiesClass.cast(properties);
-        }
-    }
-
-    public void setProperties(EndpointProperties properties) {
-        this.properties = properties;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -159,7 +124,6 @@ public class Endpoint extends CreationUpdateTimestamped {
                 ", type=" + type +
                 ", created=" + getCreated() +
                 ", updated=" + getUpdated() +
-                ", properties=" + properties +
                 '}';
     }
 }
