9d7046b267aeb94b997ddce47454699e95e7f097
Bennet Schulz <mail@bennet-schulz.de>
2021-07-05 13:23:21 +0200

    Add notifications-models

---
+++ notifications-models/src/main/java/com/redhat/cloud/notifications/models/Application.java
@@ -0,0 +1,113 @@
+package com.redhat.cloud.notifications.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+@Entity
+@Table(name = "applications")
+@JsonNaming(SnakeCaseStrategy.class)
+public class Application extends CreationUpdateTimestamped {
+
+    @Id
+    @GeneratedValue
+    @JsonProperty(access = READ_ONLY)
+    private UUID id;
+
+    @NotNull
+    @Pattern(regexp = "[a-z][a-z_0-9-]*")
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Schema(name = "display_name")
+    private String displayName;
+
+    @NotNull
+    @Transient
+    @Schema(name = "bundle_id")
+    private UUID bundleId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "bundle_id")
+    @JsonIgnore
+    private Bundle bundle;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public UUID getBundleId() {
+        if (bundleId == null && bundle != null) {
+            bundleId = bundle.getId();
+        }
+        return bundleId;
+    }
+
+    public void setBundleId(UUID bundleId) {
+        this.bundleId = bundleId;
+    }
+
+    public Bundle getBundle() {
+        return bundle;
+    }
+
+    public void setBundle(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof Application) {
+            Application other = (Application) o;
+            return Objects.equals(id, other.id);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ notifications-models/src/main/java/com/redhat/cloud/notifications/models/EmailAggregation.java
@@ -0,0 +1,105 @@
+package com.redhat.cloud.notifications.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.redhat.cloud.notifications.db.converters.JsonObjectConverter;
+import io.vertx.core.json.JsonObject;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
+
+@Entity
+@Table(name = "email_aggregation")
+public class EmailAggregation extends CreationTimestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "email_aggregation_id_seq")
+    @JsonProperty(access = READ_ONLY)
+    private Integer id;
+
+    @NotNull
+    @Size(max = 50)
+    private String accountId;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "bundle")
+    @JsonProperty("bundle")
+    private String bundleName;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "application")
+    @JsonProperty("application")
+    private String applicationName;
+
+    @NotNull
+    @Convert(converter = JsonObjectConverter.class)
+    private JsonObject payload;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getBundleName() {
+        return bundleName;
+    }
+
+    public void setBundleName(String bundleName) {
+        this.bundleName = bundleName;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    public JsonObject getPayload() {
+        return payload;
+    }
+
+    public void setPayload(JsonObject payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof EmailAggregation) {
+            EmailAggregation other = (EmailAggregation) o;
+            return Objects.equals(id, other.id);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
