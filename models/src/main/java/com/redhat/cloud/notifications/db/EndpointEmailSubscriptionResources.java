ecb88b56fff575d58b439ce803abf77af90212a4
Bennet Schulz <mail@bennet-schulz.de>
2021-07-06 12:44:24 +0200

    NOTIF-190 Add modules project

---
+++ models/src/main/java/com/redhat/cloud/notifications/db/EndpointEmailSubscriptionResources.java
@@ -0,0 +1,26 @@
+package com.redhat.cloud.notifications.db;
+
+import com.redhat.cloud.notifications.models.EmailSubscriptionType;
+import org.hibernate.Session;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class EndpointEmailSubscriptionResources {
+
+    @Inject
+    Session session;
+
+    @Transactional
+    public Long getEmailSubscribersCount(String accountNumber, String bundleName, String applicationName, EmailSubscriptionType subscriptionType) {
+        String query = "SELECT COUNT(user_id) FROM endpoint_email_subscriptions es, applications a, bundles b WHERE es.account_id = :accountId " +
+                "AND es.application_id = a.id AND a.bundle_id = b.id AND b.name = :bundleName AND a.name = :applicationName AND es.subscription_type = :subscriptionType";
+        return session.createNativeQuery(query, Long.class)
+                .setParameter("accountId", accountNumber)
+                .setParameter("bundleName", bundleName)
+                .setParameter("applicationName", applicationName)
+                .setParameter("subscriptionType", subscriptionType.name())
+                .getSingleResult();
+    }
+}
