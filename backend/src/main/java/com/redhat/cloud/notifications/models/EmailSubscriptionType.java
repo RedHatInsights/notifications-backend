ee3a8279d08677b309c5389004dcf332b1fb28cf
Bennet Schulz <mail@bennet-schulz.de>
2021-07-06 14:06:41 +0200

    NOTIF-190 Remove obsolete plugins

---
+++ backend/src/main/java/com/redhat/cloud/notifications/models/EmailSubscriptionType.java
@@ -0,0 +1,23 @@
+package com.redhat.cloud.notifications.models;
+
+import java.time.Duration;
+
+public enum EmailSubscriptionType {
+    INSTANT(null),
+    DAILY(Duration.ofDays(1));
+
+    private Duration duration;
+
+    EmailSubscriptionType(Duration duration) {
+        this.duration = duration;
+    }
+
+    public Duration getDuration() {
+        return this.duration;
+    }
+
+    // This may seem unused but it is actually required for a RestEasy request parameter deserialization.
+    public static EmailSubscriptionType fromString(String value) {
+        return EmailSubscriptionType.valueOf(value.toUpperCase());
+    }
+}
