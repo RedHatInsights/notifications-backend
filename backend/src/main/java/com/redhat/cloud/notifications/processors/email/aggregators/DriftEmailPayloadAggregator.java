5364ced4dab6b8f8acbd0a25611b6d1fe3a285c8
jpramos123 <jramos@redhat.com>
2021-07-14 14:06:49 -0300

    DRFT-344 Add Daily email template for Drift (#389)
    * Fix conflict
    
    * Template tests completed
    
    * Template tests completed
    
    * Made final changes requested by Juanjulio

---
+++ backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/DriftEmailPayloadAggregator.java
@@ -0,0 +1,79 @@
+package com.redhat.cloud.notifications.processors.email.aggregators;
+
+import com.redhat.cloud.notifications.models.EmailAggregation;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DriftEmailPayloadAggregator extends AbstractEmailPayloadAggregator {
+
+    private static final String DRIFT_KEY = "drift";
+    private static final String HOST_KEY = "hosts";
+    private static final String UNIQUE_SYSTEM_COUNT = "unique_system_count";
+    private static final String CONTEXT_KEY = "context";
+    private static final String EVENTS_KEY = "events";
+    private static final String PAYLOAD_KEY = "payload";
+
+    // Drift Payload
+    private static final String BASELINE_ID = "baseline_id";
+    private static final String BASELINE_NAME = "baseline_name";
+
+    private static final String DISPLAY_NAME = "display_name";
+    private static final String INVENTORY_ID = "inventory_id";
+    private static final String TAGS = "tags";
+
+    private final Set<String> uniqueHosts = new HashSet<>();
+    private final Map<String, HashSet<String>> uniqueHostPerBaseline = new HashMap<>();
+
+    public DriftEmailPayloadAggregator() {
+        context.put(DRIFT_KEY, new JsonObject());
+    }
+
+    void processEmailAggregation(EmailAggregation notification) {
+        JsonObject notificationJson = notification.getPayload();
+
+        JsonObject drift = context.getJsonObject(DRIFT_KEY);
+        JsonObject context = notificationJson.getJsonObject(CONTEXT_KEY);
+
+        JsonObject host = new JsonObject();
+        this.copyStringField(host, context, DISPLAY_NAME);
+        this.copyStringField(host, context, INVENTORY_ID);
+        host.put(TAGS, context.getJsonArray(TAGS));
+
+        notificationJson.getJsonArray(EVENTS_KEY).stream().forEach(eventObject -> {
+            JsonObject event = (JsonObject) eventObject;
+            JsonObject payload = event.getJsonObject(PAYLOAD_KEY);
+            String baselineId = payload.getString(BASELINE_ID);
+
+            if (!drift.containsKey(baselineId)) {
+                JsonObject newBaseline = new JsonObject();
+                this.copyStringField(newBaseline, payload, BASELINE_ID);
+                this.copyStringField(newBaseline, payload, BASELINE_NAME);
+
+                newBaseline.put(HOST_KEY, new JsonArray());
+
+                drift.put(baselineId, newBaseline);
+                uniqueHostPerBaseline.put(baselineId, new HashSet<>());
+            }
+
+            JsonObject baseline = drift.getJsonObject(baselineId);
+            String insightsId = host.getString(INVENTORY_ID);
+            baseline.getJsonArray(HOST_KEY).add(host);
+            uniqueHostPerBaseline.get(baselineId).add(insightsId);
+            baseline.put(UNIQUE_SYSTEM_COUNT, this.uniqueHostPerBaseline.get(baselineId).size());
+        });
+
+        String insightsId = host.getString(INVENTORY_ID);
+        uniqueHosts.add(insightsId);
+        this.context.put(UNIQUE_SYSTEM_COUNT, this.uniqueHosts.size());
+    }
+
+    public Integer getUniqueHostCount() {
+        return this.uniqueHosts.size();
+    }
+
+}
---
+++ backend/src/test/java/com/redhat/cloud/notifications/templates/TestDriftTemplate.java
@@ -0,0 +1,75 @@
+package com.redhat.cloud.notifications.templates;
+
+import com.redhat.cloud.notifications.DriftTestHelpers;
+import com.redhat.cloud.notifications.processors.email.aggregators.DriftEmailPayloadAggregator;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+public class TestDriftTemplate {
+    private DriftEmailPayloadAggregator aggregator;
+
+    @BeforeEach
+    void setUp() {
+        aggregator = new DriftEmailPayloadAggregator();
+    }
+
+    @Test
+    public void testDailyEmailBodyOneBaselineAndOneSystem() {
+
+        LocalDateTime startTime = LocalDateTime.of(2021, 4, 22, 13, 15, 33);
+        LocalDateTime endTime = LocalDateTime.of(2021, 4, 22, 14, 15, 33);
+
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_01", "host-01"));
+        Map<String, Object> drift = aggregator.getContext();
+        drift.put("start_time", startTime.toString());
+        drift.put("end_time", endTime.toString());
+        String result = Drift.Templates.dailyEmailBody()
+                .data("action", Map.of("context", drift))
+                .render();
+        //System.out.println(result);
+    }
+
+    @Test
+    public void testDailyEmailBodyMultipleBaselineAndOneSystem() {
+
+        LocalDateTime startTime = LocalDateTime.of(2021, 4, 22, 13, 15, 33);
+        LocalDateTime endTime = LocalDateTime.of(2021, 4, 22, 14, 15, 33);
+
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_01", "host-01"));
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_02", "host-01"));
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_03", "host-01"));
+        Map<String, Object> drift = aggregator.getContext();
+        drift.put("start_time", startTime.toString());
+        drift.put("end_time", endTime.toString());
+        String result = Drift.Templates.dailyEmailBody()
+                .data("action", Map.of("context", drift))
+                .render();
+        //System.out.println(result);
+    }
+
+    @Test
+    public void testDailyEmailBodyOneBaselineAndMultipleSystem() {
+
+        LocalDateTime startTime = LocalDateTime.of(2021, 7, 14, 13, 15, 33);
+        LocalDateTime endTime = LocalDateTime.of(2021, 7, 14, 14, 15, 33);
+
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_01", "host-01"));
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_01", "host-02"));
+        aggregator.aggregate(DriftTestHelpers.createEmailAggregation("tenant", "rhel", "drift", "baseline_01", "host-03"));
+        Map<String, Object> drift = aggregator.getContext();
+        drift.put("start_time", startTime.toString());
+        drift.put("end_time", endTime.toString());
+        String result = Drift.Templates.dailyEmailBody()
+                .data("action", Map.of("context", drift))
+                .render();
+        assertTrue(result.contains("<b>1 baseline</b> drifted on <b>3 unique systems</b>"));
+        //System.out.println(result);
+    }
+}
