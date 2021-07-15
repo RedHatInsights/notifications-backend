d26600cce948dacbc44a15840dd5af2744734734
Bennet Schulz <mail@bennet-schulz.de>
2021-07-15 12:24:43 +0200

    NOTIF-190 Add failing test

--- backend/src/test/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessorTest.java
+++ backend/src/test/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessorTest.java
@@ -1,21 +1,29 @@
 package com.redhat.cloud.notifications.processors.email;
 
+import com.redhat.cloud.notifications.db.EndpointEmailSubscriptionResources;
 import com.redhat.cloud.notifications.ingress.Action;
-import com.redhat.cloud.notifications.models.EmailSubscriptionType;
+import com.redhat.cloud.notifications.models.Application;
+import com.redhat.cloud.notifications.models.EmailSubscription;
 import com.redhat.cloud.notifications.models.Endpoint;
 import com.redhat.cloud.notifications.models.EndpointType;
 import com.redhat.cloud.notifications.models.Notification;
 import com.redhat.cloud.notifications.models.NotificationHistory;
+import com.redhat.cloud.notifications.processors.webhooks.WebhookTypeProcessor;
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import static com.redhat.cloud.notifications.models.EmailSubscriptionType.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @QuarkusTest
 class EmailSubscriptionTypeProcessorTest {
@@ -55,8 +63,20 @@ class EmailSubscriptionTypeProcessorTest {
     }
 
     @Test
-    @Disabled
     void shouldSendEmail() {
-        testee.sendEmail(new Notification(new Action(), new Endpoint()), EmailSubscriptionType.DAILY);
+        List<EmailSubscription> emailSubscriptions = new LinkedList<>();
+        final EmailSubscription emailSubscription = new EmailSubscription();
+        emailSubscription.setApplication(new Application());
+        emailSubscriptions.add(emailSubscription);
+        Uni<List<EmailSubscription>> listUni = Uni.createFrom().item(emailSubscriptions);
+
+        final EndpointEmailSubscriptionResources endpointEmailSubscriptionResources = mock(EndpointEmailSubscriptionResources.class);
+        when(endpointEmailSubscriptionResources.getEmailSubscribers(anyString(), anyString(), anyString(), any())).thenReturn(listUni);
+
+        final WebhookTypeProcessor webhookTypeProcessor = spy(WebhookTypeProcessor.class);
+
+        testee.sendEmail(new Notification(new Action(), new Endpoint()), DAILY);
+
+        verify(webhookTypeProcessor, times(1)).doHttpRequest(any(), any(), any());
     }
 }
