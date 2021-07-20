1eb9aee32d061540bbd1903367be6f7a06e87bea
Bennet Schulz <mail@bennet-schulz.de>
2021-07-20 13:22:19 +0200

    NOTIF-190 Add property so we can enable and disable the old cron job

--- backend/src/main/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessor.java
+++ backend/src/main/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessor.java
@@ -80,6 +80,9 @@ public class EmailSubscriptionTypeProcessor implements EndpointTypeProcessor {
     @Inject
     EmailTemplateFactory emailTemplateFactory;
 
+    @ConfigProperty(name = "email.subscription.daily.cron.enabled", defaultValue = "true")
+    Boolean isScheduleEnabled;
+
     @ConfigProperty(name = "processor.email.bop_url")
     String bopUrl;
 
@@ -282,7 +285,9 @@ public class EmailSubscriptionTypeProcessor implements EndpointTypeProcessor {
     @Scheduled(identity = "dailyEmailProcessor", cron = "{email.subscription.daily.cron}")
     public void processDailyEmail(ScheduledExecution se) {
         // Only delete on the largest aggregate time frame. Currently daily.
-        processAggregateEmails(se.getScheduledFireTime()).await().indefinitely();
+        if (isScheduleEnabled) {
+            processAggregateEmails(se.getScheduledFireTime()).await().indefinitely();
+        }
     }
 
     Uni<List<Tuple2<NotificationHistory, EmailAggregationKey>>> processAggregateEmails(Instant scheduledFireTime) {
