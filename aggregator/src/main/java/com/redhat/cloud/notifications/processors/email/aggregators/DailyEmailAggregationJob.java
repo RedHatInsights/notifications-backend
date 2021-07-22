983fe979cd646f02cf942aadd0e3d6a32eb51ce6
Josejulio Martínez <jmartine@redhat.com>
2021-07-22 18:03:27 -0500

    Use AggregationCommand instead of aggregated payload

--- aggregator/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/DailyEmailAggregationJob.java
+++ aggregator/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/DailyEmailAggregationJob.java
@@ -3,9 +3,7 @@ package com.redhat.cloud.notifications.processors.email.aggregators;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.cloud.notifications.db.EmailAggregationResources;
-import com.redhat.cloud.notifications.db.EndpointEmailSubscriptionResources;
-import com.redhat.cloud.notifications.models.EmailAggregation;
-import com.redhat.cloud.notifications.models.EmailAggregationKey;
+import com.redhat.cloud.notifications.models.AggregationCommand;
 import com.redhat.cloud.notifications.models.EmailSubscriptionType;
 import io.quarkus.scheduler.Scheduled;
 import io.quarkus.scheduler.ScheduledExecution;
@@ -17,9 +15,9 @@ import javax.inject.Inject;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import static java.time.ZoneOffset.UTC;
 
@@ -32,9 +30,6 @@ class DailyEmailAggregationJob {
     EmailAggregationResources emailAggregationResources;
 
     @Inject
-    EndpointEmailSubscriptionResources subscriptionResources;
-
-    @Inject
     ObjectMapper objectMapper;
 
     @Inject
@@ -44,8 +39,10 @@ class DailyEmailAggregationJob {
     @Scheduled(identity = "dailyEmailProcessor", cron = "{notifications.aggregator.email.subscription.periodic.cron}")
     public void processDailyEmail(ScheduledExecution se) throws JsonProcessingException {
         if (isCronJobEnabled()) {
-            List<EmailAggregation> aggregatedEmails = processAggregateEmails(se.getScheduledFireTime());
-            emitter.send(objectMapper.writeValueAsString(aggregatedEmails));
+            List<AggregationCommand> aggregationCommands = processAggregateEmails(se.getScheduledFireTime());
+            for (AggregationCommand aggregationCommand: aggregationCommands) {
+                emitter.send(objectMapper.writeValueAsString(aggregationCommand));
+            }
         }
     }
 
@@ -54,58 +51,32 @@ class DailyEmailAggregationJob {
         return ConfigProvider.getConfig().getOptionalValue("notifications.aggregator.email.subscription.periodic.cron.enabled", Boolean.class).orElse(false);
     }
 
-    List<EmailAggregation> processAggregateEmails(Instant scheduledFireTime) {
+    List<AggregationCommand> processAggregateEmails(Instant scheduledFireTime) {
         Instant yesterdayScheduledFireTime = scheduledFireTime.minus(EmailSubscriptionType.DAILY.getDuration());
 
         LocalDateTime endTime = LocalDateTime.ofInstant(scheduledFireTime, UTC);
         LocalDateTime startTime = LocalDateTime.ofInstant(yesterdayScheduledFireTime, UTC);
         final LocalDateTime aggregateStarted = LocalDateTime.now();
 
-        LOG.info(String.format("Running %s email aggregation for period (%s, %s)", EmailSubscriptionType.DAILY, startTime, endTime));
+        LOG.info(String.format("Collecting email aggregation for period (%s, %s) and type %s", startTime, endTime, EmailSubscriptionType.DAILY));
 
-        final List<EmailAggregationKey> applicationsWithPendingAggregation = emailAggregationResources.getApplicationsWithPendingAggregation(startTime, endTime);
-
-        List<List<EmailAggregation>> list = new LinkedList<>();
-        for (EmailAggregationKey key : applicationsWithPendingAggregation) {
-            List<EmailAggregation> emailAggregations = processAggregateEmailsByAggregationKey(key, startTime, endTime, EmailSubscriptionType.DAILY);
-            list.add(emailAggregations);
-        }
+        final List<AggregationCommand> pendingAggregationCommands =
+                emailAggregationResources.getApplicationsWithPendingAggregation(startTime, endTime)
+                .stream()
+                .map(aggregationKey -> new AggregationCommand(aggregationKey, startTime, endTime, EmailSubscriptionType.DAILY))
+                .collect(Collectors.toList());
 
         LOG.info(
                 String.format(
-                        "Finished running %s email aggregation for period (%s, %s) after %d seconds. %d (accountIds, applications) pairs were processed",
-                        EmailSubscriptionType.DAILY,
+                        "Finished collecting email aggregations for period (%s, %s) and type %s after %d seconds. %d (accountIds, applications) pairs were processed",
                         startTime,
                         endTime,
+                        EmailSubscriptionType.DAILY,
                         ChronoUnit.SECONDS.between(aggregateStarted, LocalDateTime.now()),
-                        list.size()
+                        pendingAggregationCommands.size()
                 )
         );
-        if (list.size() == 0) {
-            return new LinkedList<>();
-        }
-        return list.get(0);
-    }
 
-    private List<EmailAggregation> processAggregateEmailsByAggregationKey(EmailAggregationKey aggregationKey, LocalDateTime startTime, LocalDateTime endTime, EmailSubscriptionType emailSubscriptionType) {
-        final Long subscriberCount = subscriptionResources.getEmailSubscribersCount(aggregationKey.getAccountId(), aggregationKey.getBundle(), aggregationKey.getApplication(), emailSubscriptionType);
-        AbstractEmailPayloadAggregator aggregator = PoliciesEmailPayloadAggregator.by(aggregationKey);
-
-        List<EmailAggregation> emailAggregations = new LinkedList<>();
-        if (subscriberCount > 0 && aggregator != null) {
-            emailAggregations = emailAggregationResources.getEmailAggregation(aggregationKey, startTime, endTime);
-            for (EmailAggregation emailAggregation : emailAggregations) {
-                aggregator.aggregate(emailAggregation);
-            }
-        }
-
-        // Nothing to do, delete them right away.
-        emailAggregationResources.purgeOldAggregation(aggregationKey, endTime);
-
-        if (aggregator == null || aggregator.getProcessedAggregations() == 0) {
-            return null;
-        }
-
-        return emailAggregations;
+        return pendingAggregationCommands;
     }
 }
