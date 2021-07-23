011ed6f5f9dd1cccb4f6ac907c4ee5d5612eae75
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-07-23 15:44:15 +0200

    Use PostgreSQL dump as aggregator initial Flyway script (#421)

--- aggregator/src/test/resources/db/migration/V1.15.0__behavior_group_actions.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE behavior_group_action
-    ADD COLUMN position INTEGER NOT NULL DEFAULT 0; -- The default value is required to migrate existing actions.
-
-ALTER TABLE behavior_group_action
-    ALTER COLUMN position DROP DEFAULT; -- Migration done, we can drop the default value.
