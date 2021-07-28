3791aeead165d96534e6ceba595ec10c7e06747b
Bennet Schulz <mail@bennet-schulz.de>
2021-07-28 15:38:33 +0200

    Revert "NOTIF-190 Temporarily remove Kafka topic interactions in backend and aggregator"
    This reverts commit c514faae

--- backend/src/test/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessorTest.java
+++ backend/src/test/java/com/redhat/cloud/notifications/processors/email/EmailSubscriptionTypeProcessorTest.java
@@ -3,20 +3,36 @@ package com.redhat.cloud.notifications.processors.email;
 import com.redhat.cloud.notifications.db.DbIsolatedTest;
 import com.redhat.cloud.notifications.db.EmailAggregationResources;
 import com.redhat.cloud.notifications.ingress.Action;
+import com.redhat.cloud.notifications.models.AggregationCommand;
+import com.redhat.cloud.notifications.models.EmailAggregationKey;
 import com.redhat.cloud.notifications.models.NotificationHistory;
+import com.redhat.cloud.notifications.templates.Blank;
 import com.redhat.cloud.notifications.templates.EmailTemplateFactory;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
 import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
+import io.vertx.core.json.Json;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Map;
+
+import static com.redhat.cloud.notifications.models.EmailSubscriptionType.DAILY;
+import static com.redhat.cloud.notifications.processors.email.EmailSubscriptionTypeProcessor.AGGREGATION_CHANNEL;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @QuarkusTest
 class EmailSubscriptionTypeProcessorTest extends DbIsolatedTest {
@@ -56,4 +72,56 @@ class EmailSubscriptionTypeProcessorTest extends DbIsolatedTest {
                 .assertHasNotReceivedAnyItem();
     }
 
+    @Test
+    void shouldSuccessfullySendEmail() {
+        AggregationCommand aggregationCommand1 = new AggregationCommand(
+                new EmailAggregationKey("account-1", "bundle-1", "app-1"),
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1),
+                DAILY
+        );
+        AggregationCommand aggregationCommand2 = new AggregationCommand(
+                new EmailAggregationKey("account-2", "bundle-2", "app-2"),
+                LocalDateTime.now(ZoneOffset.UTC).plusDays(1),
+                LocalDateTime.now(ZoneOffset.UTC).plusDays(2),
+                DAILY
+        );
+
+        Mockito.when(emailTemplateFactory.get(anyString(), anyString())).thenReturn(new Blank());
+
+        inMemoryConnector.source(AGGREGATION_CHANNEL).send(Json.encode(aggregationCommand1));
+        inMemoryConnector.source(AGGREGATION_CHANNEL).send(Json.encode(aggregationCommand2));
+
+        // Let's check that EndpointEmailSubscriptionResources#sendEmail was called for each aggregation.
+        verify(emailAggregationResources, times(1)).getEmailAggregation(
+                eq(aggregationCommand1.getAggregationKey()),
+                eq(aggregationCommand1.getStart()),
+                eq(aggregationCommand1.getEnd())
+        );
+
+        verify(emailAggregationResources, times(1)).purgeOldAggregation(
+                eq(aggregationCommand1.getAggregationKey()),
+                eq(aggregationCommand1.getEnd())
+        );
+        verify(emailAggregationResources, times(1)).getEmailAggregation(
+                eq(aggregationCommand2.getAggregationKey()),
+                eq(aggregationCommand2.getStart()),
+                eq(aggregationCommand2.getEnd())
+        );
+        verify(emailAggregationResources, times(1)).purgeOldAggregation(
+                eq(aggregationCommand2.getAggregationKey()),
+                eq(aggregationCommand2.getEnd())
+        );
+        verifyNoMoreInteractions(emailAggregationResources);
+    }
+
+    @Test
+    void consumeEmailAggregationsShouldNotThrowInCaseOfInvalidPayload() {
+        Uni<Void> consumeEmailAggregations = testee.consumeEmailAggregations("I am not valid!");
+
+        consumeEmailAggregations.subscribe()
+                .withSubscriber(UniAssertSubscriber.create())
+                .assertCompleted()
+                .assertItem(null);
+    }
 }
