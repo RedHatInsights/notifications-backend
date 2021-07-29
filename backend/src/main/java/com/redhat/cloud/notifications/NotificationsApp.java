c3bc5acfcfa4eb692b542f2deb6d2b00a9c659fb
Bennet Schulz <mail@bennet-schulz.de>
2021-07-29 15:26:33 +0200

    NOTIF-175 Print git commit sha during startup

--- backend/src/main/java/com/redhat/cloud/notifications/NotificationsApp.java
+++ backend/src/main/java/com/redhat/cloud/notifications/NotificationsApp.java
@@ -3,17 +3,16 @@ package com.redhat.cloud.notifications;
 import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
-
 import javax.enterprise.event.Observes;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NotificationsApp {
-
-    private static final String BUILD_COMMIT_ENV_NAME = "OPENSHIFT_BUILD_COMMIT";
-    private static final String BUILD_REFERENCE_ENV_NAME = "OPENSHIFT_BUILD_REFERENCE";
-    private static final String BUILD_NAME_ENV_NAME = "OPENSHIFT_BUILD_NAME";
 
     public static final String FILTER_REGEX = ".*(/health(/\\w+)?|/metrics) HTTP/[0-9].[0-9]\" 200.*\\n?";
     private static final Pattern pattern = Pattern.compile(FILTER_REGEX);
@@ -27,7 +26,7 @@ public class NotificationsApp {
     void init(@Observes StartupEvent ev) {
         initAccessLogFilter();
 
-        showVersionInfo();
+        LOG.info(readGitProperties());
     }
 
     private void initAccessLogFilter() {
@@ -39,13 +38,26 @@ public class NotificationsApp {
         });
     }
 
-    private void showVersionInfo() {
-        LOG.info("Starting notifications backend");
-        String buildCommit = System.getenv(BUILD_COMMIT_ENV_NAME);
-        String osBuildRef = System.getenv(BUILD_REFERENCE_ENV_NAME);
-        String osBuildName = System.getenv(BUILD_NAME_ENV_NAME);
-
-        LOG.infof("\ts2i-build [%s]\n\tfrom branch [%s]\n\twith git sha [%s]", osBuildName, osBuildRef, buildCommit);
+    private String readGitProperties() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+        try {
+            return readFromInputStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Version information could not be retrieved";
+        }
     }
 
+    private String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
 }
