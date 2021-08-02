47b50981847e36142090032545a7c4b75136b46b
Bennet Schulz <mail@bennet-schulz.de>
2021-08-02 19:42:01 +0200

    NOTIF-190 Add registry login again

--- .rhcicd/build_deploy_backend.sh
+++ .rhcicd/build_deploy_backend.sh
@@ -5,9 +5,15 @@ set -exv
 IMAGE="quay.io/cloudservices/notifications-backend"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
+if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
+    echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
+    exit 1
+fi
+
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
+docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
 docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile-build-backend.jvm
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
 docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
