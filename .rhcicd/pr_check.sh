3c2896b0a3607e25077565ad007901f94d48ae22
Bennet Schulz <mail@bennet-schulz.de>
2021-08-02 16:48:06 +0200

    NOTIF-190 Use backend Dockerfile for Jenkins PR check

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -8,7 +8,7 @@ IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
-docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile-build.jvm
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile-build-backend.jvm
 
 # Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
 mkdir -p $WORKSPACE/artifacts
