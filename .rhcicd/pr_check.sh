62fda7536ee31270d405d5591760e26d7c463f32
Bennet Schulz <mail@bennet-schulz.de>
2021-08-03 14:03:53 +0200

    NOTIF-190 Add aggregator to PR check

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -9,6 +9,7 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
 docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile-build-backend.jvm
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile-build-aggregator.jvm
 
 # Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
 mkdir -p $WORKSPACE/artifacts
