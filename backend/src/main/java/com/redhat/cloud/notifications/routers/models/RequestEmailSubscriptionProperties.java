a00fde61a94aea7693903cda8df76e243e4a9024
Josejulio Martínez <jmartine@redhat.com>
2021-08-06 14:08:20 -0500

    Use snakecase

--- backend/src/main/java/com/redhat/cloud/notifications/routers/models/RequestEmailSubscriptionProperties.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/models/RequestEmailSubscriptionProperties.java
@@ -1,7 +1,11 @@
 package com.redhat.cloud.notifications.routers.models;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import javax.validation.constraints.NotNull;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RequestEmailSubscriptionProperties {
     @NotNull
     private boolean onlyAdmins;
