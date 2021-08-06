fe878222a73f1b5eea1b2bb5aabe64c3cbd2d580
Alexandra Ferdinand <aferdina@redhat.com>
2021-08-06 13:04:25 -0400

    add table to webapp for admin console

--- backend/src/main/webapp/src/pages/ApplicationPage.tsx
+++ backend/src/main/webapp/src/pages/ApplicationPage.tsx
@@ -1,4 +1,6 @@
 import { List, ListItem, Spinner } from '@patternfly/react-core';
+import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
+import { Caption, TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import * as React from 'react';
 import { useParams } from 'react-router';
 
@@ -11,6 +13,7 @@ type ApplicationPageParams = {
 export const ApplicationPage: React.FunctionComponent = () => {
     const { applicationId } = useParams<ApplicationPageParams>();
     const eventTypesQuery = useEventTypes(applicationId);
+    const columns = [ 'Event', 'Event Id', 'Description' ];
 
     if (eventTypesQuery.loading) {
         return <Spinner />;
@@ -21,10 +24,37 @@ export const ApplicationPage: React.FunctionComponent = () => {
     }
 
     return (
-        <List>
-            { eventTypesQuery.payload.map((e: any) => (
-                <ListItem key={ e.id }>{ e.display_name }</ListItem>
-            )) }
-        </List>
-    );
-};
+        <React.Fragment>
+            <TableComposable
+                aria-label="Simple table"
+            >
+                <Caption>
+                    <Breadcrumb>
+                        <BreadcrumbItem to="#">Red Hat Enterprise Linux</BreadcrumbItem>
+                        <BreadcrumbItem to="#" isActive>Policies</BreadcrumbItem>
+                    </Breadcrumb></Caption>
+                <Thead>
+                    <Tr>
+                        {columns.map((column, columnIndex) => (
+                            <Th key={ columnIndex }>{column}</Th>
+                        ))}
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    { eventTypesQuery.payload.map((e: any) => (
+                        <Td key={ e.id }> { e.display_name }
+                        </Td>
+                    ))}
+                    { eventTypesQuery.payload.map((e: any) => (
+                        <Td key={ e.id }> { e.id }
+                        </Td>
+                    ))}
+                    { eventTypesQuery.payload.map((e: any) => (
+                        <Td key={ e.id }> { e.description }
+                        </Td>
+                    ))}
+                </Tbody>
+            </TableComposable>
+        </React.Fragment>
+
+    );};
