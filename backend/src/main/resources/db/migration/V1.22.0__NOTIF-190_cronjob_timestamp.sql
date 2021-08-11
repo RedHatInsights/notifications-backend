6c500ace1a9db761bba7f9912ba58c27e33ca69b
Bennet Schulz <mail@bennet-schulz.de>
2021-08-11 11:53:10 +0200

    NOTIF-190 Add flyway insert script

--- backend/src/main/resources/db/migration/V1.22.0__NOTIF-190_cronjob_timestamp.sql
+++ backend/src/main/resources/db/migration/V1.22.0__NOTIF-190_cronjob_timestamp.sql
@@ -2,3 +2,6 @@ CREATE TABLE cronjob_run (
     id UUID NOT NULL,
     last_run TIMESTAMP
 ) WITH (OIDS=FALSE);
+
+INSERT INTO cronjob_run (id, last_run) VALUES
+('3db0955b-751b-48cd-b531-c1d81596d133', NOW());
