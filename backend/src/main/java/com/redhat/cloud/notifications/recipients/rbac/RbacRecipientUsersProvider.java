482ea591721d446b8f92e1044a752b865c9c9734
Josejulio Martínez <jmartine@redhat.com>
2021-08-17 12:12:01 -0500

    Adds retry for rbac calls

--- backend/src/main/java/com/redhat/cloud/notifications/recipients/rbac/RbacRecipientUsersProvider.java
+++ backend/src/main/java/com/redhat/cloud/notifications/recipients/rbac/RbacRecipientUsersProvider.java
@@ -2,6 +2,7 @@ package com.redhat.cloud.notifications.recipients.rbac;
 
 import com.redhat.cloud.notifications.recipients.User;
 import com.redhat.cloud.notifications.routers.models.Page;
+import io.netty.channel.ConnectTimeoutException;
 import io.quarkus.cache.CacheResult;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
@@ -10,6 +11,8 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -27,25 +30,38 @@ public class RbacRecipientUsersProvider {
     @ConfigProperty(name = "recipient-provider.rbac.elements-per-page", defaultValue = "40")
     Integer rbacElementsPerPage;
 
+    @ConfigProperty(name = "rbac.retry.max-attempts", defaultValue = "3")
+    long maxRetryAttempts;
+
+    @ConfigProperty(name = "rbac.retry.back-off.initial-value", defaultValue = "0.1S")
+    Duration initialBackOff;
+
+    @ConfigProperty(name = "rbac.retry.back-off.max-value", defaultValue = "1S")
+    Duration maxBackOff;
+
     @CacheResult(cacheName = "rbac-recipient-users-provider-get-users")
     public Uni<List<User>> getUsers(String accountId, boolean adminsOnly) {
         return getWithPagination(
-            page -> rbacServiceToService
-                    .getUsers(accountId, adminsOnly, page * rbacElementsPerPage, rbacElementsPerPage)
+            page -> retryOnError(
+                    rbacServiceToService
+                            .getUsers(accountId, adminsOnly, page * rbacElementsPerPage, rbacElementsPerPage)
+            )
         // .memoize().indefinitely() should be removed after the Quarkus 2.0 bump
         ).memoize().indefinitely();
     }
 
     @CacheResult(cacheName = "rbac-recipient-users-provider-get-group-users")
     public Uni<List<User>> getGroupUsers(String accountId, boolean adminOnly, UUID groupId) {
-        return rbacServiceToService.getGroup(accountId, groupId)
+        return retryOnError(rbacServiceToService.getGroup(accountId, groupId))
                 .onItem().transformToUni(rbacGroup -> {
                     if (rbacGroup.isPlatformDefault()) {
                         return getUsers(accountId, adminOnly);
                     } else {
                         return getWithPagination(
-                            page -> rbacServiceToService
-                                    .getGroupUsers(accountId, groupId, page * rbacElementsPerPage, rbacElementsPerPage)
+                            page -> retryOnError(
+                                    rbacServiceToService
+                                            .getGroupUsers(accountId, groupId, page * rbacElementsPerPage, rbacElementsPerPage)
+                            )
                         )
                         // getGroupUsers doesn't have an adminOnly param.
                         .onItem().transform(users -> {
@@ -58,6 +74,14 @@ public class RbacRecipientUsersProvider {
                     }
                 // .memoize().indefinitely() should be removed after the Quarkus 2.0 bump
                 }).memoize().indefinitely();
+    }
+
+    private <T> Uni<T> retryOnError(Uni<T> uni) {
+        return uni
+                .onFailure(failure -> failure.getClass() == IOException.class || failure.getClass() == ConnectTimeoutException.class)
+                .retry()
+                .withBackOff(initialBackOff, maxBackOff)
+                .atMost(maxRetryAttempts);
     }
 
     private Uni<List<User>> getWithPagination(Function<Integer, Uni<Page<RbacUser>>> fetcher) {
