a4fe3d0bb615b18c62234ae5da164922faf52563
Gwenneg Lepage <gwenneg@gmail.com>
2021-08-26 08:39:23 +0200

    NOTIF-291 Delete manually the initial huge amount of history data

--- backend/src/main/resources/db/migration/V1.24.0__NOTIF-291_data_migration.sql
+++ backend/src/main/resources/db/migration/V1.24.0__NOTIF-291_data_migration.sql
@@ -1,3 +1,11 @@
+-- The notification_history table contains a huge number of records that are no longer needed because we have a 14 days
+-- retention delay for that table. Normally, the EventLogCleaner scheduled job is supposed to delete the expired
+-- records but its first execution won't work because there's way too much data: the SQL query will get stuck and put a
+-- never-ending exclusive lock on the notification_history rows. To prevent this from happening, we need to clean the
+-- expired records manually here, before the event log data migration is done below.
+DELETE FROM notification_history
+WHERE created < '2021-08-12T08:00:00.000000';
+
 INSERT INTO event(id, account_id, created)
 SELECT id, account_id, created
 FROM notification_history
