7ff79a8c0b58b5372bd371b5ad51209c2c728d68
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2021-09-09 11:04:14 +0200

    feat(templates): integrate Managed Kafka notification templates
    This is the first step of integrating Managed Kafka templates. We'll be working on adding some
    content on each of the added templates but let's keep them empty for now to be able to perform all
    the required Java bindings

---
+++ backend/src/main/java/com/redhat/cloud/notifications/templates/Rhosak.java
@@ -0,0 +1,92 @@
+package com.redhat.cloud.notifications.templates;
+
+import com.redhat.cloud.notifications.models.EmailSubscriptionType;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+// Name needs to be "Rhosak" to read templates from resources/templates/Rhosak
+public class Rhosak implements EmailTemplate {
+    private static final String SCHEDULED_UPGRADE = "scheduled_upgrade";
+    private static final String DISRUPTION = "disruption";
+    private static final String INSTANCE_CREATED = "instance_created";
+    private static final String INSTANCE_DELETED = "instance_deleted";
+    private static final String ACTION_REQUIRED = "action_required";
+
+    @Override
+    public TemplateInstance getTitle(String eventType, EmailSubscriptionType type) {
+        if (eventType.equals(SCHEDULED_UPGRADE)) {
+            return Templates.scheduledUpgradeTitle();
+        } else if (eventType.equals(DISRUPTION)) {
+            return Templates.serviceDisruptionTitle();
+        } else if (eventType.equals(INSTANCE_CREATED)) {
+            return Templates.instanceCreatedTitle();
+        } else if (eventType.equals(INSTANCE_DELETED)) {
+            return Templates.instanceDeletedTitle();
+        } else if (eventType.equals(ACTION_REQUIRED)) {
+            return Templates.actionRequiredTitle();
+        }
+
+        throw new UnsupportedOperationException(String.format(
+                "No email title template for RHOSAK event_type: %s and EmailSubscription: %s found.",
+                eventType, type
+        ));
+    }
+
+    @Override
+    public TemplateInstance getBody(String eventType, EmailSubscriptionType type) {
+        if (eventType.equals(SCHEDULED_UPGRADE)) {
+            return Templates.scheduledUpgradeBody();
+        } else if (eventType.equals(DISRUPTION)) {
+            return Templates.serviceDisruptionBody();
+        } else if (eventType.equals(INSTANCE_CREATED)) {
+            return Templates.instanceCreatedBody();
+        } else if (eventType.equals(INSTANCE_DELETED)) {
+            return Templates.instanceDeletedBody();
+        } else if (eventType.equals(ACTION_REQUIRED)) {
+            return Templates.actionRequiredBody();
+        }
+
+        throw new UnsupportedOperationException(String.format(
+                "No email body template for RHOSAK event_type: %s and EmailSubscription: %s found.",
+                eventType, type
+        ));
+    }
+
+    @Override
+    public boolean isSupported(String eventType, EmailSubscriptionType type) {
+        if (eventType.equals(SCHEDULED_UPGRADE)) {
+            return type == EmailSubscriptionType.DAILY; // scheduled upgrades emails are sent on daily basis
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isEmailSubscriptionSupported(EmailSubscriptionType type) {
+        return true;
+    }
+
+    @CheckedTemplate
+    public static class Templates {
+
+        public static native TemplateInstance actionRequiredTitle();
+
+        public static native TemplateInstance actionRequiredBody();
+
+        public static native TemplateInstance instanceCreatedTitle();
+
+        public static native TemplateInstance instanceCreatedBody();
+
+        public static native TemplateInstance instanceDeletedTitle();
+
+        public static native TemplateInstance instanceDeletedBody();
+
+        public static native TemplateInstance scheduledUpgradeTitle();
+
+        public static native TemplateInstance scheduledUpgradeBody();
+
+        public static native TemplateInstance serviceDisruptionTitle();
+
+        public static native TemplateInstance serviceDisruptionBody();
+    }
+}
