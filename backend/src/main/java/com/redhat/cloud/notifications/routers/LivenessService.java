4fd72ab78faedf1dbec96671cecf713bcde6968d
Gwenneg Lepage <glepage@redhat.com>
2021-09-15 09:16:08 +0200

    Implement AsyncHealthCheck in LivenessService

--- backend/src/main/java/com/redhat/cloud/notifications/routers/LivenessService.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/LivenessService.java
@@ -1,8 +1,8 @@
 package com.redhat.cloud.notifications.routers;
 
 import com.redhat.cloud.notifications.StuffHolder;
+import io.smallrye.health.api.AsyncHealthCheck;
 import io.smallrye.mutiny.Uni;
-import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Liveness;
@@ -13,13 +13,13 @@ import javax.inject.Inject;
 
 @ApplicationScoped
 @Liveness
-public class LivenessService implements HealthCheck {
+public class LivenessService implements AsyncHealthCheck {
 
 //    @Inject
 //    HealthCenter messagingHealth;
 
     @Inject
-    Mutiny.Session session;
+    Mutiny.StatelessSession statelessSession;
 
 //    public Uni<Boolean> getLiveness() {
 //        Uni<Boolean> postgresHealth = postgresConnectionHealth();
@@ -38,25 +38,26 @@ public class LivenessService implements HealthCheck {
 //    }
 
     Uni<Boolean> postgresConnectionHealth() {
-        return session.createNativeQuery("SELECT 1")
+        return statelessSession.createNativeQuery("SELECT 1")
                 .getSingleResult()
                 .replaceWith(Boolean.TRUE)
                 .onFailure().recoverWithItem(Boolean.FALSE);
     }
 
     @Override
-    public HealthCheckResponse call() {
+    public Uni<HealthCheckResponse> call() {
 
         boolean adminDown = StuffHolder.getInstance().isAdminDown();
 
         HealthCheckResponseBuilder response = HealthCheckResponse.named("Notifications readiness check");
         if (adminDown) {
-            response.down().withData("status", "admin-down");
+            return Uni.createFrom().item(() ->
+                response.down().withData("status", "admin-down").build()
+            );
         } else {
-            boolean dbState = postgresConnectionHealth().await().indefinitely();
-            response.state(dbState).withData("reactive-db-check", dbState);
+            return postgresConnectionHealth().onItem().transform(dbState ->
+                response.state(dbState).withData("reactive-db-check", dbState).build()
+            );
         }
-
-        return response.build();
     }
 }
