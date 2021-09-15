b2d595dcd754fe305f00223770b02621335f30d4
Gwenneg Lepage <glepage@redhat.com>
2021-09-15 09:34:58 +0200

    Remove RBAC user query feature flag

--- backend/src/test/java/com/redhat/cloud/notifications/processors/email/RecipientResolverTest.java
+++ backend/src/test/java/com/redhat/cloud/notifications/processors/email/RecipientResolverTest.java
@@ -3,7 +3,6 @@ package com.redhat.cloud.notifications.processors.email;
 import com.redhat.cloud.notifications.models.EmailSubscriptionProperties;
 import com.redhat.cloud.notifications.models.Endpoint;
 import com.redhat.cloud.notifications.models.EndpointType;
-import com.redhat.cloud.notifications.recipients.User;
 import com.redhat.cloud.notifications.recipients.rbac.RbacRecipientUsersProvider;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
@@ -22,37 +21,8 @@ public class RecipientResolverTest {
     RbacRecipientUsersProvider rbacRecipientUsersProvider;
 
     @Test
-    public void withPersonalizedEmailOff() {
-        RecipientResolver recipientResolver = new RecipientResolver();
-        recipientResolver.rbacUserQuery = false;
-        recipientResolver.rbacRecipientUsersProvider = rbacRecipientUsersProvider;
-
-        Endpoint endpoint = new Endpoint();
-        endpoint.setType(EndpointType.EMAIL_SUBSCRIPTION);
-        endpoint.setEnabled(true);
-        endpoint.setAccountId(ACCOUNT_ID);
-        endpoint.setProperties(new EmailSubscriptionProperties());
-
-        User user1 = new User();
-        user1.setUsername("foo");
-        User user2 = new User();
-        user2.setUsername("bar");
-
-        recipientResolver.recipientUsers(
-                ACCOUNT_ID,
-                Set.of(endpoint),
-                Set.of("foo", "bar")
-        )
-                .subscribe()
-                .withSubscriber(UniAssertSubscriber.create())
-                .assertCompleted()
-                .assertItem(Set.of(user1, user2));
-    }
-
-    @Test
     public void withPersonalizedEmailOn() {
         RecipientResolver recipientResolver = new RecipientResolver();
-        recipientResolver.rbacUserQuery = true;
         recipientResolver.rbacRecipientUsersProvider = rbacRecipientUsersProvider;
 
         Endpoint endpoint = new Endpoint();
