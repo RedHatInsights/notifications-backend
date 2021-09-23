2c60401f54e0f6ec23d96084531f2c5c85867ccc
Josejulio Martínez <jmartine@redhat.com>
2021-09-23 12:24:12 -0500

    NOTIF-328 Split admin ui from backend code

---
+++ .github/scripts/push-admin-console.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DEPLOY_REPO="git@github.com:RedHatInsights/notifications-frontend-admin-build.git"
+BRANCH="main"
+
+cd admin-console/src/main/webapp/build/ || exit
+if git ls-remote --exit-code ${DEPLOY_REPO:-$REPO} ${BRANCH} &>/dev/null; then
+  # Handle where the target branch exists
+  git clone --bare --branch ${BRANCH} --depth 1 ${DEPLOY_REPO:-$REPO} .git
+else
+  # Handle where the repo does not have a default branch (i.e. an empty repo)
+  git init
+  git remote add origin ${DEPLOY_REPO:-$REPO}
+  git commit --allow-empty -m "Initial commit"
+  git push origin master
+  git checkout -b ${BRANCH}
+fi
+
+git config core.bare false
+git add -A
+git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+git push origin ${BRANCH
