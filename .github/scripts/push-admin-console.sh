156505e6dcfc97d1fe25e3c651f72e432d9ebd51
Josejulio Martínez <jmartine@redhat.com>
2021-09-23 13:43:00 -0500

    Adding gh token, user and commit message

--- .github/scripts/push-admin-console.sh
+++ .github/scripts/push-admin-console.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-set -x
+set -v
 
-DEPLOY_REPO="git@github.com:RedHatInsights/notifications-frontend-admin-build.git"
+DEPLOY_REPO="https://${GH_TOKEN}@github.com/RedHatInsights/notifications-frontend-admin-build.git"
 BRANCH="main"
 
 cd admin-console/src/main/webapp/build/ || exit
 
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GH_USER}"
+git config --global user.email "${GH_MAIL}"
 
-if git ls-remote --exit-code ${DEPLOY_REPO:-$REPO} ${BRANCH} &>/dev/null; then
+if git ls-remote --exit-code "${DEPLOY_REPO:-$REPO}" ${BRANCH} &>/dev/null; then
   # Handle where the target branch exists
-  git clone --bare --branch ${BRANCH} --depth 1 ${DEPLOY_REPO:-$REPO} .git
+  git clone --bare --branch ${BRANCH} --depth 1 "${DEPLOY_REPO:-$REPO}" .git
 else
   # Handle where the repo does not have a default branch (i.e. an empty repo)
   git init
-  git remote add origin ${DEPLOY_REPO:-$REPO}
+  git remote add origin "${DEPLOY_REPO:-$REPO}"
   git commit --allow-empty -m "Initial commit"
   git push origin master
   git checkout -b ${BRANCH}
@@ -23,5 +23,5 @@ fi
 
 git config core.bare false
 git add -A
-git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+git commit -m "${COMMIT_MESSAGE}"
 git push origin ${BRANCH}
