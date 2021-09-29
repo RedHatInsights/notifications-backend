204ec931546902956c502dabbc8868c646c25286
Josejulio Martínez <jmartine@redhat.com>
2021-09-29 18:51:37 -0500

    Try pushing to local repository

--- .github/scripts/push-admin-console.sh
+++ .github/scripts/push-admin-console.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
+# When the builds succeeds, we need to place the results in the backend META-INF code and commit it.
+# Delete whatever is there and add the new build.
 
-DEPLOY_REPO="https://${GITHUB_ACTOR}:${1}@github.com/RedHatInsights/notifications-frontend-admin-build.git"
-BRANCH="main"
+TARGET="backend/src/main/resources/META-INF/resources/internal"
 
-cd admin-console/src/main/webapp/build/ || exit
+rm -rf "${TARGET}"
+mkdir -p "${TARGET}"
+cp -r admin-console/src/main/webapp/build/* "${TARGET}"
 
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${GITHUB_ACTOR}[automatic action]"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
-if git ls-remote --exit-code "${DEPLOY_REPO:-$REPO}" ${BRANCH} &>/dev/null; then
-  # Handle where the target branch exists
-  git clone --bare --branch ${BRANCH} --depth 1 "${DEPLOY_REPO:-$REPO}" .git
-else
-  # Handle where the repo does not have a default branch (i.e. an empty repo)
-  git init -b "${BRANCH}"
-  git remote add origin "${DEPLOY_REPO}"
-  git commit --allow-empty -m "Initial commit"
-fi
-
-git config core.bare false
-git add -A
-git commit -m "${COMMIT_MESSAGE}"
-git push origin ${BRANCH}
+git add -f "${TARGET}"
+git commit -m "Updating admin-console build"
+git push
