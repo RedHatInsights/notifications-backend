16de3988bce3991237d5448f07dafc3ceb70d328
Yuval Lahav <ylahav@redhat.com>
2021-09-29 14:02:12 +0300

    remove the empty line from pr_check

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -5,7 +5,6 @@ set -exv
 docker build . -f docker/Dockerfile-build-backend.jvm
 docker build . -f docker/Dockerfile-build-aggregator.jvm
 
-
 # Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
 mkdir -p $WORKSPACE/artifacts
 cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
