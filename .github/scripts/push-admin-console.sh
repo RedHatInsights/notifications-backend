0e9f9d00ebcf1779c3788d6857f73dfbbf718131
Josejulio Martínez <jmartine@redhat.com>
2021-09-30 18:41:14 +0200

    NOTIF-328 Split admin ui from backend code (#643)
    * NOTIF-328 Split admin ui from backend code
    
    * Using depth:2
    
    * Add checkstyle
    
    * Set config name/email
    
    * Fixes
    
    * Using github actions env
    
    * Adding gh token, user and commit message
    
    * More tweaking
    
    * Moving the token to a user token
    
    * Downloads admin-ui and places it in build dir
    
    * Try pushing to local repository
    
    * Using master branch
    
    * Updating artifactID
    
    * Use new artifactId in build.yml
    
    * Apply suggestions from code review
    
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>
    
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

---
+++ .github/scripts/push-admin-console.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# When the builds succeeds, we need to place the results in the backend META-INF code and commit it.
+# Delete whatever is there and add the new build.
+
+TARGET="backend/src/main/resources/META-INF/resources/internal"
+
+rm -rf "${TARGET}"
+mkdir -p "${TARGET}"
+cp -r admin-console/src/main/webapp/build/* "${TARGET}"
+
+git config --global user.name "${GITHUB_ACTOR}[admin-console push script]"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add -f "${TARGET}"
+git commit -m "Update admin-console build artifacts"
+git push
