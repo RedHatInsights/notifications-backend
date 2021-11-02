2871ad8925b6061bdbc61bc4b248455f275852f6
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2021-11-02 14:51:33 +0100

    refactor(rhosak): update texts following some content adjustments (#738)
    * refactor(rhosak): update texts following some content adjustments
    
    * feat(rhosak): add service disruption's start time in daily report
    
    * refactor: update backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/RhosakEmailAggregator.java
    
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>
    
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/RhosakEmailAggregator.java
+++ backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/RhosakEmailAggregator.java
@@ -3,6 +3,8 @@ package com.redhat.cloud.notifications.processors.email.aggregators;
 import com.redhat.cloud.notifications.models.EmailAggregation;
 import io.vertx.core.json.JsonObject;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,7 +28,9 @@ public class RhosakEmailAggregator extends AbstractEmailPayloadAggregator {
     private static final String LATENCY = "latency";
     private static final String THROUGHPUT = "throughput";
     private static final String AVAILABILITY = "availability";
-    public static final String UPGRADE_TIME = "upgrade_time";
+    private static final String UPGRADE_TIME = "upgrade_time";
+    private static final String START_TIME = "start_time";
+    private static final String TIMESTAMP = "timestamp";
 
     public RhosakEmailAggregator() {
         context.put(UPGRADES, new JsonObject());
@@ -72,6 +76,7 @@ public class RhosakEmailAggregator extends AbstractEmailPayloadAggregator {
     }
 
     private void buildServiceDisruptionPayload(JsonObject aggregationPayload, JsonObject context) {
+        String timestamp = aggregationPayload.getString(TIMESTAMP, LocalDateTime.now(ZoneOffset.UTC).toString());
         String serviceDisruptionImpact = context.getString(SERVICE_DISRUPTION_IMPACT);
         boolean currentDisruptionImpactPerformance = serviceDisruptionImpact.contains(PERFORMANCE);
         boolean currentDisruptionImpactLatency = serviceDisruptionImpact.contains(LATENCY);
@@ -87,6 +92,7 @@ public class RhosakEmailAggregator extends AbstractEmailPayloadAggregator {
             if (!disruptions.containsKey(name)) {
                 serviceDisruption = new JsonObject();
                 serviceDisruption.put(PAYLOAD_NAME, name);
+                serviceDisruption.put(START_TIME, timestamp);
                 serviceDisruption.put(SERVICE_DISRUPTION_IMPACT, serviceDisruptionImpact);
                 disruptions.put(name, serviceDisruption);
             } else {
