44a8476fbe64c329040082260829e0eefa5a0510
Papa Bakary Camara <pcamara@redhat.com>
2021-11-03 13:24:15 +0100

    Cleanup OpenShift Advisor templates and tests

--- backend/src/test/java/com/redhat/cloud/notifications/templates/TestOpenshiftAdvisorTemplate.java
+++ backend/src/test/java/com/redhat/cloud/notifications/templates/TestOpenshiftAdvisorTemplate.java
@@ -19,7 +19,8 @@ public class TestOpenshiftAdvisorTemplate {
                 .data("action", action)
                 .render();
 
-        assertEquals("OpenShift - Advisor Instant Notification - 20 May 2021", result, "Title is the expected.");
+        assertTrue(result.contains("OpenShift - Advisor Instant Notification - "));
+        assertTrue(result.contains("2021"));
     }
 
     @Test
@@ -61,50 +62,5 @@ public class TestOpenshiftAdvisorTemplate {
         assertFalse(result2.contains("alt=\"Moderate severity\""), "Body 2 should not contain moderate severity rule image");
         assertFalse(result2.contains("alt=\"Important severity\""), "Body 2 should not contain important severity rule image");
         assertFalse(result2.contains("alt=\"Critical severity\""), "Body 2 should not contain critical severity rule image");
-    }
-
-    @Test
-    public void testDigestEmailTitle() {
-        Action action = TestHelpers.createAdvisorOpenshiftAction("123456", "weekly-digest");
-        String result = AdvisorOpenshift.Templates.weeklyDigestEmailTitle()
-                .data("action", action)
-                .render();
-
-        assertEquals("OpenShift - Advisor Weekly Report - 20 May 2021", result, "Title is the expected.");
-    }
-
-    @Test
-    public void testDigestEmailBody() {
-        Action action = TestHelpers.createAdvisorOpenshiftAction("123456", "weekly-digest");
-        final String result = AdvisorOpenshift.Templates.weeklyDigestEmailBody()
-                .data("action", action)
-                .render();
-
-        assertTrue(action.getEvents().size() == 1, "weekly digest should only contain one event");
-
-        action.getEvents().forEach(event -> {
-            assertTrue(
-                    result.contains(event.getPayload().get("total_clusters").toString()),
-                    "Body should contain total_clusters" + event.getPayload().get("total_clusters")
-            );
-            assertTrue(
-                    result.contains(event.getPayload().get("total_recommendations").toString()),
-                    "Body should contain total_recommendations" + event.getPayload().get("total_recommendations")
-            );
-            assertTrue(
-                    result.contains(event.getPayload().get("total_incidents").toString()),
-                    "Body should contain total_incidents" + event.getPayload().get("total_incidents")
-            );
-            assertTrue(
-                    result.contains(event.getPayload().get("total_critical").toString()),
-                    "Body should contain total_critical" + event.getPayload().get("total_critical")
-            );
-            assertTrue(
-                    result.contains(event.getPayload().get("total_important").toString()),
-                    "Body should contain total_important" + event.getPayload().get("total_important")
-            );        });
-
-        assertTrue(result.contains("alt=\"Important severity\""), "Body should contain important severity rule image");
-        assertTrue(result.contains("alt=\"Critical severity\""), "Body should contain critical severity rule image");
     }
 }
