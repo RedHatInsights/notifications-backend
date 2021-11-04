ddaf0b27a70cf6e5171e92f5ed645b84481f1cd3
Gwenneg Lepage <glepage@redhat.com>
2021-11-04 13:46:11 +0100

    NOTIF-336 Work around an ephemeral RBAC timeout issue

--- backend/src/main/java/com/redhat/cloud/notifications/ClowderConfigInitializer.java
+++ backend/src/main/java/com/redhat/cloud/notifications/ClowderConfigInitializer.java
@@ -14,6 +14,7 @@ public class ClowderConfigInitializer {
     private static final Logger LOGGER = Logger.getLogger(ClowderConfigInitializer.class);
     private static final String RBAC_AUTHENTICATION_URL_KEY = "rbac-authentication/mp-rest/url";
     private static final String RBAC_S2S_URL_KEY = "rbac-s2s/mp-rest/url";
+    private static final String EPHEMERAL_RBAC_AUTH_TIMEOUT = "10000";
 
     @ConfigProperty(name = "clowder.endpoints.rbac-service")
     Optional<String> rbacClowderEndpoint;
@@ -24,6 +25,15 @@ public class ClowderConfigInitializer {
             LOGGER.infof("Overriding the RBAC URL with the config value from Clowder: %s", rbacUrl);
             System.setProperty(RBAC_AUTHENTICATION_URL_KEY, rbacUrl);
             System.setProperty(RBAC_S2S_URL_KEY, rbacUrl);
+        }
+        increaseRbacAuthTimeoutOnEphemeral();
+    }
+
+    void increaseRbacAuthTimeoutOnEphemeral() {
+        String envName = System.getenv("ENV_NAME");
+        if (envName != null && envName.startsWith("env-ephemeral")) {
+            LOGGER.warnf("Ephemeral environment detected, activating RBAC auth timeout workaround. The new timeout is %sms. Remove this ASAP!", EPHEMERAL_RBAC_AUTH_TIMEOUT);
+            System.setProperty("rbac-authentication/mp-rest/readTimeout", EPHEMERAL_RBAC_AUTH_TIMEOUT);
         }
     }
 }
