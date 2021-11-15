699a367558cda7653f287c1a7aee3ad5403efc76
Bennet Schulz <mail@bennet-schulz.de>
2021-11-15 14:51:07 +0100

    NOTIF-360 Add produces and consumes

--- backend/src/main/java/com/redhat/cloud/notifications/routers/ValidationEndpoint.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/ValidationEndpoint.java
@@ -7,8 +7,11 @@ import org.jboss.resteasy.reactive.RestQuery;
 
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static com.redhat.cloud.notifications.Constants.INTERNAL;
@@ -20,6 +23,8 @@ public class ValidationEndpoint {
     ApplicationResources appResources;
 
     @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> validate(@RestQuery String bundle, @RestQuery String application, @RestQuery String eventType) {
         return appResources.getEventType(bundle, application, eventType)
                 .onItem()
