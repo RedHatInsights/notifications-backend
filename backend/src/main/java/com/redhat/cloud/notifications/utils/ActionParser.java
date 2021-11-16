614f71f6e7ab660bf4ffea07c650c9266f2c0300
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-11-16 09:43:11 +0100

    Log the payload in case of action parsing failure (#787)

--- backend/src/main/java/com/redhat/cloud/notifications/utils/ActionParser.java
+++ backend/src/main/java/com/redhat/cloud/notifications/utils/ActionParser.java
@@ -4,16 +4,20 @@ import com.redhat.cloud.notifications.ingress.Action;
 import com.redhat.cloud.notifications.ingress.Decoder;
 import com.redhat.cloud.notifications.ingress.Registry;
 import io.smallrye.mutiny.Uni;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class ActionParser {
 
+    private static final Logger LOGGER = Logger.getLogger(ActionParser.class);
+
     private final Registry registry = new Registry();
     private final Decoder decoder = new Decoder(registry);
 
     public Uni<Action> fromJsonString(String actionJson) {
-        return Uni.createFrom().item(() -> decoder.decode(actionJson));
+        return Uni.createFrom().item(() -> decoder.decode(actionJson))
+                .onFailure().invoke(e -> LOGGER.warnf(e, "Action parsing failed for payload: %s", actionJson));
     }
 }
