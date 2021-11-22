5d3c867156d7ea7de17c4c7223e7d32d6c43a0b3
Josejulio Martínez <jmartine@redhat.com>
2021-11-22 12:55:17 -0700

    Remove import

--- backend/src/main/java/com/redhat/cloud/notifications/routers/models/SettingsValueJsonForm.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/models/SettingsValueJsonForm.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.redhat.cloud.notifications.models.EmailSubscriptionType;
 
 import java.util.ArrayList;
 import java.util.List;
