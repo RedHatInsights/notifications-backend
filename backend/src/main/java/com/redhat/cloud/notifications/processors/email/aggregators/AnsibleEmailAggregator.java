8549deec45e4e21a921a39648470019d3c100793
Josejulio Martínez <finwemartinez@gmail.com>
2021-12-06 11:50:31 -0700

    Update backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AnsibleEmailAggregator.java

--- backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AnsibleEmailAggregator.java
+++ backend/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AnsibleEmailAggregator.java
@@ -1,13 +1,6 @@
 package com.redhat.cloud.notifications.processors.email.aggregators;
 
 import com.redhat.cloud.notifications.models.EmailAggregation;
-import io.vertx.core.json.JsonObject;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class AnsibleEmailAggregator extends AbstractEmailPayloadAggregator {
 
