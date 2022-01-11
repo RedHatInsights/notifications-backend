297f2c7a072c8f7c6e6fbc96539237c168e399df
Bennet Schulz <mail@bennet-schulz.de>
2022-01-11 08:20:58 +0100

    NOTIF-381 remove unused import

--- backend/src/main/java/com/redhat/cloud/notifications/recipients/itservice/ITUserServiceWrapper.java
+++ backend/src/main/java/com/redhat/cloud/notifications/recipients/itservice/ITUserServiceWrapper.java
@@ -2,16 +2,11 @@ package com.redhat.cloud.notifications.recipients.itservice;
 
 import com.redhat.cloud.notifications.recipients.itservice.pojo.request.ITUserRequest;
 import com.redhat.cloud.notifications.recipients.itservice.pojo.response.ITUserResponse;
-import com.redhat.cloud.notifications.recipients.rbac.RbacUser;
-import com.redhat.cloud.notifications.routers.models.Page;
 import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 @ApplicationScoped
