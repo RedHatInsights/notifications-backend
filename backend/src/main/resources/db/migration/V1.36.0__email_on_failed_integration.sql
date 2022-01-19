5b1a0aafb618aff941b952241afde5ebc1a1eb7b
Heiko W. Rupp <hwr@pilhuhn.de>
2022-01-19 12:43:02 +0100

    Don't fail if the b/a/et already exist.

--- backend/src/main/resources/db/migration/V1.36.0__email_on_failed_integration.sql
+++ backend/src/main/resources/db/migration/V1.36.0__email_on_failed_integration.sql
@@ -1,7 +1,8 @@
 --
 -- Create a new b/a/et console/notifications/integration-failed
 insert into  bundles (name, display_name, id, created)
-    values ('console','Console', public.gen_random_uuid(), now() at time zone 'UTC' );
+    values ('console','Console', public.gen_random_uuid(), now() at time zone 'UTC' )
+    on conflict do nothing;
 
 insert into applications (bundle_id, name, display_name, id, created)
     select b.id ,
@@ -11,6 +12,7 @@ insert into applications (bundle_id, name, display_name, id, created)
             now() at time zone 'UTC'
     from bundles b
     where b.name = 'console'
+    on conflict do nothing
         ;
 
 insert into event_type (application_id, name, display_name, id )
@@ -20,4 +22,5 @@ insert into event_type (application_id, name, display_name, id )
             public.gen_random_uuid()
     from applications a
     where a.name = 'notifications'
+    on conflict do nothing
     ;
