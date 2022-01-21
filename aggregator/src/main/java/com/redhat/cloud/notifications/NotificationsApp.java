9238eddc943b3faeb455a316c2cc6d070295c038
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2022-01-21 11:10:28 +0100

    Remove HTTP config keys from aggregator (#927)

--- aggregator/src/main/java/com/redhat/cloud/notifications/NotificationsApp.java
+++ aggregator/src/main/java/com/redhat/cloud/notifications/NotificationsApp.java
@@ -2,48 +2,28 @@ package com.redhat.cloud.notifications;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @QuarkusMain
 public class NotificationsApp implements QuarkusApplication {
 
-    private static final String FILTER_REGEX = ".*(/health(/\\w+)?|/metrics) HTTP/[0-9].[0-9]\" 200.*\\n?";
-    private static final Pattern PATTERN = Pattern.compile(FILTER_REGEX);
-
     private static final Logger LOG = Logger.getLogger(NotificationsApp.class);
-
-    @ConfigProperty(name = "quarkus.http.access-log.category")
-    String loggerName;
 
     @Inject
     DailyEmailAggregationJob dailyEmailAggregationJob;
 
     @Override
     public int run(String... args) {
-        initAccessLogFilter();
-
         LOG.info(readGitProperties());
 
         dailyEmailAggregationJob.processDailyEmail();
 
         return 0;
-    }
-
-    private void initAccessLogFilter() {
-        java.util.logging.Logger accessLog = java.util.logging.Logger.getLogger(loggerName);
-        accessLog.setFilter(record -> {
-            final String logMessage = record.getMessage();
-            Matcher matcher = PATTERN.matcher(logMessage);
-            return !matcher.matches();
-        });
     }
 
     private String readGitProperties() {
