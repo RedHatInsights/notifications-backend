f8947d21894e142a2ae62a00fc2c95012cae9d5e
Alexandra Ferdinand <aferdina@redhat.com>
2022-02-01 12:53:18 -0500

    add link

--- admin-console/src/main/webapp/src/pages/BundlePage.tsx
+++ admin-console/src/main/webapp/src/pages/BundlePage.tsx
@@ -2,10 +2,10 @@ import { Breadcrumb, BreadcrumbItem, Button, PageSection, Spinner, Title, Toolba
 import { PencilAltIcon, TrashIcon } from '@patternfly/react-icons';
 import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import * as React from 'react';
+import { useMemo } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import { linkTo } from '../Routes';
-import { useApplicationTypes } from '../services/GetApplication';
 import { useBundles } from '../services/GetBundles';
 
 type BundlePageParams = {
@@ -15,8 +15,15 @@ type BundlePageParams = {
 export const BundlePage: React.FunctionComponent = () => {
     const getBundles = useBundles();
     const { applicationId } = useParams<BundlePageParams>();
-    const getApplications = useApplicationTypes(applicationId);
     const eventTypePageUrl = React.useMemo(() => linkTo.application(applicationId), [ applicationId ]);
+
+    const bundle = useMemo(() => {
+        if (getBundles.isLoading || getBundles.bundles.map(b => b.id)) {
+            return getBundles.bundles.map(b =>b.displayName);
+        }
+
+        return undefined;
+    }, [ getBundles.bundles, getBundles.isLoading ]);
 
     const columns = [ 'Application', 'Application Id', 'Event Types' ];
 
@@ -26,7 +33,8 @@ export const BundlePage: React.FunctionComponent = () => {
                 <Title headingLevel='h1'>
                     <Breadcrumb>
                         <BreadcrumbItem target='#'> Bundles </BreadcrumbItem>
-                        <BreadcrumbItem target='#'> { getBundles.bundles.map(b => b.displayName)} </BreadcrumbItem>
+                        <BreadcrumbItem target='#'> { bundle }
+                        </BreadcrumbItem>
                     </Breadcrumb>
                 </Title>
                 <TableComposable aria-label="Applications table">
@@ -45,17 +53,19 @@ export const BundlePage: React.FunctionComponent = () => {
                         </Tr>
                     </Thead>
                     <Tbody>
-                        <Tr>
-                            <Link to={ eventTypePageUrl }></Link>
-                            <Td></Td>
-                            <Td>eventType</Td>
-                            <Td>
-                                <Button className='edit' type='button' variant='plain'
-                                > { <PencilAltIcon /> } </Button></Td>
-                            <Td>
-                                <Button className='delete' type='button' variant='plain'
-                                >{ <TrashIcon /> } </Button></Td>
-                        </Tr>
+                        { getBundles.bundles.map(b => b.applications.map(a => (
+                            <Tr key={ a.id }>
+                                <Link to={ eventTypePageUrl }>{ a.displayName }</Link>
+                                <Td>{ a.id }</Td>
+                                <Td>eventType</Td>
+                                <Td>
+                                    <Button className='edit' type='button' variant='plain'
+                                    > { <PencilAltIcon /> } </Button></Td>
+                                <Td>
+                                    <Button className='delete' type='button' variant='plain'
+                                    >{ <TrashIcon /> } </Button></Td>
+                            </Tr>
+                        )))}
                     </Tbody>
                 </TableComposable>
             </PageSection>
