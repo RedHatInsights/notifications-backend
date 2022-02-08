dd67d02115c1a661bb174dc262050a8d2eb4cdf3
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2022-02-08 14:19:56 +0100

    Remove endpoints subtype migration query (#974)

--- database/src/main/resources/db/migration/V1.37.0__add_subtype_to_endpoint.sql
+++ database/src/main/resources/db/migration/V1.37.0__add_subtype_to_endpoint.sql
@@ -10,8 +10,5 @@ CREATE INDEX ix_endpoints_type_sub_type ON endpoints (endpoint_type, endpoint_su
 -- Migrate camel sub_types
 UPDATE endpoints as e SET endpoint_sub_type = (SELECT sub_type FROM camel_properties WHERE id = e.id);
 
--- Populate notification_history endpoint_sub_type
-UPDATE notification_history as n SET endpoint_sub_type = e.endpoint_sub_type FROM endpoints e WHERE e.id = n.endpoint_id;
-
 -- Drop column
 ALTER TABLE camel_properties DROP COLUMN sub_type;
