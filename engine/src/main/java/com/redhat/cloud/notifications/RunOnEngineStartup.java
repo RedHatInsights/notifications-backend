b20eaad2e2c80e3ec5aa0cf6e377779aed668522
Gwenneg Lepage <glepage@redhat.com>
2022-03-11 11:11:19 +0100

    Print info about the keystore

--- engine/src/main/java/com/redhat/cloud/notifications/RunOnEngineStartup.java
+++ engine/src/main/java/com/redhat/cloud/notifications/RunOnEngineStartup.java
@@ -1,12 +1,19 @@
 package com.redhat.cloud.notifications;
 
 import io.quarkus.runtime.Startup;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jboss.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Startup
 public class RunOnEngineStartup {
+
+    private static Logger LOGGER = Logger.getLogger(RunOnEngineStartup.class);
 
     @Inject
     StartupUtils startupUtils;
@@ -16,5 +23,19 @@ public class RunOnEngineStartup {
         startupUtils.initAccessLogFilter();
         startupUtils.logGitProperties();
         startupUtils.logExternalServiceUrl("rbac-s2s/mp-rest/url");
+
+        logConfigValue("quarkus.http.ssl.certificate.key-store-file");
+        logConfigValue("quarkus.http.ssl.certificate.key-store-file-type");
+
+        try {
+            String keystore = Files.readString(Paths.get(ConfigProvider.getConfig().getValue("quarkus.http.ssl.certificate.key-store-file", String.class)));
+            LOGGER.info(keystore.substring(0, 5));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void logConfigValue(String configKey) {
+        LOGGER.infof("%s=%s", configKey, ConfigProvider.getConfig().getValue(configKey, String.class));
     }
 }
