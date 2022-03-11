205002626278ae1071a5cc21329119fc71f4d0f8
Gwenneg Lepage <glepage@redhat.com>
2022-03-11 12:26:24 +0100

    Fix decoding? one more tiiiime

--- engine/src/main/java/com/redhat/cloud/notifications/RunOnEngineStartup.java
+++ engine/src/main/java/com/redhat/cloud/notifications/RunOnEngineStartup.java
@@ -37,6 +37,7 @@ public class RunOnEngineStartup {
             LOGGER.info(new String(decodedKs, StandardCharsets.UTF_8).substring(0, 5));
             Files.write(Paths.get("/tmp/keystore.jks"), decodedKs);
             System.setProperty("quarkus.http.ssl.certificate.key-store-file", "/tmp/keystore.jks");
+            logConfigValue("quarkus.http.ssl.certificate.key-store-file");
         } catch (IOException e) {
             e.printStackTrace();
         }
