4c2671af461d3e1466bb5262ae755d5176e5f0b1
Heiko W. Rupp <hwr@pilhuhn.de>
2022-03-14 17:59:22 +0100

    Use the bridge uuid instead of bridge name.

--- common/src/main/java/com/redhat/cloud/notifications/openbridge/Bridge.java
+++ common/src/main/java/com/redhat/cloud/notifications/openbridge/Bridge.java
@@ -6,10 +6,12 @@ package com.redhat.cloud.notifications.openbridge;
 public class Bridge {
     private String id;
     private String eventsEndpoint;
+    private String name;
 
-    public Bridge(String id, String eventsEndpoint) {
+    public Bridge(String id, String eventsEndpoint, String name) {
         this.id = id;
         this.eventsEndpoint = eventsEndpoint;
+        this.name = name;
     }
 
     public String getId() {
@@ -20,4 +22,13 @@ public class Bridge {
         return eventsEndpoint;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bridge{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", eventsEndpoint='").append(eventsEndpoint).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
