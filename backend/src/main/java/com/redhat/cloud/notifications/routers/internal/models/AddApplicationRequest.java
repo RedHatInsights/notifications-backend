3e04aec72df3e1bbdbbe5d472a7723f845c12d1c
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-03-21 11:49:50 -0600

    Review feedback

--- backend/src/main/java/com/redhat/cloud/notifications/routers/internal/models/AddApplicationRequest.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/internal/models/AddApplicationRequest.java
@@ -1,12 +1,18 @@
 package com.redhat.cloud.notifications.routers.internal.models;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AddApplicationRequest {
     @NotNull
     @Pattern(regexp = "[a-z][a-z_0-9-]*")
+    @Size(max = 255)
     public String name;
 
     @NotNull
@@ -15,5 +21,6 @@ public class AddApplicationRequest {
     @NotNull
     public UUID bundleId;
 
+    @Size(max = 200)
     public String ownerRole;
 }
