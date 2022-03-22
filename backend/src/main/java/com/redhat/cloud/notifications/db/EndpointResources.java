fb2aa2abd966836b22a043bb3fb0e804edab42ce
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-03-22 07:21:27 -0600

    Apply suggestions from code review
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- backend/src/main/java/com/redhat/cloud/notifications/db/EndpointResources.java
+++ backend/src/main/java/com/redhat/cloud/notifications/db/EndpointResources.java
@@ -139,7 +139,7 @@ public class EndpointResources {
                 .where(
                         WhereBuilder.builder()
                                 .and("e.accountId = :accountId", "accountId", tenant)
-                                .ifAnd(name != null && !name.equals(""), "e.name LIKE :name", "name", "%" + name + "%")
+                                .ifAnd(name != null && !name.isEmpty(), "LOWER(e.name) LIKE :name", "name", "%" + name.toLowerCase() + "%")
                 );
     }
 
@@ -320,7 +320,7 @@ public class EndpointResources {
                                             .ifOr(compositeTypes.size() > 0, "e.compositeType IN (:compositeTypes)", "compositeTypes", compositeTypes)
                             )
                             .ifAnd(activeOnly != null, "e.enabled = :enabled", "enabled", activeOnly)
-                            .ifAnd(name != null && !name.equals(""), "e.name LIKE :name", "name", "%" + name + "%")
+                            .ifAnd(name != null && !name.isEmpty(), "LOWER(e.name) LIKE :name", "name", "%" + name.toLowerCase() + "%")
                 );
     }
 
