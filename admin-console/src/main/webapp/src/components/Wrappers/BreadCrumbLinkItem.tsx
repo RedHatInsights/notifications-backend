db3b945efc6ce454ea1af30cc4f413c878b77c7d
Alexandra Ferdinand <aferdina@redhat.com>
2022-03-24 10:04:17 -0400

    remove ouia

--- admin-console/src/main/webapp/src/components/Wrappers/BreadCrumbLinkItem.tsx
+++ admin-console/src/main/webapp/src/components/Wrappers/BreadCrumbLinkItem.tsx
@@ -1,15 +1,14 @@
 import { BreadcrumbItem, BreadcrumbItemProps } from '@patternfly/react-core';
 import * as React from 'react';
 
-import { OuiaComponentProps, withoutOuiaProps } from '../../utils/Ouia';
 import { LinkAdapter } from './LinkAdapter';
 
-type BreadcrumbLinkItemProps = Omit<BreadcrumbItemProps, 'component'> & OuiaComponentProps;
+type BreadcrumbLinkItemProps = Omit<BreadcrumbItemProps, 'component'>
 
 export const BreadcrumbLinkItem: React.FunctionComponent<BreadcrumbLinkItemProps> = (props) => {
     return (
         <BreadcrumbItem
-            { ...withoutOuiaProps(props) }
+            { ...props }
             component={ LinkAdapter }
         >
             { props.children }
