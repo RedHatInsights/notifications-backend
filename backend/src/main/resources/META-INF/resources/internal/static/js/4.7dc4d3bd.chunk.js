d2d6c241187a46e56777cff8f888dcdfced0f2a9
josejulio[admin-console push script] <josejulio@users.noreply.github.com>
2022-05-03 19:46:54 +0000

    Update admin-console build artifacts

---
+++ backend/src/main/resources/META-INF/resources/internal/static/js/4.7dc4d3bd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinternal=this.webpackJsonpinternal||[]).push([[4],{706:function(n,i,p){}}]);
+//# sourceMappingURL=4.7dc4d3bd.chunk.js.map
---
+++ backend/src/main/resources/META-INF/resources/internal/static/js/6.52777c69.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinternal=this.webpackJsonpinternal||[]).push([[6],{707:function(n,i,p){}}]);
+//# sourceMappingURL=6.52777c69.chunk.js.map
