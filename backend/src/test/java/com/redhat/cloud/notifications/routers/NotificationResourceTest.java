5d181cd8e641660aff378827d1bc563d828d975a
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-05-06 07:12:57 -0600

    NOTIF-338 Allow to filter by event type name (#1212)
    * NOTIF-338 Allow to filter by event type name
    
    * Adds name
    
    * Adding tests

--- backend/src/test/java/com/redhat/cloud/notifications/routers/NotificationResourceTest.java
+++ backend/src/test/java/com/redhat/cloud/notifications/routers/NotificationResourceTest.java
@@ -244,6 +244,68 @@ public class NotificationResourceTest extends DbIsolatedTest {
     }
 
     @Test
+    void testEventTypeFetchingByEventTypeName() {
+        helpers.createTestAppAndEventTypes();
+        Header identityHeader = initRbacMock(TENANT, USERNAME, RbacAccess.FULL_ACCESS);
+
+        Response response = given()
+                .when()
+                .header(identityHeader)
+                .queryParam("eventTypeName", "50")
+                .get("/notifications/eventTypes")
+                .then()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract().response();
+
+        JsonObject page = new JsonObject(response.getBody().asString());
+        JsonArray eventTypes = page.getJsonArray("data");
+        for (int i = 0; i < eventTypes.size(); i++) {
+            JsonObject ev = eventTypes.getJsonObject(i);
+            ev.mapTo(EventType.class);
+            assertTrue(ev.getString("display_name").contains("50") || ev.getString("name").contains("50"));
+        }
+
+        assertEquals(2, page.getJsonObject("meta").getInteger("count"));
+        assertEquals(2, eventTypes.size());
+    }
+
+    @Test
+    void testEventTypeFetchingByBundleApplicationAndEventTypeName() {
+        helpers.createTestAppAndEventTypes();
+        List<Application> apps = applicationRepository.getApplications(TEST_BUNDLE_NAME);
+        Application app = apps.stream().filter(a -> a.getName().equals(TEST_APP_NAME_2)).findFirst().get();
+        UUID myOtherTesterApplicationId = app.getId();
+        UUID myBundleId = app.getBundleId();
+        Header identityHeader = initRbacMock(TENANT, USERNAME, RbacAccess.FULL_ACCESS);
+
+        Response response = given()
+                .when()
+                .header(identityHeader)
+                .queryParam("bundleId", myBundleId)
+                .queryParam("applicationIds", myOtherTesterApplicationId)
+                .queryParam("eventTypeName", "50")
+                .get("/notifications/eventTypes")
+                .then()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract().response();
+
+        JsonObject page = new JsonObject(response.getBody().asString());
+        JsonArray eventTypes = page.getJsonArray("data");
+        for (int i = 0; i < eventTypes.size(); i++) {
+            JsonObject ev = eventTypes.getJsonObject(i);
+            ev.mapTo(EventType.class);
+            assertEquals(myBundleId.toString(), ev.getJsonObject("application").getString("bundle_id"));
+            assertEquals(myOtherTesterApplicationId.toString(), ev.getJsonObject("application").getString("id"));
+            assertTrue(ev.getString("display_name").contains("50") || ev.getString("name").contains("50"));
+        }
+
+        assertEquals(1, page.getJsonObject("meta").getInteger("count"));
+        assertEquals(1, eventTypes.size());
+    }
+
+    @Test
     void testGetEventTypesAffectedByEndpoint() {
         String tenant = "testGetEventTypesAffectedByEndpoint";
         Header identityHeader = initRbacMock(tenant, "user", FULL_ACCESS);
