9e5e37098dcda234a272deb4f606e3617fd5c8ed
Bennet Schulz <mail@bennet-schulz.de>
2022-05-10 14:20:26 +0200

    NOTIF-603 Change index so it matches most of the existing index name pattern

--- database/src/main/resources/db/migration/V1.50.0__NOTIF-603_add_org_id.sql
+++ database/src/main/resources/db/migration/V1.50.0__NOTIF-603_add_org_id.sql
@@ -1,9 +1,10 @@
 ALTER TABLE email_aggregation
   ADD COLUMN org_id text;
 
-CREATE INDEX email_aggregation_idx ON public.email_aggregation (org_id);
+CREATE INDEX ix_email_aggregation_org_id ON public.email_aggregation (org_id);
 
 ALTER TABLE event
   ADD COLUMN org_id text;
 
-CREATE INDEX event_idx ON public.event (org_id);
+CREATE INDEX ix_event_org_id ON public.event (org_id);
+
