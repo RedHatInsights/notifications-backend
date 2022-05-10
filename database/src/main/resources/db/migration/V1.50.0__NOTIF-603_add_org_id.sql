fd56a44e79fb92aa191db558219b3ce7e481ac60
Bennet Schulz <mail@bennet-schulz.de>
2022-05-10 09:26:54 +0200

    NOTIF-603 Add index on email_aggregation and event

--- database/src/main/resources/db/migration/V1.50.0__NOTIF-603_add_org_id.sql
+++ database/src/main/resources/db/migration/V1.50.0__NOTIF-603_add_org_id.sql
@@ -1,5 +1,9 @@
 ALTER TABLE email_aggregation
   ADD COLUMN org_id text;
 
+CREATE INDEX email_aggregation_idx ON public.email_aggregation (org_id);
+
 ALTER TABLE event
   ADD COLUMN org_id text;
+
+CREATE INDEX event_idx ON public.event (org_id);
