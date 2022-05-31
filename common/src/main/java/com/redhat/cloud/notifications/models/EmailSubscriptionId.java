e291da9b5cb18233fcd3fe3764f1eaafe379e12d
Bennet Schulz <mail@bennet-schulz.de>
2022-05-31 12:13:08 +0200

    NOTIF-603 Add orgId to EmailSubscription

--- common/src/main/java/com/redhat/cloud/notifications/models/EmailSubscriptionId.java
+++ common/src/main/java/com/redhat/cloud/notifications/models/EmailSubscriptionId.java
@@ -29,6 +29,9 @@ public class EmailSubscriptionId implements Serializable {
     @Convert(converter = EmailSubscriptionTypeConverter.class)
     public EmailSubscriptionType subscriptionType;
 
+    @Size(max = 50)
+    public String orgId;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
