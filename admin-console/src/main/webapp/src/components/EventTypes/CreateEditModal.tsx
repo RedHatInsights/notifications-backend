4b5456e41ca2c89eba544fe3add27d3c1147f44c
Alexandra Ferdinand <aferdina@redhat.com>
2022-06-06 14:33:06 -0400

    remove BG from ET

--- admin-console/src/main/webapp/src/components/EventTypes/CreateEditModal.tsx
+++ admin-console/src/main/webapp/src/components/EventTypes/CreateEditModal.tsx
@@ -1,9 +1,8 @@
-import { ActionGroup, Button, Form, FormGroup, FormSelect, FormSelectOption,
+import { ActionGroup, Button, Form, FormGroup,
     HelperText, HelperTextItem, Modal, ModalVariant, TextArea, TextInput } from '@patternfly/react-core';
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import { useSystemBehaviorGroups } from '../../services/SystemBehaviorGroups/GetBehaviorGroups';
-import { BehaviorGroup, EventType } from '../../types/Notifications';
+import { EventType } from '../../types/Notifications';
 
 interface CreateEditModalProps {
     isEdit: boolean;
@@ -19,17 +18,10 @@ interface CreateEditModalProps {
 export const CreateEditModal: React.FunctionComponent<CreateEditModalProps> = (props) => {
 
     const [ eventType, setEventType ] = React.useState<Partial<EventType>>(props.initialEventType ?? {});
-    const [ systemBehaviorGroup, setSystemBehaviorGroup ] = React.useState<Partial<BehaviorGroup>>();
-    const getBehaviorGroups = useSystemBehaviorGroups();
 
     const handleChange = (value: string, event: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => {
         const target = event.target as HTMLInputElement;
         setEventType(prev => ({ ...prev, [target.name]: target.value }));
-    };
-
-    const handleSelect = (value: string, event: React.FormEvent<HTMLSelectElement>) => {
-        const target = event.target as HTMLSelectElement;
-        setSystemBehaviorGroup(prev => ({ ...prev, [target.name]: target.value }));
     };
 
     const onSubmitLocal = React.useCallback(() => {
@@ -39,18 +31,6 @@ export const CreateEditModal: React.FunctionComponent<CreateEditModalProps> = (p
     React.useEffect(() => {
         setEventType(props.initialEventType ?? {});
     }, [ props.initialEventType ]);
-
-    const listSystemBehaviorGroups = useMemo(() => {
-        if (getBehaviorGroups.payload?.status === 200) {
-            return getBehaviorGroups.payload.value;
-        }
-
-        return undefined;
-    }, [ getBehaviorGroups.payload?.status, getBehaviorGroups.payload?.value ]);
-
-    const options = React.useMemo(() => {
-        return listSystemBehaviorGroups?.map(b => (<FormSelectOption key={ b.id } label={ b.displayName } value={ b.id } />));
-    }, [ listSystemBehaviorGroups ]);
 
     return (
         <React.Fragment>
@@ -80,16 +60,6 @@ export const CreateEditModal: React.FunctionComponent<CreateEditModalProps> = (p
                             id='display-name'
                             name="displayName"
                         /></FormGroup>
-                    <FormGroup label='System Behavior Group' fieldId='system behavior group'>
-                        <FormSelect
-                            value={ systemBehaviorGroup?.actions }
-                            open={ props.isOpen }
-                            onChange={ handleSelect }
-                            id='system behavior group'
-                            name='system behavior group'
-                        >
-                            { options }
-                        </FormSelect></FormGroup>
                     <FormGroup label='Description' fieldId='description'
                         helperText='Optional short description that appears in the UI
                                                 to help admin decide how to notify users.'>
