e443fdfe42ed0fa7f7461fe40e96b92707cd51eb
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2022-06-28 15:56:10 +0200

    Prevent error if an endpoint is gone when the history is written (#1310)

--- engine/src/main/java/com/redhat/cloud/notifications/db/repositories/NotificationHistoryRepository.java
+++ engine/src/main/java/com/redhat/cloud/notifications/db/repositories/NotificationHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.redhat.cloud.notifications.db.repositories;
 
 import com.redhat.cloud.notifications.db.StatelessSessionFactory;
+import com.redhat.cloud.notifications.db.converters.EndpointTypeConverter;
+import com.redhat.cloud.notifications.db.converters.NotificationHistoryDetailsConverter;
 import com.redhat.cloud.notifications.models.Endpoint;
 import com.redhat.cloud.notifications.models.NotificationHistory;
 
@@ -17,10 +19,28 @@ public class NotificationHistoryRepository {
     @Inject
     StatelessSessionFactory statelessSessionFactory;
 
-    public NotificationHistory createNotificationHistory(NotificationHistory history) {
-        history.prePersist(); // This method must be called manually while using a StatelessSession.
-        statelessSessionFactory.getCurrentSession().insert(history);
-        return history;
+    @Transactional
+    public void createNotificationHistory(NotificationHistory history) {
+        /*
+         * The following query contains a subquery that retrieves the ID of the endpoint if it still exists. There's no
+         * guarantee the endpoint will still exist in the DB at the time when the history is written. If it's gone, then
+         * the subquery will return null.
+         */
+        String hql = "INSERT INTO notification_history (id, invocation_time, invocation_result, details, event_id, endpoint_type, endpoint_sub_type, created, endpoint_id) " +
+                "VALUES (:id, :invocationTime, :invocationResult, :details, :eventId, :endpointType, :endpointSubType, :created, " +
+                "(SELECT id FROM endpoints WHERE id = :endpointId))";
+        history.prePersist();
+        statelessSessionFactory.getCurrentSession().createNativeQuery(hql)
+                .setParameter("id", history.getId())
+                .setParameter("invocationTime", history.getInvocationTime())
+                .setParameter("invocationResult", history.isInvocationResult())
+                .setParameter("details", new NotificationHistoryDetailsConverter().convertToDatabaseColumn(history.getDetails()))
+                .setParameter("eventId", history.getEvent().getId())
+                .setParameter("endpointType", new EndpointTypeConverter().convertToDatabaseColumn(history.getEndpointType()))
+                .setParameter("endpointSubType", history.getEndpointSubType())
+                .setParameter("created", history.getCreated())
+                .setParameter("endpointId", history.getEndpoint().getId())
+                .executeUpdate();
     }
 
     /**
