b54e3f202ad1fb394da32d034e488dcea452531b
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-06-30 08:16:19 -0600

    Adding a few more annotations to the response

--- backend/src/main/java/com/redhat/cloud/notifications/routers/models/behaviorgroup/CreateBehaviorGroupResponse.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/models/behaviorgroup/CreateBehaviorGroupResponse.java
@@ -3,6 +3,7 @@ package com.redhat.cloud.notifications.routers.models.behaviorgroup;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -17,41 +18,48 @@ public class CreateBehaviorGroupResponse {
     /**
      * Id of the newly created behavior group.
      */
+    @NotNull
     public UUID id;
 
     /**
      * Account id the behavior group belongs to.
      */
     @Deprecated
+    @NotNull
     public String accountId;
 
     /**
      * Org id the behavior group belongs to.
      */
-    public String orgId;
+    public String orgId; // Should add @NotNull when have fully migrated to org id.
 
     /**
      * Bundle if of the newly created behavior group.
      */
+    @NotNull
     public UUID bundleId;
 
     /**
      * Display name of the newly created behavior group.
      */
+    @NotNull
     public String displayName;
 
     /**
      * Endpoints linked to the newly created behavior group.
      */
+    @NotNull
     public List<UUID> endpoints;
 
     /**
      * Event types linked to the newly created behavior group.
      */
+    @NotNull
     public Set<UUID> eventTypes;
 
     /**
      * Creation time of the behavior group
      */
+    @NotNull
     public LocalDateTime created;
 }
