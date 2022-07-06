9d9bd2aaa4ffa0eefd3085ce02511f554f7f1acd
Gwenneg Lepage <glepage@redhat.com>
2022-07-06 18:25:49 +0200

    RHCLOUD-20150 Connect to managed Kafka using Clowder

--- engine/src/main/java/com/redhat/cloud/notifications/clowder/KafkaSaslInitializer.java
+++ engine/src/main/java/com/redhat/cloud/notifications/clowder/KafkaSaslInitializer.java
@@ -8,7 +8,6 @@ import org.eclipse.microprofile.config.ConfigProvider;
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import java.util.Optional;
 
 import static javax.interceptor.Interceptor.Priority.PLATFORM_BEFORE;
 
@@ -24,29 +23,53 @@ public class KafkaSaslInitializer {
     private static final String KAFKA_SECURITY_PROTOCOL = "kafka.security.protocol";
     private static final String KAFKA_SSL_TRUSTSTORE_LOCATION = "kafka.ssl.truststore.location";
     private static final String KAFKA_SSL_TRUSTSTORE_TYPE = "kafka.ssl.truststore.type";
+    private static final String PLAIN = "PLAIN";
+    private static final String SASL_SSL = "SASL_SSL";
+    private static final String SCRAM_SHA_512 = "SCRAM-SHA-512";
 
     void init(@Observes @Priority(PLATFORM_BEFORE) StartupEvent event) {
         Config config = ConfigProvider.getConfig();
-        Optional<String> kafkaSaslJaasConfig = config.getOptionalValue(KAFKA_SASL_JAAS_CONFIG, String.class);
-        Optional<String> kafkaSaslMechanism = config.getOptionalValue(KAFKA_SASL_MECHANISM, String.class);
-        Optional<String> kafkaSecurityProtocol = config.getOptionalValue(KAFKA_SECURITY_PROTOCOL, String.class);
-        Optional<String> kafkaSslTruststoreLocation = config.getOptionalValue(KAFKA_SSL_TRUSTSTORE_LOCATION, String.class);
-        Optional<String> kafkaSslTruststoreType = config.getOptionalValue(KAFKA_SSL_TRUSTSTORE_TYPE, String.class);
-
-        if (kafkaSaslJaasConfig.isPresent() || kafkaSaslMechanism.isPresent() || kafkaSecurityProtocol.isPresent() || kafkaSslTruststoreLocation.isPresent() || kafkaSslTruststoreType.isPresent()) {
-            Log.info("Initializing Kafka SASL configuration...");
-            setValue(KAFKA_SASL_JAAS_CONFIG, kafkaSaslJaasConfig);
-            setValue(KAFKA_SASL_MECHANISM, kafkaSaslMechanism);
-            setValue(KAFKA_SECURITY_PROTOCOL, kafkaSecurityProtocol);
-            setValue(KAFKA_SSL_TRUSTSTORE_LOCATION, kafkaSslTruststoreLocation);
-            setValue(KAFKA_SSL_TRUSTSTORE_TYPE, kafkaSslTruststoreType);
-        }
+        config.getOptionalValue(KAFKA_SECURITY_PROTOCOL, String.class).ifPresent(securityProtocol -> {
+            switch (securityProtocol) {
+                case SASL_SSL:
+                    Log.info("Initializing Kafka SASL configuration...");
+                    String saslMechanism = config.getValue(KAFKA_SASL_MECHANISM, String.class);
+                    String saslJaasConfig = config.getValue(KAFKA_SASL_JAAS_CONFIG, String.class);
+                    switch (saslMechanism) {
+                        case PLAIN:
+                            configurePlainAuthentication(securityProtocol, saslMechanism, saslJaasConfig);
+                            break;
+                        case SCRAM_SHA_512:
+                            String truststoreLocation = config.getValue(KAFKA_SSL_TRUSTSTORE_LOCATION, String.class);
+                            String truststoreType = config.getValue(KAFKA_SSL_TRUSTSTORE_TYPE, String.class);
+                            configureScramAuthentication(securityProtocol, saslMechanism, saslJaasConfig, truststoreLocation, truststoreType);
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected Kafka SASL mechanism: " + saslMechanism);
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected Kafka security protocol: " + securityProtocol);
+            }
+        });
     }
 
-    private static void setValue(String configKey, Optional<String> configValue) {
-        configValue.ifPresent(value -> {
-            System.setProperty(configKey, value);
-            Log.infof("%s has been set", configKey);
-        });
+    private static void configurePlainAuthentication(String securityProtocol, String saslMechanism, String saslJaasConfig) {
+        setValue(KAFKA_SECURITY_PROTOCOL, securityProtocol);
+        setValue(KAFKA_SASL_MECHANISM, saslMechanism);
+        setValue(KAFKA_SASL_JAAS_CONFIG, saslJaasConfig);
+    }
+
+    private static void configureScramAuthentication(String securityProtocol, String saslMechanism, String saslJaasConfig, String truststoreLocation, String truststoreType) {
+        setValue(KAFKA_SECURITY_PROTOCOL, securityProtocol);
+        setValue(KAFKA_SASL_MECHANISM, saslMechanism);
+        setValue(KAFKA_SASL_JAAS_CONFIG, saslJaasConfig);
+        setValue(KAFKA_SSL_TRUSTSTORE_LOCATION, truststoreLocation);
+        setValue(KAFKA_SSL_TRUSTSTORE_TYPE, truststoreType);
+    }
+
+    private static void setValue(String configKey, String configValue) {
+        System.setProperty(configKey, configValue);
+        Log.infof("%s has been set", configKey);
     }
 }
