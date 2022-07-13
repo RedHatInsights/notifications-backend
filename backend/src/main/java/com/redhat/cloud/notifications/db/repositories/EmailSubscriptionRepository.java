2bcfa3ab73fb64d98e5d1f1d40d2f1eee9307697
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2022-07-13 10:07:08 +0200

    NOTIF-603 Remove duplicate placeholder email subscriptions (#1364)

--- backend/src/main/java/com/redhat/cloud/notifications/db/repositories/EmailSubscriptionRepository.java
+++ backend/src/main/java/com/redhat/cloud/notifications/db/repositories/EmailSubscriptionRepository.java
@@ -22,10 +22,12 @@ public class EmailSubscriptionRepository {
 
     @Transactional
     public boolean subscribe(String accountNumber, String orgId, String username, String bundleName, String applicationName, EmailSubscriptionType subscriptionType) {
+        // TODO NOTIF-603 After the migration, change the ON CONFLICT clause to:
+        // ON CONFLICT (account_id, org_id, user_id, application_id, subscription_type) DO NOTHING
         String query = "INSERT INTO endpoint_email_subscriptions(account_id, org_id, user_id, application_id, subscription_type) " +
                 "SELECT :accountId, :orgId, :userId, a.id, :subscriptionType " +
                 "FROM applications a, bundles b WHERE a.bundle_id = b.id AND a.name = :applicationName AND b.name = :bundleName " +
-                "ON CONFLICT (account_id, org_id, user_id, application_id, subscription_type) DO NOTHING"; // The value is already on the database, this is OK
+                "ON CONFLICT (account_id, user_id, application_id, subscription_type) DO UPDATE SET org_id = :orgId";
         // HQL does not support the ON CONFLICT clause so we need a native query here
         entityManager.createNativeQuery(query)
                 .setParameter("accountId", accountNumber)
