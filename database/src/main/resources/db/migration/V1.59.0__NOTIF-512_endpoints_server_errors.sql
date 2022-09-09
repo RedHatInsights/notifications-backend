abde0bc15d5012ac8c1e058d52e1d3e8e13a4313
Gwenneg Lepage <glepage@redhat.com>
2022-09-09 14:59:22 +0200

    Remove UPDATE query from DB migration

--- database/src/main/resources/db/migration/V1.59.0__NOTIF-512_endpoints_server_errors.sql
+++ database/src/main/resources/db/migration/V1.59.0__NOTIF-512_endpoints_server_errors.sql
@@ -1,6 +1,2 @@
--- This only affects future inserted rows but not the existing ones.
 ALTER TABLE endpoints
     ADD COLUMN server_errors INTEGER NOT NULL DEFAULT 0;
-
--- This updates existing rows.
-UPDATE endpoints SET server_errors = 0;
