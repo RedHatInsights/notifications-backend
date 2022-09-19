cbc98bfc505c26f76b17dd2eb041142d6809baee
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2022-09-19 12:59:26 +0530

    RHIROS-803 Add daily email template for ROS (#1502)

---
+++ engine/src/main/java/com/redhat/cloud/notifications/templates/ResourceOptimizationService.java
@@ -0,0 +1,40 @@
+package com.redhat.cloud.notifications.templates;
+
+import com.redhat.cloud.notifications.models.EmailSubscriptionType;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+// Name needs to be "ResourceOptimizationService" to read templates from resources/templates/ResourceOptimizationService
+public class ResourceOptimizationService implements EmailTemplate {
+
+    @Override
+    public TemplateInstance getTitle(String eventType, EmailSubscriptionType type) {
+
+        return Templates.dailyEmailTitle();
+    }
+
+    @Override
+    public TemplateInstance getBody(String eventType, EmailSubscriptionType type) {
+
+        return Templates.dailyEmailBody();
+    }
+
+    @Override
+    public boolean isSupported(String eventType, EmailSubscriptionType type) {
+        return type == EmailSubscriptionType.DAILY;
+    }
+
+    @Override
+    public boolean isEmailSubscriptionSupported(EmailSubscriptionType type) {
+        return type == EmailSubscriptionType.DAILY;
+    }
+
+    @CheckedTemplate(requireTypeSafeExpressions = false)
+    public static class Templates {
+
+        public static native TemplateInstance dailyEmailTitle();
+
+        public static native TemplateInstance dailyEmailBody();
+    }
+
+}
