bf5c80c5ede82aaeb73a0b2ac3b389e2e485b0a0
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-10-12 11:03:48 -0600

    Fix daily email for inventory

--- engine/src/test/java/com/redhat/cloud/notifications/templates/TestInventoryTemplate.java
+++ engine/src/test/java/com/redhat/cloud/notifications/templates/TestInventoryTemplate.java
@@ -2,10 +2,15 @@ package com.redhat.cloud.notifications.templates;
 
 import com.redhat.cloud.notifications.InventoryTestHelpers;
 import com.redhat.cloud.notifications.ingress.Action;
+import com.redhat.cloud.notifications.processors.email.aggregators.InventoryEmailAggregator;
 import com.redhat.cloud.notifications.templates.models.Environment;
 import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -41,5 +46,25 @@ public class TestInventoryTemplate {
 
         // Event name
         assertTrue(result.contains("FooEvent"), "Body should contain the event_name");
+    }
+
+    @Test
+    public void testDailyEmailBody() {
+        InventoryEmailAggregator aggregator = new InventoryEmailAggregator();
+        aggregator.aggregate(InventoryTestHelpers.createEmailAggregation("tenant", "rhel", "inventory", "test event"));
+
+        String result = Inventory.Templates.dailyEmailBody()
+                .data("action", Map.of(
+                        "context", aggregator.getContext(),
+                        "timestamp", LocalDateTime.now()
+                ))
+                .data("environment", environment)
+                .render();
+
+        JsonObject context = new JsonObject(aggregator.getContext());
+        assertTrue(context.getJsonObject("inventory").getJsonArray("errors").size() < 10);
+        assertTrue(result.contains("Daily Inventory Summary"), "Body should contain 'Daily Inventory Summary'");
+        assertTrue(result.contains("Host Name"), "Body should contain 'Host Name' header");
+        assertTrue(result.contains("Error"), "Body should contain 'Error' header");
     }
 }
