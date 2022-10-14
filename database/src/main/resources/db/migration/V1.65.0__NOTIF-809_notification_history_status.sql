56af84e2754d58da20abee037f020444500b8daf
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-10-14 08:53:47 -0600

    Update database/src/main/resources/db/migration/V1.65.0__NOTIF-809_notification_history_status.sql
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- database/src/main/resources/db/migration/V1.65.0__NOTIF-809_notification_history_status.sql
+++ database/src/main/resources/db/migration/V1.65.0__NOTIF-809_notification_history_status.sql
@@ -2,6 +2,6 @@ ALTER TABLE notification_history
     ADD COLUMN status varchar(20);
 
 UPDATE notification_history SET status = CASE
-    WHEN invocation_result = true THEN 'SUCCESS'
+    WHEN invocation_result IS true THEN 'SUCCESS'
     ELSE 'FAILED_PROCESSING'
     END;
