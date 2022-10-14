56e62e4183278b9b65d7e7babe13f14521ad55e3
MikelAlejoBR <mbarcina@redhat.com>
2022-10-14 11:03:13 +0200

    refactor: ignore incoming unknown properties for safety
    This will prevent us from having errors if Sources sends a payload with
    properties we haven't got defined in our class.
    
    RHCLOUD-22065

--- backend/src/main/java/com/redhat/cloud/notifications/routers/sources/Secret.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/sources/Secret.java
@@ -1,6 +1,7 @@
 package com.redhat.cloud.notifications.routers.sources;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * {@code NULL}, since Sources is very strict with non-existing keys in the payload: it will return a bad request for
  * those requests.</p>
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public final class Secret {
 
     public static final String TYPE_BASIC_AUTH = "notifications-basic-authentication";
