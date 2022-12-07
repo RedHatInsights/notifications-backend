58b06c8374b10fb8658f2cb62a00b4eba2db7791
Paul Wayper <paulway@redhat.com>
2022-12-07 16:33:13 +1100

    Initial write-up of Advisor daily digest notification aggregator
    Signed-off-by: Paul Wayper <paulway@redhat.com>

---
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AdvisorEmailAggregator.java
@@ -0,0 +1,85 @@
+package com.redhat.cloud.notifications.processors.email.aggregators;
+
+import com.redhat.cloud.notifications.models.EmailAggregation;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Set;
+
+public class AdvisorDailyDigestEmailPayloadAggregator extends AbstractEmailPayloadAggregator {
+
+    // Notification common
+    private static final String EVENT_TYPE_KEY = "event_type";
+    private static final String EVENTS_KEY = "events";
+    private static final String PAYLOAD_KEY = "payload";
+    private static final String INVENTORY_ID = "inventory_id";
+    private static final String PAYLOAD_RULE_ID = "rule_id";
+
+    // Advisor event payloads
+    private static final String NEW_RECOMMENDATION = "new_recommendation";
+    private static final String RESOLVED_RECOMMENDATION = "resolved_recommendation";
+    private static final String DEACTIVATED_RECOMMENDATION = "resolved_recommendation";
+
+    // Advisor events aggregator
+    private static final String ADVISOR_KEY = "advisor";
+    private static final String NEW_RECOMMENDATIONS = "new_recommendations";
+    private static final String RESOLVED_RECOMMENDATIONS = "resolved_recommendations";
+    private static final String DEACTIVATED_RECOMMENDATIONS = "deactivated_recommendations";
+
+    // Advisor event types
+    private static final String NEW_RECOMMENDATION_EVENT = "new-recommendation";
+    private static final String RESOLVED_RECOMMENDATION_EVENT = "resolved-recommendation";
+    private static final String DEACTIVATED_RULE_EVENT = "deactivated-rule";
+    private static final Set<String> EVENT_TYPES = new HashSet<>(Arrays.asList(
+        NEW_RECOMMENDATION_EVENT, RESOLVED_RECOMMENDATION_EVENT,
+        DEACTIVATED_RULE_EVENT
+    ));
+
+    // Advisor final payload helpers
+    private final HashMap<String, HashSet<String>> newRecommendations = new HashMap<String, HashSet<>>();
+    private final HashMap<String, HashSet<String>> resolvedRecommendations = new HashSet<String, HashSet<>>();
+    private final HashSet<String> deactivatedRecommendations = new HashSet<>();
+
+    public AdvisorDailyDigestEmailPayloadAggregator() {
+        JsonObject advisor = new JsonObject();
+
+        context.put(ADVISOR_KEY, advisor);
+    }
+
+    @Override
+    void processEmailAggregation(EmailAggregation notification) {
+        JsonObject notifPayload = notification.getPayload();
+        JsonObject advisor = context.getJsonObject(ADVISOR_KEY);
+        String eventType = notifPayload.getString(EVENT_TYPE_KEY);
+        String inventoryId = notification.getContext().getString(INVENTORY_ID);
+
+        if (!EVENT_TYPES.contains(eventType)) {
+            return;
+        }
+
+        notifPayload.getJsonArray(EVENTS_KEY).stream().forEach(eventObject -> {
+            JsonObject event = (JsonObject) eventObject;
+            JsonObject payload = event.getJsonObject(PAYLOAD_KEY);
+            String ruleId = payload.getString(PAYLOAD_RULE_ID);
+
+            if (eventType == NEW_RECOMMENDATION) {
+                if (! newRecommendations.contains(ruleId)) {
+                    newRecommendations.put(ruleId, new HashSet<String>());
+                }
+                newRecommendations.get(ruleId).add(inventoryId);
+            } else if (eventType == RESOLVED_RECOMMENDATION) {
+                if (! resolvedRecommendations.contains(ruleId)) {
+                    resolvedRecommendations.put(ruleId, new HashSet<String>());
+                }
+                resolvedRecommendations.get(ruleId).add(inventoryId);
+            } else if (eventType == DEACTIVATED_RECOMMENDATION) {
+                deactivatedRecommendations.add(ruleId);
+            }
+        });
+        advisor.put(NEW_RECOMMENDATIONS, newRecommendations);
+        advisor.put(RESOLVED_RECOMMENDATIONS, newRecommendations);
+        advisor.put(DEACTIVATED_RECOMMENDATIONS, newRecommendations);
+    }
+}
