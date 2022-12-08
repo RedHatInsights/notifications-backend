5bf8c5307e5c286497843f02e15ff45a99792870
Paul Wayper <paulway@redhat.com>
2022-12-08 15:19:13 +1100

    Add a lot more information to each rule
    The mock-up of the output lists the description, risk, incident flag, and
    number of affected systems for each rule.  We need to pull this from the
    rule information when we create a new entry in the map for the new,
    resolved and deactivated recommendations.
    
    We're also going to simplify this by making a major assumption.
    
    Let us assume that in general a system will only have a new or resolved
    event for each recommendation once per day.  Systems upload once per
    day in general, and each upload can never report the same rule as new or
    resolved twice (because each rule impacts a system only once).  The only
    situation in which this could be false would be for a system to upload
    at least three times - once to report a new rule, then to resolve it,
    and finally to report the same rule as new again.  That's got to be
    pretty rare, right?
    
    With that assumption, we do not have to remember the set of Inventory
    IDs reporting each rule as new or resolved; we just have to increment
    a count each time a new event is seen.
    
    Signed-off-by: Paul Wayper <paulway@redhat.com>

--- engine/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AdvisorEmailAggregator.java
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/email/aggregators/AdvisorEmailAggregator.java
@@ -10,6 +10,41 @@ import java.util.Map;
 import java.util.Set;
 
 
+/*
+ * Events are of the form:
+ * {
+   "bundle":"rhel",
+   "application":"advisor",
+   "event_type":"new-recommendation",
+   "timestamp":"2022-11-30T17:30:20.773398",
+   "account_id":"5758117",
+   "org_id":"7806094",
+   "context":{
+      "inventory_id":"6ad30f3e-0497-4e74-99f1-b3f9a6120a6f",
+      "hostname":"my-computer-jmartine",
+      "display_name":"my-computer-jmartine",
+      "rhel_version":"8.4",
+      "host_url":"https://console.redhat.com/insights/inventory/6ad30f3e-0497-4e74-99f1-b3f9a6120a6f",
+      "tags":[]
+   },
+   "events":[
+      {
+         "metadata":{},
+         "payload":{
+            "rule_id":"insights_core_egg_not_up2date|INSIGHTS_CORE_EGG_NOT_UP2DATE",
+            "rule_description":"System is not able to get the latest recommendations and may miss bug fixes when the Insights Client Core egg file is outdated",
+            "total_risk":"2",
+            "publish_date":"2021-03-13T18:44:00+00:00",
+            "rule_url":"https://console.redhat.com/insights/advisor/recommendations/insights_core_egg_not_up2date|INSIGHTS_CORE_EGG_NOT_UP2DATE/",
+            "reboot_required":false,
+            "has_incident":false,
+            "report_url":"https://console.redhat.com/insights/advisor/recommendations/insights_core_egg_not_up2date|INSIGHTS_CORE_EGG_NOT_UP2DATE/6ad30f3e-0497-4e74-99f1-b3f9a6120a6f"
+         }
+      }
+   ]
+}
+ */
+
 public class AdvisorEmailPayloadAggregator extends AbstractEmailPayloadAggregator {
 
     // Notification common
@@ -18,17 +53,26 @@ public class AdvisorEmailPayloadAggregator extends AbstractEmailPayloadAggregato
     private static final String PAYLOAD_KEY = "payload";
     private static final String INVENTORY_ID = "inventory_id";
     private static final String PAYLOAD_RULE_ID = "rule_id";
+    private static final String PAYLOAD_RULE_DESCRIPTION = "rule_description";
+    private static final String PAYLOAD_RULE_TOTAL_RISK = "total_risk";
+    private static final String PAYLOAD_RULE_HAS_INCIDENT = "has_incident";
+    private static final String PAYLOAD_RULE_URL = "rule_url";
 
     // Advisor event payloads
     private static final String NEW_RECOMMENDATION = "new_recommendation";
     private static final String RESOLVED_RECOMMENDATION = "resolved_recommendation";
     private static final String DEACTIVATED_RECOMMENDATION = "resolved_recommendation";
 
-    // Advisor events aggregator
+    // Advisor events aggregator data contents
     private static final String ADVISOR_KEY = "advisor";
     private static final String NEW_RECOMMENDATIONS = "new_recommendations";
     private static final String RESOLVED_RECOMMENDATIONS = "resolved_recommendations";
     private static final String DEACTIVATED_RECOMMENDATIONS = "deactivated_recommendations";
+    private static final String CONTENT_RULE_DESCRIPTION = "description";
+    private static final String CONTENT_RULE_TOTAL_RISK = "total_risk";
+    private static final String CONTENT_RULE_HAS_INCIDENT = "has_incident";
+    private static final String CONTENT_RULE_URL = "url";
+    private static final String CONTENT_SYSTEM_COUNT = "systems";
 
     // Advisor event types
     private static final String NEW_RECOMMENDATION_EVENT = "new-recommendation";
@@ -40,9 +84,9 @@ public class AdvisorEmailPayloadAggregator extends AbstractEmailPayloadAggregato
     ));
 
     // Advisor final payload helpers
-    private final Map<String, Set<String>> newRecommendations = new HashMap<>();
-    private final Map<String, Set<String>> resolvedRecommendations = new HashMap<>();
-    private final Set<String> deactivatedRecommendations = new HashSet<>();
+    private final Map<String, Map<String, Object>> newRecommendations = new HashMap<>();
+    private final Map<String, Map<String, Object>> resolvedRecommendations = new HashMap<>();
+    private final Map<String, Map<String, String>> deactivatedRecommendations = new HashMap<>();
 
     public AdvisorEmailPayloadAggregator() {
         JsonObject advisor = new JsonObject();
@@ -58,7 +102,13 @@ public class AdvisorEmailPayloadAggregator extends AbstractEmailPayloadAggregato
         JsonObject notifPayload = notification.getPayload();
         JsonObject advisor = context.getJsonObject(ADVISOR_KEY);
         String eventType = notifPayload.getString(EVENT_TYPE_KEY);
-        String inventoryId = notifPayload.getJsonObject("context").getString(INVENTORY_ID);
+        /* Major assumption: that there is at most one event per day from each
+         * rule,system pair - i.e. that the tuple (day, rule, system) is
+         * unique for new, and resolved, events.  This is generally true,
+         * because a rule would have to be new twice or resolved twice - i.e.
+         * new, then resolved, then occur again - on the same system in order
+         * for this to not be wrong.  This means we don't need to remember the
+         * inventory ID of each system impacted by or resolving a rule. */
 
         if (!EVENT_TYPES.contains(eventType)) {
             return;
@@ -68,18 +118,43 @@ public class AdvisorEmailPayloadAggregator extends AbstractEmailPayloadAggregato
             JsonObject event = (JsonObject) eventObject;
             JsonObject payload = event.getJsonObject(PAYLOAD_KEY);
             String ruleId = payload.getString(PAYLOAD_RULE_ID);
+            String ruleDescription = payload.getString(PAYLOAD_RULE_DESCRIPTION);
+            String ruleRisk = payload.getString(PAYLOAD_RULE_TOTAL_RISK);
+            String ruleIncident = payload.getString(PAYLOAD_RULE_HAS_INCIDENT);
+            String ruleURL = payload.getString(PAYLOAD_RULE_URL);
 
             switch (eventType) {
-                case COMPLIANCE:
-                    newRecommendations.computeIfAbsent(
-                        ruleId, key -> new HashSet<String>()
-                    ).add(inventoryId);
+                case NEW_RECOMMENDATION:
+                    systemCount = newRecommendations.computeIfAbsent(
+                        ruleId, key -> new HashMap<>(Map.of(
+                            CONTENT_RULE_DESCRIPTION, ruleDescription,
+                            CONTENT_RULE_HAS_INCIDENT, ruleIncident,
+                            CONTENT_RULE_TOTAL_RISK, ruleRisk,
+                            CONTENT_RULE_URL, ruleURL,
+                            CONTENT_SYSTEM_COUNT, 0
+                        ))
+                    ).get(CONTENT_SYSTEM_COUNT);
+                    newRecommendations.set(CONTENT_SYSTEM_COUNT, systemCount + 1);
                 case RESOLVED_RECOMMENDATION:
-                    resolvedRecommendations.computeIfAbsent(
-                        ruleId, key -> new HashSet<String>()
-                    ).add(inventoryId);
+                    systemCount = resolvedRecommendations.computeIfAbsent(
+                        ruleId, key -> new HashMap<>(Map.of(
+                            CONTENT_RULE_DESCRIPTION, ruleDescription,
+                            CONTENT_RULE_HAS_INCIDENT, ruleIncident,
+                            CONTENT_RULE_TOTAL_RISK, ruleRisk,
+                            CONTENT_RULE_URL, ruleURL,
+                            CONTENT_SYSTEM_COUNT, 0
+                        ))
+                    ).get(CONTENT_SYSTEM_COUNT);
+                    resolvedRecommendations.set(CONTENT_SYSTEM_COUNT, systemCount + 1);
                 case DEACTIVATED_RECOMMENDATION:
-                    deactivatedRecommendations.add(ruleId);
+                    deactivatedRecommendations.computeIfAbsent(
+                        ruleId, key -> new HashMap<>(Map.of(
+                            CONTENT_RULE_DESCRIPTION, ruleDescription,
+                            CONTENT_RULE_HAS_INCIDENT, ruleIncident,
+                            CONTENT_RULE_TOTAL_RISK, ruleRisk,
+                            CONTENT_RULE_URL, ruleURL
+                        ))
+                    );
                 default:
                     break;
             }
