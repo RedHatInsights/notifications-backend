1498ce5acfc602d0c064a03608a468d9caf10b90
Gwenneg Lepage <glepage@redhat.com>
2022-12-12 15:05:50 +0100

    Add missing conversion strategy

--- database/src/main/resources/db/migration/V1.70.0__RHCLOUD-22943_no_more_endpoint_type_ordinal.sql
+++ database/src/main/resources/db/migration/V1.70.0__RHCLOUD-22943_no_more_endpoint_type_ordinal.sql
@@ -9,8 +9,15 @@ ALTER TABLE notification_history
 -- TODO Remove this function after the migration is done.
 CREATE FUNCTION transition_to_string_endpoint_type() RETURNS TRIGGER AS $$
 BEGIN
-    -- The endpoint type will be copied into the new column every time a row is inserted or updated.
-    NEW.endpoint_type_v2 := NEW.endpoint_type;
+    IF (NEW.endpoint_type = 0) THEN
+        NEW.endpoint_type_v2 := 'WEBHOOK';
+    ELSIF (NEW.endpoint_type = 1) THEN
+        NEW.endpoint_type_v2 := 'EMAIL_SUBSCRIPTION';
+    ELSIF (NEW.endpoint_type = 2) THEN
+        NEW.endpoint_type_v2 := 'DEFAULT';
+    ELSIF (NEW.endpoint_type = 3) THEN
+        NEW.endpoint_type_v2 := 'CAMEL';
+    END IF;
     RETURN NEW;
 END
 $$ LANGUAGE PLPGSQL;
@@ -26,8 +33,22 @@ CREATE TRIGGER notification_history_string_endpoint_type
     FOR EACH ROW EXECUTE PROCEDURE transition_to_string_endpoint_type();
 
 -- Updates all existing rows.
-UPDATE endpoints SET endpoint_type_v2 = endpoint_type;
-UPDATE notification_history SET endpoint_type_v2 = endpoint_type;
+
+UPDATE endpoints SET endpoint_type_v2 =
+    CASE
+        WHEN endpoint_type = 0 THEN 'WEBHOOK'
+        WHEN endpoint_type = 1 THEN 'EMAIL_SUBSCRIPTION'
+        WHEN endpoint_type = 2 THEN 'DEFAULT'
+        WHEN endpoint_type = 3 THEN 'CAMEL'
+    END;
+
+UPDATE notification_history SET endpoint_type_v2 =
+    CASE
+        WHEN endpoint_type = 0 THEN 'WEBHOOK'
+        WHEN endpoint_type = 1 THEN 'EMAIL_SUBSCRIPTION'
+        WHEN endpoint_type = 2 THEN 'DEFAULT'
+        WHEN endpoint_type = 3 THEN 'CAMEL'
+    END;
 
 ALTER TABLE endpoints
     ALTER COLUMN endpoint_type_v2 SET NOT NULL,
