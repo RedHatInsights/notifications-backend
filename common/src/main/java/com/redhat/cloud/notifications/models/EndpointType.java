06aee403fc80b7c06e304d9557f20a4d5e3333fa
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2022-12-13 18:37:51 +0100

    RHCLOUD-22943 Migrate from endpoints ordinal to name in DB (step 3) (#1645)

--- common/src/main/java/com/redhat/cloud/notifications/models/EndpointType.java
+++ common/src/main/java/com/redhat/cloud/notifications/models/EndpointType.java
@@ -3,19 +3,12 @@ package com.redhat.cloud.notifications.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-@Schema(enumeration = { "webhook", "email_subscription", "default", "camel" })
+@Schema(enumeration = { "webhook", "email_subscription", "camel" })
 public enum EndpointType {
     @JsonProperty("webhook")
     WEBHOOK(false),
     @JsonProperty("email_subscription")
     EMAIL_SUBSCRIPTION(false),
-    /**
-     * This enum member is no longer used.
-     * @deprecated Since the behavior groups migration.
-     */
-    @JsonProperty("default")
-    @Deprecated
-    DEFAULT(false),
     @JsonProperty("camel")
     CAMEL(true);
 
