e9e3de9430de02245c0bc49c0a6d0768a783b170
Gwenneg Lepage <glepage@redhat.com>
2023-01-03 15:21:27 +0100

    Bump Quarkus to 2.15.1.Final

--- backend/src/main/java/com/redhat/cloud/notifications/routers/JsonParseExceptionMapper.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/JsonParseExceptionMapper.java
@@ -8,6 +8,13 @@ import javax.ws.rs.ext.Provider;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
+/*
+ * This exception mapper doesn't seem to be used anymore starting Quarkus 2.15.1.Final.
+ * Since that change was not documented by the Quarkus team, let's keep this class around
+ * for a while in case the new behavior would be rolled back in the near future. The old
+ * logic below has been moved to JaxRsExceptionMapper.
+ * TODO Remove this class later, AFTER checking that its method is never called.
+ */
 @Provider
 public class JsonParseExceptionMapper implements ExceptionMapper<JsonParseException> {
 
