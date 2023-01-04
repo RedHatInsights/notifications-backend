7d9cc203dd4e871b4d90aaeaf7f3ba30e541ca0a
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-01-04 13:51:01 +0100

    Bump Quarkus to 2.15.1.Final (#1669)

--- backend/src/main/java/com/redhat/cloud/notifications/routers/JaxRsExceptionMapper.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/JaxRsExceptionMapper.java
@@ -1,10 +1,14 @@
 package com.redhat.cloud.notifications.routers;
 
+import com.fasterxml.jackson.core.JsonParseException;
+
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 /**
  * Map thrown Exceptions to Responses with appropriate status codes
@@ -15,7 +19,12 @@ public class JaxRsExceptionMapper implements ExceptionMapper<WebApplicationExcep
     @Override
     public Response toResponse(WebApplicationException exception) {
         if (exception instanceof BadRequestException) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(exception.getMessage()).build();
+            return Response.status(BAD_REQUEST).entity(exception.getMessage()).build();
+        }
+        // JsonParseException is no longer intercepted directly by exception mappers with Quarkus 2.15.1.Final.
+        if (exception.getCause() != null && exception.getCause() instanceof JsonParseException) {
+            JsonParseException jsonParseException = (JsonParseException) exception.getCause();
+            return Response.status(BAD_REQUEST).entity(jsonParseException.getMessage()).build();
         }
         return Response.status(exception.getResponse().getStatus()).entity(exception.getMessage()).build();
     }
