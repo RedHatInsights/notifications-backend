1f0b34a9029b06f6f7405fece7be312f1fa87e7a
Alexandra Ferdinand <aferdina@redhat.com>
2023-01-04 09:49:38 -0500

    change name back

--- backend/src/main/java/com/redhat/cloud/notifications/routers/models/SettingsValueJsonForm.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/models/SettingsValueJsonForm.java
@@ -32,7 +32,7 @@ public class SettingsValueJsonForm {
         @JsonInclude(Include.NON_NULL)
         public String checkedWarning;
         @JsonInclude(Include.NON_NULL)
-        public String isSubscribed;
+        public String infoMessage;
     }
 
     @JsonAutoDetect(fieldVisibility = Visibility.ANY)
@@ -88,7 +88,7 @@ public class SettingsValueJsonForm {
                             field.label = "Instant notification";
                             field.description = "Immediate email for each triggered application event. See notification settings for configuration.";
                             field.checkedWarning = "Opting into this notification may result in a large number of emails";
-                            field.isSubscribed = "You may still receive forced notifications for this service";
+                            field.infoMessage = "You may still receive forced notifications for this service";
                             break;
                         default:
                             return;
