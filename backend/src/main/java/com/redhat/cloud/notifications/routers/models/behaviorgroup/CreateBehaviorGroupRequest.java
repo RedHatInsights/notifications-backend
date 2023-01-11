3524a2c6e20a9bce3f1bcb2d60f3773ee6a4766c
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2023-01-11 11:28:21 +0100

    refactor: avoid using StringUtils
    Basically we don't want to depend on Apache's package, since it might go
    away soon as its a transitive dependency at the moement.
    
    RHCLOUD-22513

--- backend/src/main/java/com/redhat/cloud/notifications/routers/models/behaviorgroup/CreateBehaviorGroupRequest.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/models/behaviorgroup/CreateBehaviorGroupRequest.java
@@ -3,10 +3,9 @@ package com.redhat.cloud.notifications.routers.models.behaviorgroup;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import org.apache.commons.lang3.StringUtils;
 
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -27,7 +26,7 @@ public class CreateBehaviorGroupRequest {
     /**
      * Display name the behavior group will have.
      */
-    @NotNull
+    @NotBlank
     public String displayName;
 
     /**
@@ -46,8 +45,10 @@ public class CreateBehaviorGroupRequest {
      * Validates that the bundle is identifiable.
      * @return true if the bundle ID is null and the bundle name is blank. False otherwise.
      */
-    @AssertFalse(message = "either the bundle name or the bundle UUID are required")
+    @AssertTrue(message = "either the bundle name or the bundle UUID are required")
     private boolean isBundleUuidNullOrBundleNameBlank() {
-        return this.bundleId == null && StringUtils.isBlank(this.bundleName);
+        return this.bundleId != null || (
+            this.bundleName != null && !this.bundleName.isBlank()
+            );
     }
 }
