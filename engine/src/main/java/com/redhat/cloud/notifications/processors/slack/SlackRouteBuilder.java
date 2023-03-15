26ef0e3692b04d15527c7e643b63b94730790f5e
Gwenneg Lepage <glepage@redhat.com>
2023-03-15 10:11:25 +0100

    RHCLOUD-24718 Introduce Camel retries for Slack notifications

--- engine/src/main/java/com/redhat/cloud/notifications/processors/slack/SlackRouteBuilder.java
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/slack/SlackRouteBuilder.java
@@ -1,5 +1,6 @@
 package com.redhat.cloud.notifications.processors.slack;
 
+import org.apache.camel.CamelExchangeException;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -7,6 +8,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import static com.redhat.cloud.notifications.Constants.API_INTERNAL;
+import static org.apache.camel.LoggingLevel.ERROR;
 
 @ApplicationScoped
 public class SlackRouteBuilder extends RouteBuilder {
@@ -14,6 +16,15 @@ public class SlackRouteBuilder extends RouteBuilder {
     public static final String REST_PATH = API_INTERNAL + "/slack";
 
     private static final String DIRECT_ENDPOINT_NAME = "direct:slack";
+
+    @ConfigProperty(name = "notifications.slack.camel.maximum-redeliveries", defaultValue = "3")
+    int maxRedeliveries;
+
+    @ConfigProperty(name = "notifications.slack.camel.redelivery-delay", defaultValue = "1")
+    long redeliveryDelay;
+
+    @ConfigProperty(name = "notifications.slack.camel.log-retry-attempted", defaultValue = "true")
+    boolean logRetryAttempted;
 
     @ConfigProperty(name = "notifications.slack.camel.max-endpoint-cache-size", defaultValue = "100")
     int maxEndpointCacheSize;
@@ -23,6 +34,12 @@ public class SlackRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() {
+
+        onException(CamelExchangeException.class)
+                .maximumRedeliveries(maxRedeliveries)
+                .redeliveryDelay(redeliveryDelay)
+                .logRetryAttempted(logRetryAttempted)
+                .log(ERROR, "Could not send message to webhookUrl=${header.webhookUrl} channel=${header.channel}: ${exception.message}");
 
         rest(REST_PATH)
                 .post()
