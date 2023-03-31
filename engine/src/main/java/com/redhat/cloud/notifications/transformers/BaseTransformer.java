dba79fcad7647998e842fefdcfc3fcc6c4857dca
Mikel Alejo <mbarcina@redhat.com>
2023-03-31 09:07:15 +0200

    feature: include display names for objects when serializing (#1588)
    The idea is to make the messages more user friendly, by allowing them to
    chose the more friendlier display names, instead of the more schematic
    canonical ones.
    
    RHCLOUD-22667

--- engine/src/main/java/com/redhat/cloud/notifications/transformers/BaseTransformer.java
+++ engine/src/main/java/com/redhat/cloud/notifications/transformers/BaseTransformer.java
@@ -1,6 +1,7 @@
 package com.redhat.cloud.notifications.transformers;
 
 import com.redhat.cloud.notifications.ingress.Action;
+import com.redhat.cloud.notifications.models.Event;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
@@ -16,20 +17,24 @@ public class BaseTransformer {
     public static final String APPLICATION = "application";
     public static final String BUNDLE = "bundle";
     public static final String CONTEXT = "context";
+    public static final String DISPLAY_NAME = "display_name";
     public static final String EVENT_TYPE = "event_type";
     public static final String EVENTS = "events";
     public static final String METADATA = "metadata";
     public static final String ORG_ID = "org_id";
     public static final String PAYLOAD = "payload";
+    public static final String SOURCE = "source";
     public static final String TIMESTAMP = "timestamp";
 
     /**
-     * Transforms the given action into a {@link JsonObject}.
-     * @param action the {@link Action} to transform.
-     * @return a {@link JsonObject} containing the given action.
+     * Transforms the given event into a {@link JsonObject}.
+     * @param event the {@link Event} to transform.
+     * @return a {@link JsonObject} containing the given event.
      */
-    public JsonObject toJsonObject(final Action action) {
-        JsonObject message = new JsonObject();
+    public JsonObject toJsonObject(final Event event) {
+        final JsonObject message = new JsonObject();
+
+        final Action action = event.getAction();
 
         message.put(ACCOUNT_ID, action.getAccountId());
         message.put(APPLICATION, action.getApplication());
@@ -40,9 +45,9 @@ public class BaseTransformer {
             EVENTS,
             new JsonArray(
                 action.getEvents().stream().map(
-                    event -> Map.of(
-                        METADATA, JsonObject.mapFrom(event.getMetadata()),
-                        PAYLOAD, JsonObject.mapFrom(event.getPayload())
+                    ev -> Map.of(
+                        METADATA, JsonObject.mapFrom(ev.getMetadata()),
+                        PAYLOAD, JsonObject.mapFrom(ev.getPayload())
                     )
                 ).collect(Collectors.toList())
             )
@@ -50,7 +55,24 @@ public class BaseTransformer {
         message.put(ORG_ID, action.getOrgId());
         message.put(TIMESTAMP, action.getTimestamp().toString());
 
+        // Include the display names of the different objects.
+        // Since "insights-notifications-schemas-java:0.20".
+        final JsonObject source = new JsonObject();
+
+        final JsonObject sourceAppDisplayName = new JsonObject();
+        sourceAppDisplayName.put(DISPLAY_NAME, event.getApplicationDisplayName());
+        source.put(APPLICATION, sourceAppDisplayName);
+
+        final JsonObject sourceBundleDisplayName = new JsonObject();
+        sourceBundleDisplayName.put(DISPLAY_NAME, event.getBundleDisplayName());
+        source.put(BUNDLE, sourceBundleDisplayName);
+
+        final JsonObject sourceEventTypeDisplayName = new JsonObject();
+        sourceEventTypeDisplayName.put(DISPLAY_NAME, event.getEventTypeDisplayName());
+        source.put(EVENT_TYPE, sourceEventTypeDisplayName);
+
+        message.put(SOURCE, source);
+
         return message;
     }
-
 }
