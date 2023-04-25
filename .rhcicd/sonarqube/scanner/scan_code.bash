67fb300b61b6f94f69ffca584815155fc11f2432
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2023-04-25 12:32:51 +0200

    feature: SonarQube analysis script
    The script is intended to be run in order to analyze the codebase. It
    will run for PR checks, and also every time we merge something to
    master.
    
    The "sonarqube-project.properties" file is removed the SonarQube
    Scanner's Maven plugin doesn't honor it.
    
    RHCLOUD-22207

---
+++ .rhcicd/sonarqube/scanner/scan_code.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Bash safety options:
+#   - e is for exiting immediately when a command exits with a non-zero status.
+#   - u is for treating unset variables as an error when substituting.
+#   - x is for printing all the commands as they're executed.
+#   - o pipefail is for taking into account the exit status of the commands that run on pipelines.
+#
+set -euxo pipefail
+
+#
+# On the master branch there is no need to give the pull request details.
+#
+if [ -n "${GIT_BRANCH:-}" ] && [ "${GIT_BRANCH}" == "master" ]; then
+  ./mvnw sonar:sonar \
+    -Dsonar.host.url="${SONARQUBE_HOST_URL}" \
+    -Dsonar.exclusions="**/*.sql" \
+    -Dsonar.projectKey="com.redhat.console.notifications.backend" \
+    -Dsonar.projectVersion="${COMMIT_SHORT}" \
+    -Dsonar.sourceEncoding="UTF-8" \
+    -Dsonar.token="${SONARQUBE_TOKEN}"
+else
+  ./mvnw sonar:sonar \
+    -Dsonar.host.url="${SONARQUBE_HOST_URL}" \
+    -Dsonar.exclusions="**/*.sql" \
+    -Dsonar.projectKey="com.redhat.console.notifications.backend" \
+    -Dsonar.projectVersion="${COMMIT_SHORT}" \
+    -Dsonar.pullrequest.base="master" \
+    -Dsonar.pullrequest.branch="${GIT_BRANCH}" \
+    -Dsonar.pullrequest.key="${GITHUB_PULL_REQUEST_ID}" \
+    -Dsonar.sourceEncoding="UTF-8" \
+    -Dsonar.token="${SONARQUBE_TOKEN}"
+fi
---
+++ .rhcicd/sonarqube/sonarqube.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# Bash safety options:
+#   - e is for exiting immediately when a command exits with a non-zero status.
+#   - u is for treating unset variables as an error when substituting.
+#   - x is for printing all the commands as they're executed.
+#   - o pipefail is for taking into account the exit status of the commands that run on pipelines.
+#
+set -euxo pipefail
+
+#
+# Get the commit SHA to give the scanner a unique "project version" setting.
+#
+readonly COMMIT_SHORT=$(git rev-parse --short=7 HEAD)
+
+#
+# Build the Docker image.
+#
+docker build \
+  --build-arg rh_it_root_ca_cert_url="${RH_IT_ROOT_CA_CERT_URL}" \
+  --file .rhcicd/sonarqube/Dockerfile \
+  --tag notifications-sonarqube:latest \
+  .
+
+#
+# Should we be running on master, then skip passing the information regarding
+# the pull request.
+#
+if [ -n "${GIT_BRANCH:-}" ] && [ "${GIT_BRANCH}" == "master" ]; then
+  docker run \
+    --env COMMIT_SHORT="${COMMIT_SHORT}" \
+    --env GIT_BRANCH="${GIT_BRANCH}" \
+    --env RH_IT_ROOT_CA_CERT_URL="${RH_IT_ROOT_CA_CERT_URL}" \
+    --env SONARQUBE_HOST_URL="${SONARQUBE_HOST_URL}" \
+    --env SONARQUBE_TOKEN="${SONARQUBE_TOKEN}" \
+    --rm \
+    notifications-sonarqube:latest \
+    bash .rhcicd/sonarqube/scanner/scan_code.bash
+else
+  docker run \
+    --env COMMIT_SHORT="${COMMIT_SHORT}" \
+    --env GIT_BRANCH="${GIT_BRANCH}" \
+    --env GITHUB_PULL_REQUEST_ID="${ghprbPullId}" \
+    --env RH_IT_ROOT_CA_CERT_URL="${RH_IT_ROOT_CA_CERT_URL}" \
+    --env SONARQUBE_HOST_URL="${SONARQUBE_HOST_URL}" \
+    --env SONARQUBE_TOKEN="${SONARQUBE_TOKEN}" \
+    --rm \
+    notifications-sonarqube:latest \
+    bash .rhcicd/sonarqube/scanner/scan_code.bash
+fi
+
+# Need to make a dummy results file to make the Jenkins tests pass.
+mkdir -p artifacts
+cat << EOF > artifacts/junit-dummy.xml
+<testsuite tests="1">
+  <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
