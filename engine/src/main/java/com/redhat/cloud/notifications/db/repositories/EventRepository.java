87e0118a3ca3facf2d4648d0624279b6f45c0a01
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2023-05-04 15:10:04 +0200

    feature: pull events from the database with date filters
    Events will be the first entities that we will export when clients
    request it. The idea is to mimic what a user would see in the "event
    log", so that is why just a subset of the columns and rows are pulled
    from the database, instead of pulling the whole rows.
    
    RHCLOUD-25597

--- engine/src/main/java/com/redhat/cloud/notifications/db/repositories/EventRepository.java
+++ engine/src/main/java/com/redhat/cloud/notifications/db/repositories/EventRepository.java
@@ -2,12 +2,50 @@ package com.redhat.cloud.notifications.db.repositories;
 
 import com.redhat.cloud.notifications.db.StatelessSessionFactory;
 import com.redhat.cloud.notifications.models.Event;
+import org.hibernate.Session;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class EventRepository {
+    /**
+     * The maximum hours value. Taken from
+     * {@link java.time.temporal.ChronoField#HOUR_OF_DAY}
+     */
+    private static final int MAX_HOURS = 23;
+    /**
+     * The maximum minutes value. Taken from
+     * {@link java.time.temporal.ChronoField#MINUTE_OF_HOUR}
+     */
+    private static final int MAX_MINUTES = 59;
+    /**
+     * The maximum seconds value. Taken from
+     * {@link java.time.temporal.ChronoField#SECOND_OF_MINUTE}
+     */
+    private static final int MAX_SECONDS = 59;
+    /**
+     * The maximum nanoseconds value. Taken from
+     * {@link java.time.temporal.ChronoField#NANO_OF_SECOND}
+     */
+    private static final int MAX_NANOSECONDS = 999999999;
+
+    @Inject
+    EntityManager entityManager;
+
+    @Inject
+    Session session;
 
     @Inject
     StatelessSessionFactory statelessSessionFactory;
@@ -16,5 +54,104 @@ public class EventRepository {
         event.prePersist(); // This method must be called manually while using a StatelessSession.
         statelessSessionFactory.getCurrentSession().insert(event);
         return event;
+    }
+
+    /**
+     * Finds the events related to the provided org id in order to export them.
+     * It is the caller's responsibility to provide valid "from" and "to"
+     * filters.
+     * @param orgId the org id the events are related to.
+     * @param from the initial date to filter the dates from.
+     * @param to the final date to filter the dates from.
+     * @return a list of events that comply with the provided filters.
+     */
+    public List<Event> findEventsToExport(final String orgId, final LocalDate from, final LocalDate to) {
+        this.validateFromTo(from, to);
+
+        final CriteriaBuilder criteriaBuilder = this.session.getCriteriaBuilder();
+        final CriteriaQuery<Event> criteria = criteriaBuilder.createQuery(Event.class);
+        final Root<Event> root = criteria.from(Event.class);
+
+        criteria.multiselect(
+            root.get("id"),
+            root.get("bundleDisplayName"),
+            root.get("applicationDisplayName"),
+            root.get("eventTypeDisplayName"),
+            root.get("created")
+        );
+
+        final List<Predicate> predicates = new ArrayList<>(3);
+
+        // Make sure we are grabbing the events from the correct tenant.
+        predicates.add(
+            criteriaBuilder.equal(root.get("orgId"), orgId)
+        );
+
+        if (from != null) {
+            predicates.add(
+                criteriaBuilder.greaterThanOrEqualTo(
+                    root.get("created"),
+                    Date.from(from.atStartOfDay(ZoneId.systemDefault()).toInstant())
+                )
+            );
+        }
+
+        if (to != null) {
+            predicates.add(
+                criteriaBuilder.lessThanOrEqualTo(
+                    root.get("created"),
+                    Date.from(to.atTime(MAX_HOURS, MAX_MINUTES, MAX_SECONDS, MAX_NANOSECONDS).toInstant(ZoneOffset.UTC))
+                )
+            );
+        }
+
+        criteria.where(predicates.toArray(new Predicate[]{}));
+
+        return this.entityManager.createQuery(criteria).getResultList();
+    }
+
+    /**
+     * <p>Validates that the initial and end date of the filters are valid and
+     * throws an exception if they are not. The constraints are:
+     * <ul>
+     *     <li>{@code null} dates are considered valid.</li>
+     *     <li>The initial date can't be older than a month.</li>
+     *     <li>The initial date can't be in the future.</li>
+     *     <li>If both the initial and end date are provided, the initial date
+     *     can't be after the end date.</li>
+     *     <li>The end date can't be older than a month.</li>
+     *     <li>The end date can't be in the future.</li>
+     * </ul></p>
+     *
+     * @param from the initial date of the filter.
+     * @param to the end date of the filter.
+     */
+    protected void validateFromTo(final LocalDate from, final LocalDate to) {
+        final LocalDate today = LocalDate.now();
+        final LocalDate aMonthAgo = today.minusMonths(1);
+
+        if (from != null) {
+            if (from.compareTo(today) > 0) {
+                throw new IllegalStateException("can't fetch events from the future!");
+            }
+
+            if (aMonthAgo.compareTo(from) > 0) {
+                throw new IllegalStateException("events that are older than a month cannot be fetched");
+            }
+
+            if (to != null && to.compareTo(from) < 0) {
+                throw new IllegalStateException("the 'to' date cannot be lower than the 'from' date");
+            }
+        }
+
+        if (to != null) {
+            if (to.compareTo(today) > 0) {
+                throw new IllegalStateException("can't fetch events from the future!");
+            }
+
+            if (aMonthAgo.compareTo(to) > 0) {
+                throw new IllegalStateException("events that are older than a month cannot be fetched");
+            }
+        }
     }
 }
---
+++ engine/src/test/java/com/redhat/cloud/notifications/db/repositories/EventRepositoryTest.java
@@ -0,0 +1,295 @@
+package com.redhat.cloud.notifications.db.repositories;
+
+import com.redhat.cloud.notifications.TestLifecycleManager;
+import com.redhat.cloud.notifications.db.ResourceHelpers;
+import com.redhat.cloud.notifications.models.Application;
+import com.redhat.cloud.notifications.models.Bundle;
+import com.redhat.cloud.notifications.models.Event;
+import com.redhat.cloud.notifications.models.EventType;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.redhat.cloud.notifications.TestConstants.DEFAULT_ORG_ID;
+
+@QuarkusTest
+@QuarkusTestResource(TestLifecycleManager.class)
+public class EventRepositoryTest {
+
+    private final List<Event> createdEvents = new ArrayList<>(5);
+
+    @Inject
+    EntityManager entityManager;
+
+    @Inject
+    EventRepository eventRepository;
+
+    @Inject
+    ResourceHelpers resourceHelpers;
+
+    /**
+     * Inserts five event fixtures in the database. The fixtures then get
+     * their "created at" timestamp modified by removing days from their dates.
+     * The first one will have "today - 1 days" as the creation date, the
+     * second one will be "today - 2 days" etc.
+     *
+     * This function is manually used instead of the {@link org.junit.jupiter.api.BeforeEach}
+     * annotation because using that annotation makes Hibernate fail with the
+     * following message: "session is closed and settings disallow loading
+     * outside the Session".
+     */
+    @Transactional
+    void insertEventFixtures() {
+        final Bundle bundle = this.resourceHelpers.createBundle("test-engine-event-repository-bundle");
+        final Application application = this.resourceHelpers.createApp(bundle.getId(), "test-engine-event-repository-application");
+        final EventType eventType = this.resourceHelpers.createEventType(application.getId(), "test-engine-event-repository-event-type");
+
+        // Create five events which will be used in the tests.
+        for (int i = 0; i < 5; i++) {
+            this.createdEvents.add(
+                this.resourceHelpers.createEvent(eventType)
+            );
+        }
+
+        final LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+
+        // Modify the events' creation date.
+        for (int i = 0; i < 5; i++) {
+            final Event event = this.createdEvents.get(i);
+            event.setCreated(now.minusDays(i + 1));
+
+            this.entityManager.persist(event);
+        }
+    }
+
+    /**
+     * Tests that when no date ranges are provided all the events related to
+     * the org id are fetched.
+     */
+    @Test
+    @TestTransaction
+    void testGetAll() {
+        this.insertEventFixtures();
+
+        final List<Event> result = this.eventRepository.findEventsToExport(DEFAULT_ORG_ID, null, null);
+
+        Assertions.assertEquals(this.createdEvents.size(), result.size(), "unexpected number of fetched events");
+        Assertions.assertIterableEquals(this.createdEvents, result, "the fetched events are not the same as the created ones");
+    }
+
+    /**
+     * Tests that when just the "from" date is provided, the events are
+     * filtered as expected.
+     */
+    @Test
+    @TestTransaction
+    void testGetJustFrom() {
+        this.insertEventFixtures();
+
+        final LocalDate today = LocalDate.now();
+        final LocalDate fourDaysAgo = today.minusDays(4);
+
+        final List<Event> result = this.eventRepository.findEventsToExport(DEFAULT_ORG_ID, fourDaysAgo, null);
+
+        for (final Event event : result) {
+            final LocalDate eventDate = event.getCreated().toLocalDate();
+
+            Assertions.assertTrue(
+                eventDate.compareTo(fourDaysAgo) >= 0,
+                String.format(
+                    "the event doesn't have a date greater or equal than the specified \"from\" filter. \"from\" filter date: %s. Event date: %s",
+                    fourDaysAgo,
+                    eventDate
+                )
+            );
+        }
+    }
+
+
+    /**
+     * Tests that when just the "to" date is provided, the events are filtered
+     * as expected.
+     */
+    @Test
+    @TestTransaction
+    void testGetJustTo() {
+        this.insertEventFixtures();
+
+        final LocalDate today = LocalDate.now();
+        final LocalDate threeDaysAgo = today.minusDays(3);
+
+        final List<Event> result = this.eventRepository.findEventsToExport(DEFAULT_ORG_ID, null, threeDaysAgo);
+
+        for (final Event event : result) {
+            final LocalDate eventDate = event.getCreated().toLocalDate();
+
+            Assertions.assertTrue(
+                eventDate.compareTo(threeDaysAgo) <= 0,
+                String.format(
+                    "the event doesn't have a date less or equal than the specified \"to\" filter. \"to\" filter date: %s. Event date: %s",
+                    threeDaysAgo,
+                    eventDate
+                )
+            );
+        }
+    }
+
+    /**
+     * Tests that when a date range is provided, only the events that comply
+     * with that range are fetched.
+     */
+    @Test
+    @TestTransaction
+    void testGetDateRange() {
+        this.insertEventFixtures();
+
+        final LocalDate today = LocalDate.now();
+        final LocalDate fourDaysAgo = today.minusDays(4);
+        final LocalDate threeDaysAgo = today.minusDays(3);
+
+        final List<Event> result = this.eventRepository.findEventsToExport(DEFAULT_ORG_ID, fourDaysAgo, threeDaysAgo);
+
+        for (final Event event : result) {
+            final LocalDate eventDate = event.getCreated().toLocalDate();
+
+            Assertions.assertTrue(
+                eventDate.compareTo(fourDaysAgo) >= 0,
+                String.format(
+                    "the event doesn't have a date greater or equal than the specified \"from\" filter. \"from\" filter date: %s. Event date: %s",
+                    threeDaysAgo,
+                    eventDate
+                )
+            );
+
+            Assertions.assertTrue(
+                eventDate.compareTo(threeDaysAgo) <= 0,
+                String.format(
+                    "the event doesn't have a date less or equal than the specified \"to\" filter. \"to\" filter date: %s. Event date: %s",
+                    threeDaysAgo,
+                    eventDate
+                )
+            );
+        }
+    }
+
+    /**
+     * Tests that when no dates are provided the function under test does not
+     * raise any exceptions.
+     */
+    @Test
+    void testNoDates() {
+        this.eventRepository.validateFromTo(null, null);
+    }
+
+    /**
+     * Tests that when proper "from" and "to" dates are provided to the
+     * function under test, no exceptions are raised.
+     */
+    @Test
+    void testValidDates() {
+        final LocalDate today = LocalDate.now();
+
+        this.eventRepository.validateFromTo(today.minusDays(10), today.minusDays(5));
+    }
+
+    /**
+     * Tests that "from" dates that are in the future cause an exception to
+     * raise.
+     */
+    @Test
+    void testInvalidFromDateFuture() {
+        final LocalDate today = LocalDate.now();
+
+        final IllegalStateException exception = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.eventRepository.validateFromTo(today.plusDays(1), null)
+        );
+
+        Assertions.assertEquals("can't fetch events from the future!", exception.getMessage(), "unexpected error message when validating a 'from' date from the future");
+    }
+
+    /**
+     * Tests that "from" dates that are older than a month cause an exception
+     * to raise.
+     */
+    @Test
+    void testInvalidFromDateOlderMonth() {
+        final LocalDate today = LocalDate.now();
+
+        final IllegalStateException exception = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.eventRepository.validateFromTo(today.minusMonths(1).minusDays(1), null)
+        );
+
+        Assertions.assertEquals("events that are older than a month cannot be fetched", exception.getMessage(), "unexpected error message when validating a 'from' date older than a month");
+    }
+
+    /**
+     * Tests that "from" dates that are after "to" dates cause an exception to
+     * raise.
+     */
+    @Test
+    void testInvalidFromDateAfterToDate() {
+        final LocalDate today = LocalDate.now();
+
+        final IllegalStateException exception = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.eventRepository.validateFromTo(today.minusDays(5), today.minusDays(10))
+        );
+
+        Assertions.assertEquals("the 'to' date cannot be lower than the 'from' date", exception.getMessage(), "unexpected error message when validating a 'from' date which is after a 'to' date");
+    }
+
+    /**
+     * Tests that "to" dates that are in the future cause an exception to
+     * raise.
+     */
+    @Test
+    void testInvalidToDateFuture() {
+        final LocalDate today = LocalDate.now();
+
+        final IllegalStateException exception = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.eventRepository.validateFromTo(null, today.plusDays(1))
+        );
+
+        Assertions.assertEquals("can't fetch events from the future!", exception.getMessage(), "unexpected error message when validating a 'to' date from the future");
+    }
+
+    /**
+     * Tests that "to" dates that are older than a month cause an exception to
+     * raise.
+     */
+    @Test
+    void testInvalidToDateOlderMonth() {
+        final LocalDate today = LocalDate.now();
+
+        final IllegalStateException exception = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.eventRepository.validateFromTo(null, today.minusMonths(1).minusDays(1))
+        );
+
+        Assertions.assertEquals("events that are older than a month cannot be fetched", exception.getMessage(), "unexpected error message when validating a 'to' date older than a month");
+    }
+
+    String generateRandomString() {
+        final byte[] array = new byte[50];
+        final Random random = new Random();
+        random.nextBytes(array);
+
+        return new String(array, StandardCharsets.UTF_8);
+    }
+}
