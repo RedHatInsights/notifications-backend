9ffcf0845045fecf8476aa7e07406fcc4e746e77
Gwenneg Lepage <glepage@redhat.com>
2023-05-11 14:56:53 +0200

    Deploy the same image tag for backend and engine in ephemeral

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -2,8 +2,36 @@
 
 set -exv
 
-source ./.rhcicd/pr_check_backend.sh
-source ./.rhcicd/pr_check_engine.sh
+# Bonfire init
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# Build the notifications-backend image and push it to Quay
+export IMAGE="quay.io/cloudservices/notifications-backend"
+export DOCKERFILE=docker/Dockerfile.notifications-backend.jvm
+source $CICD_ROOT/build.sh
+
+# Build the notifications-engine image and push it to Quay
+export IMAGE="quay.io/cloudservices/notifications-engine"
+export DOCKERFILE=docker/Dockerfile.notifications-engine.jvm
+source $CICD_ROOT/build.sh
+
+# Deploy both images on ephemeral
+export APP_NAME="notifications"
+export COMPONENT_NAME="notifications-backend"
+export IMAGE="quay.io/cloudservices/notifications-backend"
+export DEPLOY_TIMEOUT="900"
+export EXTRA_DEPLOY_ARGS="--set-parameter notifications-engine/IMAGE_TAG=${IMAGE_TAG}"
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Run IQE tests
+export IQE_PLUGINS="notifications"
+export IQE_MARKER_EXPRESSION="notification_smoke and api"
+export IQE_FILTER_EXPRESSION=""
+export IQE_CJI_TIMEOUT="30m"
+source $CICD_ROOT/cji_smoke_test.sh
+
+# Build additional images that won't be pushed to Quay
 docker build . -f docker/Dockerfile.notifications-aggregator.jvm
 docker build . -f docker/Dockerfile.notifications-camel-demo-log.jvm
 
