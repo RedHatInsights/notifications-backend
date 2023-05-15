0193e9f001297e569c48bd3040646daa36946407
Guillaume Duval <gduval@redhat.com>
2023-05-15 16:14:46 +0200

    [RHCLOUD-25672] Integrations trought camel refactoring

---
+++ engine/src/test/java/com/redhat/cloud/notifications/processors/slack/SlackNotificationProcessorTest.java
@@ -0,0 +1,40 @@
+package com.redhat.cloud.notifications.processors.slack;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redhat.cloud.notifications.processors.camel.CamelNotificationProcessorTest;
+import io.quarkus.test.junit.QuarkusTest;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.junit.jupiter.api.Test;
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class SlackNotificationProcessorTest extends CamelNotificationProcessorTest {
+
+    @Inject
+    SlackNotificationProcessor slackNotificationProcessor;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    @Override
+    protected Processor getProcessor() {
+        return slackNotificationProcessor;
+    }
+
+    @Test
+    protected void testProcess() throws Exception {
+        SlackNotification notification = SlackRoutesTest.buildCamelSlackNotification("https://redhat.com");
+
+        String notificationAsString = objectMapper.writeValueAsString(notification);
+        Exchange exchange = createExchangeWithBody(notificationAsString);
+
+        getProcessor().process(exchange);
+
+        verifyCommonFields(notification, exchange);
+        assertEquals(notification.channel, exchange.getProperty("channel", String.class));
+    }
+
+}
---
+++ engine/src/test/java/com/redhat/cloud/notifications/processors/teams/TeamsNotificationProcessorTest.java
@@ -0,0 +1,19 @@
+package com.redhat.cloud.notifications.processors.teams;
+
+import com.redhat.cloud.notifications.processors.camel.CamelNotificationProcessorTest;
+import io.quarkus.test.junit.QuarkusTest;
+import org.apache.camel.Processor;
+import javax.inject.Inject;
+
+@QuarkusTest
+public class TeamsNotificationProcessorTest extends CamelNotificationProcessorTest {
+
+    @Inject
+    TeamsNotificationProcessor teamsNotificationProcessor;
+
+    @Override
+    protected Processor getProcessor() {
+        return teamsNotificationProcessor;
+    }
+
+}
