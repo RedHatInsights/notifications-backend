03858a3cd50948b296ada7dee5c6352b75e6bd09
Guillaume Duval <gduval@redhat.com>
2023-06-21 16:56:57 +0200

    fix checkstyle

--- engine/src/main/java/com/redhat/cloud/notifications/processors/drawer/DrawerProcessor.java
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/drawer/DrawerProcessor.java
@@ -37,7 +37,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import static com.redhat.cloud.notifications.events.EndpointProcessor.DELAYED_EXCEPTION_MSG;
