6fb42eedea4229595f8d5a2c5e257619981c1321
Gwenneg Lepage <glepage@redhat.com>
2023-06-27 12:27:05 +0200

    RHCLOUD-26620 Remove old connectors code from engine

--- engine/src/test/java/com/redhat/cloud/notifications/processors/camel/google/chat/GoogleChatTestProfile.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.cloud.notifications.processors.camel.google.chat;
-
-import io.quarkus.test.junit.QuarkusTestProfile;
-
-public class GoogleChatTestProfile implements QuarkusTestProfile {
-}
--- engine/src/test/java/com/redhat/cloud/notifications/processors/camel/teams/TeamsTestProfile.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.cloud.notifications.processors.camel.teams;
-
-import io.quarkus.test.junit.QuarkusTestProfile;
-
-public class TeamsTestProfile implements QuarkusTestProfile {
-}
