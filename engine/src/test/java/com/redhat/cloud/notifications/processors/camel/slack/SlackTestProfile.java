e301bddde0d4701789eae2c43edee4bbd49f093b
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-07-07 10:20:43 +0200

    RHCLOUD-26620 Remove old connectors code from engine (#2035)

--- engine/src/test/java/com/redhat/cloud/notifications/processors/camel/slack/SlackTestProfile.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.cloud.notifications.processors.camel.slack;
-
-import io.quarkus.test.junit.QuarkusTestProfile;
-
-public class SlackTestProfile implements QuarkusTestProfile {
-}
