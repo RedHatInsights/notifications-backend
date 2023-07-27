a928ac82b2ef3f93168bf496a43315f35d1caa87
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-07-27 09:38:36 +0200

    Make Kafka poll-on-error configurable from deployment config (#2091)

--- connector-common/src/main/java/com/redhat/cloud/notifications/connector/ConnectorConfig.java
+++ connector-common/src/main/java/com/redhat/cloud/notifications/connector/ConnectorConfig.java
@@ -1,5 +1,6 @@
 package com.redhat.cloud.notifications.connector;
 
+import io.quarkus.logging.Log;
 import io.quarkus.runtime.configuration.ProfileManager;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -10,37 +11,67 @@ import static io.quarkus.runtime.LaunchMode.TEST;
 @ApplicationScoped
 public class ConnectorConfig {
 
-    @ConfigProperty(name = "notifications.connector.endpoint-cache-max-size", defaultValue = "100")
+    private static final String ENDPOINT_CACHE_MAX_SIZE = "notifications.connector.endpoint-cache-max-size";
+    private static final String KAFKA_INCOMING_GROUP_ID = "notifications.connector.kafka.incoming.group-id";
+    private static final String KAFKA_INCOMING_MAX_POLL_INTERVAL_MS = "notifications.connector.kafka.incoming.max-poll-interval-ms";
+    private static final String KAFKA_INCOMING_MAX_POLL_RECORDS = "notifications.connector.kafka.incoming.max-poll-records";
+    private static final String KAFKA_INCOMING_POLL_ON_ERROR = "notifications.connector.kafka.incoming.poll-on-error";
+    private static final String KAFKA_INCOMING_TOPIC = "notifications.connector.kafka.incoming.topic";
+    private static final String KAFKA_OUTGOING_TOPIC = "notifications.connector.kafka.outgoing.topic";
+    private static final String NAME = "notifications.connector.name";
+    private static final String REDELIVERY_COUNTER_NAME = "notifications.connector.redelivery.counter-name";
+    private static final String REDELIVERY_DELAY = "notifications.connector.redelivery.delay";
+    private static final String REDELIVERY_MAX_ATTEMPTS = "notifications.connector.redelivery.max-attempts";
+
+    @ConfigProperty(name = ENDPOINT_CACHE_MAX_SIZE, defaultValue = "100")
     int endpointCacheMaxSize;
 
-    @ConfigProperty(name = "notifications.connector.kafka.incoming.group-id")
+    @ConfigProperty(name = KAFKA_INCOMING_GROUP_ID)
     String incomingKafkaGroupId;
 
     // https://docs.confluent.io/platform/current/installation/configuration/consumer-configs.html#max-poll-interval-ms
-    @ConfigProperty(name = "notifications.connector.kafka.incoming.max-poll-interval-ms", defaultValue = "300000")
+    @ConfigProperty(name = KAFKA_INCOMING_MAX_POLL_INTERVAL_MS, defaultValue = "300000")
     long incomingKafkaMaxPollIntervalMs;
 
     // https://docs.confluent.io/platform/current/installation/configuration/consumer-configs.html#max-poll-records
-    @ConfigProperty(name = "notifications.connector.kafka.incoming.max-poll-records", defaultValue = "500")
+    @ConfigProperty(name = KAFKA_INCOMING_MAX_POLL_RECORDS, defaultValue = "500")
     int incomingKafkaMaxPollRecords;
 
-    @ConfigProperty(name = "notifications.connector.kafka.incoming.topic")
+    @ConfigProperty(name = KAFKA_INCOMING_POLL_ON_ERROR, defaultValue = "RECONNECT")
+    String incomingKafkaPollOnError;
+
+    @ConfigProperty(name = KAFKA_INCOMING_TOPIC)
     String incomingKafkaTopic;
 
-    @ConfigProperty(name = "notifications.connector.kafka.outgoing.topic")
+    @ConfigProperty(name = KAFKA_OUTGOING_TOPIC)
     String outgoingKafkaTopic;
 
-    @ConfigProperty(name = "notifications.connector.name")
+    @ConfigProperty(name = NAME)
     String connectorName;
 
-    @ConfigProperty(name = "notifications.connector.redelivery.counter-name")
+    @ConfigProperty(name = REDELIVERY_COUNTER_NAME)
     String redeliveryCounterName;
 
-    @ConfigProperty(name = "notifications.connector.redelivery.delay", defaultValue = "1000")
+    @ConfigProperty(name = REDELIVERY_DELAY, defaultValue = "1000")
     long redeliveryDelay;
 
-    @ConfigProperty(name = "notifications.connector.redelivery.max-attempts", defaultValue = "2")
+    @ConfigProperty(name = REDELIVERY_MAX_ATTEMPTS, defaultValue = "2")
     int redeliveryMaxAttempts;
+
+    public void log() {
+        Log.info("=== Connector configuration ===");
+        Log.infof("%s=%s", ENDPOINT_CACHE_MAX_SIZE, endpointCacheMaxSize);
+        Log.infof("%s=%s", KAFKA_INCOMING_GROUP_ID, incomingKafkaGroupId);
+        Log.infof("%s=%s", KAFKA_INCOMING_MAX_POLL_INTERVAL_MS, incomingKafkaMaxPollIntervalMs);
+        Log.infof("%s=%s", KAFKA_INCOMING_MAX_POLL_RECORDS, incomingKafkaMaxPollRecords);
+        Log.infof("%s=%s", KAFKA_INCOMING_POLL_ON_ERROR, incomingKafkaPollOnError);
+        Log.infof("%s=%s", KAFKA_INCOMING_TOPIC, incomingKafkaTopic);
+        Log.infof("%s=%s", KAFKA_OUTGOING_TOPIC, outgoingKafkaTopic);
+        Log.infof("%s=%s", NAME, connectorName);
+        Log.infof("%s=%s", REDELIVERY_COUNTER_NAME, redeliveryCounterName);
+        Log.infof("%s=%s", REDELIVERY_DELAY, redeliveryDelay);
+        Log.infof("%s=%s", REDELIVERY_MAX_ATTEMPTS, redeliveryMaxAttempts);
+    }
 
     public int getEndpointCacheMaxSize() {
         return endpointCacheMaxSize;
@@ -76,6 +107,15 @@ public class ConnectorConfig {
     public void setIncomingKafkaMaxPollRecords(int incomingKafkaMaxPollRecords) {
         checkTestLaunchMode();
         this.incomingKafkaMaxPollRecords = incomingKafkaMaxPollRecords;
+    }
+
+    public String getIncomingKafkaPollOnError() {
+        return incomingKafkaPollOnError;
+    }
+
+    public void setIncomingKafkaPollOnError(String incomingKafkaPollOnError) {
+        checkTestLaunchMode();
+        this.incomingKafkaPollOnError = incomingKafkaPollOnError;
     }
 
     public String getIncomingKafkaTopic() {
