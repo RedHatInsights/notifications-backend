97effbc36abe9749364a1535a9a9997ca017d9d1
Gwenneg Lepage <gwenneg@redhat.com>
2023-07-27 16:18:18 +0200

    Configure Splunk HTTPS timeouts programmatically

--- connector-common/src/main/java/com/redhat/cloud/notifications/connector/ConnectorConfig.java
+++ connector-common/src/main/java/com/redhat/cloud/notifications/connector/ConnectorConfig.java
@@ -1,14 +1,13 @@
 package com.redhat.cloud.notifications.connector;
 
+import io.quarkus.arc.DefaultBean;
 import io.quarkus.logging.Log;
-import io.quarkus.runtime.configuration.ProfileManager;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import static io.quarkus.runtime.LaunchMode.TEST;
-
 @ApplicationScoped
+@DefaultBean
 public class ConnectorConfig {
 
     private static final String ENDPOINT_CACHE_MAX_SIZE = "notifications.connector.endpoint-cache-max-size";
@@ -77,109 +76,43 @@ public class ConnectorConfig {
         return endpointCacheMaxSize;
     }
 
-    public void setEndpointCacheMaxSize(int endpointCacheMaxSize) {
-        checkTestLaunchMode();
-        this.endpointCacheMaxSize = endpointCacheMaxSize;
-    }
-
     public String getIncomingKafkaGroupId() {
         return incomingKafkaGroupId;
-    }
-
-    public void setIncomingKafkaGroupId(String incomingKafkaGroupId) {
-        checkTestLaunchMode();
-        this.incomingKafkaGroupId = incomingKafkaGroupId;
     }
 
     public long getIncomingKafkaMaxPollIntervalMs() {
         return incomingKafkaMaxPollIntervalMs;
     }
 
-    public void setIncomingKafkaMaxPollIntervalMs(long incomingKafkaMaxPollIntervalMs) {
-        checkTestLaunchMode();
-        this.incomingKafkaMaxPollIntervalMs = incomingKafkaMaxPollIntervalMs;
-    }
-
     public int getIncomingKafkaMaxPollRecords() {
         return incomingKafkaMaxPollRecords;
-    }
-
-    public void setIncomingKafkaMaxPollRecords(int incomingKafkaMaxPollRecords) {
-        checkTestLaunchMode();
-        this.incomingKafkaMaxPollRecords = incomingKafkaMaxPollRecords;
     }
 
     public String getIncomingKafkaPollOnError() {
         return incomingKafkaPollOnError;
     }
 
-    public void setIncomingKafkaPollOnError(String incomingKafkaPollOnError) {
-        checkTestLaunchMode();
-        this.incomingKafkaPollOnError = incomingKafkaPollOnError;
-    }
-
     public String getIncomingKafkaTopic() {
         return incomingKafkaTopic;
-    }
-
-    public void setIncomingKafkaTopic(String incomingKafkaTopic) {
-        checkTestLaunchMode();
-        this.incomingKafkaTopic = incomingKafkaTopic;
     }
 
     public String getOutgoingKafkaTopic() {
         return outgoingKafkaTopic;
     }
 
-    public void setOutgoingKafkaTopic(String outgoingKafkaTopic) {
-        checkTestLaunchMode();
-        this.outgoingKafkaTopic = outgoingKafkaTopic;
-    }
-
     public String getConnectorName() {
         return connectorName;
-    }
-
-    public void setConnectorName(String connectorName) {
-        checkTestLaunchMode();
-        this.connectorName = connectorName;
     }
 
     public String getRedeliveryCounterName() {
         return redeliveryCounterName;
     }
 
-    public void setRedeliveryCounterName(String redeliveryCounterName) {
-        checkTestLaunchMode();
-        this.redeliveryCounterName = redeliveryCounterName;
-    }
-
     public long getRedeliveryDelay() {
         return redeliveryDelay;
     }
 
-    public void setRedeliveryDelay(long redeliveryDelay) {
-        checkTestLaunchMode();
-        this.redeliveryDelay = redeliveryDelay;
-    }
-
     public int getRedeliveryMaxAttempts() {
         return redeliveryMaxAttempts;
-    }
-
-    public void setRedeliveryMaxAttempts(int redeliveryMaxAttempts) {
-        checkTestLaunchMode();
-        this.redeliveryMaxAttempts = redeliveryMaxAttempts;
-    }
-
-    /**
-     * This method throws an {@link IllegalStateException} if it is invoked with a launch mode different from
-     * {@link io.quarkus.runtime.LaunchMode#TEST TEST}. It should be added to methods that allow overriding a
-     * config value from tests only, preventing doing so from runtime code.
-     */
-    private static void checkTestLaunchMode() {
-        if (ProfileManager.getLaunchMode() != TEST) {
-            throw new IllegalStateException("Illegal config value override detected");
-        }
     }
 }
