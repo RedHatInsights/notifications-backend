95db4ca3f0964961152928fc14f2db0d87358c64
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2023-08-21 18:15:35 +0200

    refactor: remove non-optimal annotation
    RHCLOIUD-23645

--- engine/src/main/java/com/redhat/cloud/notifications/recipients/mbop/MBOPUser.java
+++ engine/src/main/java/com/redhat/cloud/notifications/recipients/mbop/MBOPUser.java
@@ -1,16 +1,13 @@
 package com.redhat.cloud.notifications.recipients.mbop;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 /**
- * Represents the response to the {@link MBOPService#getUsersByOrgId(String, boolean, String, int, int)}
+ * Represents the response to the {@link MBOPService#getUsersByOrgId(String, String, String, String, boolean, String, int, int)}
  * REST API call. The {@link JsonProperty} annotations are required because
  * otherwise Jackson struggles to deserialize the incoming payload into a
  * record structure.
  */
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MBOPUser(
     @JsonProperty("id")             String id,
     @JsonProperty("username")       String username,
