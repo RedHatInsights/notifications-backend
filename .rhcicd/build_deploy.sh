977a30d6939db0d912c1ad281aa4480134310f71
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2023-09-06 17:04:11 +0200

    feature: build, deploy and test files for the email connector
    These files are required to be able to build, deploy and test the email
    connector.
    
    RHCLOUD-28089

--- .rhcicd/build_deploy.sh
+++ .rhcicd/build_deploy.sh
@@ -26,6 +26,7 @@ function buildAndDeploy() {
 
 buildAndDeploy "notifications-aggregator"
 buildAndDeploy "notifications-backend"
+buildAndDeploy "notifications-connector-email"
 buildAndDeploy "notifications-connector-google-chat"
 buildAndDeploy "notifications-connector-microsoft-teams"
 buildAndDeploy "notifications-connector-servicenow"
