3c839eae46406b2d66d864ba43b95c71fbec1c8e
Guillaume Duval <gduval@redhat.com>
2023-09-20 14:13:07 +0200

    [RHCLOUD-28414] Bump Postgres test images from 11 to 15

--- .rhcicd/sonarqube/scanner/scan_code.bash
+++ .rhcicd/sonarqube/scanner/scan_code.bash
@@ -10,8 +10,8 @@
 set -euxo pipefail
 
 # Start Postgresql and init notifications database
-su -l postgres -c /usr/pgsql-11/bin/initdb
-su -l postgres -c "/usr/pgsql-11/bin/pg_ctl -D /var/lib/pgsql/11/data -l /tmp/pg_logfile start"
+su -l postgres -c /usr/pgsql-15/bin/initdb
+su -l postgres -c "/usr/pgsql-15/bin/pg_ctl -D /var/lib/pgsql/15/data -l /tmp/pg_logfile start"
 createdb -U postgres notifications
 
 #
