24f179f3792b0b375ff4a6504b134fbd6ead5a68
shannon-donahue <49072851+shannon-donahue@users.noreply.github.com>
2023-09-29 02:55:15 -0400

    Update build deploy script. (#2209)

--- .rhcicd/build_deploy.sh
+++ .rhcicd/build_deploy.sh
@@ -11,6 +11,7 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
 
 function buildAndDeploy() {
     IMAGE_NAME=$1
@@ -18,10 +19,15 @@ function buildAndDeploy() {
     docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
     docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f docker/Dockerfile.${IMAGE_NAME}.jvm
     docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
-    docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
-    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
-    docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+    if [[ $GIT_BRANCH == *"security-compliance"* ]]; then
+        docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${SECURITY_COMPLIANCE_TAG}"
+        docker --config="$DOCKER_CONF" push "${IMAGE}:${SECURITY_COMPLIANCE_TAG}"
+    else
+        docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
+        docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
+        docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+        docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+    fi
 }
 
 buildAndDeploy "notifications-aggregator"
