443952730ea903c99147e22250122d02492225e8
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-10-24 10:27:03 +0200

    Use a separate request scope for async aggregation (#2260)

---
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/email/AsyncAggregation.java
@@ -0,0 +1,42 @@
+package com.redhat.cloud.notifications.processors.email;
+
+import com.redhat.cloud.notifications.models.Event;
+import io.quarkus.logging.Log;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.control.ActivateRequestContext;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+@Dependent
+public class AsyncAggregation implements Runnable {
+
+    @Inject
+    EmailSubscriptionTypeProcessor emailSubscriptionTypeProcessor;
+
+    private Event event;
+
+    @PostConstruct
+    void postConstruct() {
+        // Temporary log entry.
+        Log.infof("Instance created: %d", hashCode());
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    @ActivateRequestContext
+    @Transactional
+    public void run() {
+        emailSubscriptionTypeProcessor.processAggregationSync(event);
+    }
+
+    @PreDestroy
+    void preDestroy() {
+        // Temporary log entry.
+        Log.infof("Instance destroyed: %d", hashCode());
+    }
+}
