b416533701f14aca92fbf945d0ec0a9713de0bd1
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-11-08 08:41:52 +0100

    Ignore unknown RecipientsQuery properties (#2322)

--- recipients-resolver/src/main/java/com/redhat/cloud/notifications/recipients/rest/pojo/RecipientsQuery.java
+++ recipients-resolver/src/main/java/com/redhat/cloud/notifications/recipients/rest/pojo/RecipientsQuery.java
@@ -1,5 +1,6 @@
 package com.redhat.cloud.notifications.recipients.rest.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.redhat.cloud.notifications.recipients.model.RecipientSettings;
 import jakarta.validation.constraints.NotBlank;
@@ -9,6 +10,7 @@ import java.util.Set;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
 @JsonNaming(SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RecipientsQuery {
 
     @NotBlank
