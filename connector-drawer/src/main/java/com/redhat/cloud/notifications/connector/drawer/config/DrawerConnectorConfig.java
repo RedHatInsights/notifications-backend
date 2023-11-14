aa8eaa6a8f4818a9e9e9ac9aa37e99337c9c1db7
Gwenneg Lepage <glepage@redhat.com>
2023-11-14 10:32:50 +0100

    Make the drawer connector an HTTP one as well

--- connector-drawer/src/main/java/com/redhat/cloud/notifications/connector/drawer/config/DrawerConnectorConfig.java
+++ connector-drawer/src/main/java/com/redhat/cloud/notifications/connector/drawer/config/DrawerConnectorConfig.java
@@ -1,12 +1,20 @@
 package com.redhat.cloud.notifications.connector.drawer.config;
 
-import com.redhat.cloud.notifications.connector.ConnectorConfig;
+import com.redhat.cloud.notifications.connector.http.HttpConnectorConfig;
+import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import java.util.Map;
 
 @ApplicationScoped
-public class DrawerConnectorConfig extends ConnectorConfig {
+/*
+ * @Alternative and Priority will soon go away.
+ * See https://github.com/quarkusio/quarkus/issues/37042 for more details about the replacement.
+ */
+@Alternative
+@Priority(0) // The value doesn't matter.
+public class DrawerConnectorConfig extends HttpConnectorConfig {
 
     private static final String DRAWER_TOPIC = "notifications.connector.kafka.outgoing.drawer.topic";
 
