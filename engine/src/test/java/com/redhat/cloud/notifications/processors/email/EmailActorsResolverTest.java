b87906ee628175a6fe64604c3d6c9c07cb3f17ab
Gwenneg Lepage <glepage@redhat.com>
2023-11-28 12:23:18 +0100

    RHCLOUD-29489 Make the email sender vary depending on the bundle/app

--- engine/src/test/java/com/redhat/cloud/notifications/processors/email/EmailActorsResolverTest.java
+++ engine/src/test/java/com/redhat/cloud/notifications/processors/email/EmailActorsResolverTest.java
@@ -1,10 +1,16 @@
 package com.redhat.cloud.notifications.processors.email;
 
+import com.redhat.cloud.notifications.models.Application;
+import com.redhat.cloud.notifications.models.Bundle;
 import com.redhat.cloud.notifications.models.Event;
+import com.redhat.cloud.notifications.models.EventType;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static com.redhat.cloud.notifications.processors.email.EmailActorsResolver.OPENSHIFT_SENDER;
+import static com.redhat.cloud.notifications.processors.email.EmailActorsResolver.RH_INSIGHTS_SENDER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class EmailActorsResolverTest {
@@ -12,10 +18,33 @@ public class EmailActorsResolverTest {
     EmailActorsResolver emailActorsResolver;
 
     /**
-     * Tests that the default "RH Insights" sender is returned for every event.
+     * Tests that the "Red Hat Insights" sender is returned by default.
      */
     @Test
-    void testGetEmailSender() {
-        Assertions.assertEquals(EmailActorsResolver.RH_INSIGHTS_SENDER, this.emailActorsResolver.getEmailSender(new Event()), "unexpected email sender returned from the function under test");
+    void testDefaultEmailSender() {
+        Event event = new Event();
+        assertEquals(RH_INSIGHTS_SENDER, this.emailActorsResolver.getEmailSender(event), "unexpected email sender returned from the function under test");
+    }
+
+    /**
+     * Tests that the OpenShift sender is returned for OCM events.
+     */
+    @Test
+    void testOpenshiftClusterManagerEmailSender() {
+
+        Bundle bundle = new Bundle();
+        bundle.setName("openshift");
+
+        Application app = new Application();
+        app.setBundle(bundle);
+        app.setName("cluster-manager");
+
+        EventType eventType = new EventType();
+        eventType.setApplication(app);
+
+        Event event = new Event();
+        event.setEventType(eventType);
+
+        assertEquals(OPENSHIFT_SENDER, this.emailActorsResolver.getEmailSender(event), "unexpected email sender returned from the function under test");
     }
 }
