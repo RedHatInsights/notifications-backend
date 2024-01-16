ea693a5e876120490ef911f300ccec59186f621a
Guillaume Duval <gduval@redhat.com>
2024-01-16 10:21:03 +0100

    [RHCLOUD-30332] Update heap dump tool

--- backend/src/main/java/com/redhat/cloud/notifications/routers/internal/HeapDumpResource.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/internal/HeapDumpResource.java
@@ -1,15 +1,19 @@
 package com.redhat.cloud.notifications.routers.internal;
 
+import com.sun.management.HotSpotDiagnosticMXBean;
 import io.quarkus.logging.Log;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
-import java.io.BufferedReader;
+import org.apache.http.HttpStatus;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import javax.management.MBeanServer;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
 
 import static com.redhat.cloud.notifications.Constants.API_INTERNAL;
 import static com.redhat.cloud.notifications.auth.ConsoleIdentityProvider.RBAC_INTERNAL_ADMIN;
@@ -19,10 +23,18 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
 @RolesAllowed(RBAC_INTERNAL_ADMIN)
 public class HeapDumpResource {
 
+    @ConfigProperty(name = "quarkus.log.cloudwatch.log-stream-name", defaultValue = "localhost")
+    String hostname;
+
     @GET
+    @Path("{hostname}")
     @Produces(APPLICATION_OCTET_STREAM)
-    public Response getHeapDump() {
-        long pid = ProcessHandle.current().pid();
+    public Response getHeapDump(@PathParam("hostname") String hostname) {
+        if (!this.hostname.equals(hostname)) {
+            Log.infof("%s doesn't match with this host (%s)", hostname, this.hostname);
+            return Response.status(HttpStatus.SC_SEE_OTHER).build();
+        }
+
         String tmpFileName = "notifications_dump.hprof";
         String fullPathTmpFile = String.format("%s/%s", System.getProperty("java.io.tmpdir"), tmpFileName);
 
@@ -30,40 +42,22 @@ public class HeapDumpResource {
         file.delete();
 
         Log.infof("Heap dump will be generated on %s", fullPathTmpFile);
-
-        ProcessBuilder processBuilder = new ProcessBuilder();
-
-        // Run shell heap dump command
-        processBuilder.command("bash", "-c", String.format("jcmd %d GC.heap_dump %s", pid, fullPathTmpFile));
-
         try {
-
-            Process process = processBuilder.start();
-            StringBuilder output = new StringBuilder();
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line + "\n");
-            }
-
-            int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                Log.info("Heap dump was successfully generated");
-            } else {
-                Log.info("Error generating Heap dump");
-            }
-            Log.info(output);
-
-        } catch (IOException | InterruptedException e) {
-            Log.error("Error generating heap dump", e);
+            dumpHeap(fullPathTmpFile, true);
+        } catch (IOException ie) {
+            Log.error("Error generating heap dump", ie);
         }
-
         file = new File(fullPathTmpFile);
 
         Response.ResponseBuilder response = Response.ok(file);
         response.header("Content-Disposition", "attachment; filename=\"" + tmpFileName + "\"");
         return response.build();
+    }
+
+    public static void dumpHeap(String filePath, boolean live) throws IOException {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(
+            server, "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+        mxBean.dumpHeap(filePath, live);
     }
 }
