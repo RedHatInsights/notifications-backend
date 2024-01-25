eddca0b51c9cf500cf3a4553f91fc611866db986
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2024-01-25 19:12:12 +0100

    Update connector-common/src/main/java/com/redhat/cloud/notifications/connector/KafkaHeader.java

--- connector-common/src/main/java/com/redhat/cloud/notifications/connector/KafkaHeader.java
+++ connector-common/src/main/java/com/redhat/cloud/notifications/connector/KafkaHeader.java
@@ -5,5 +5,5 @@ public class KafkaHeader {
      * Specifies the name of the header that we will use to hold the number of
      * times a message got reinjected in the "incoming" topic.
      */
-    public static final String REINJECTION_COUNT = "notifications-connector-reinjections-count";
+    public static final String REINJECTION_COUNT = "x-rh-notifications-connector-reinjections-count";
 }
