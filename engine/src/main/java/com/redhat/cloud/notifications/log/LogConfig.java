79d3897a6833634b2ae0387cd4e964673d47f8d0
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2024-03-10 18:27:48 +0100

    RHCLOUD-26188 Change engine logs level from Unleash (#2511)

---
+++ engine/src/main/java/com/redhat/cloud/notifications/log/LogConfig.java
@@ -0,0 +1,14 @@
+package com.redhat.cloud.notifications.log;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(SnakeCaseStrategy.class)
+public class LogConfig {
+    public String hostName;
+    public String category;
+    public String level;
+}
