b8b986fa22eed8f085ad7222b637bc550c97e790
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2024-03-28 17:18:55 +0100

    Update admin-console/src/main/webapp/src/components/EventTypes/CreateEditModal.tsx
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- admin-console/src/main/webapp/src/components/EventTypes/CreateEditModal.tsx
+++ admin-console/src/main/webapp/src/components/EventTypes/CreateEditModal.tsx
@@ -110,7 +110,7 @@ export const CreateEditModal: React.FunctionComponent<CreateEditModalProps> = (p
                         />
                     </FormGroup>
                     <FormGroup label='is visible?' fieldId='visible'
-                        helperText='Should the event type be visible by users?'>
+                        helperText='Should the event type be visible in the public UI?'>
                         <Checkbox
                             id='visible'
                             name='visible'
