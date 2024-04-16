52d50fc0a8e5f02dd00fd567745a948a4b37bf83
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2024-04-16 15:03:32 +0200

    [RHCLOUD-31862] Implement OCM ROSA HCP email template (#2618)
    * [RHCLOUD-31862] Implement ROSA HCP email template
    ---------
    
    Co-authored-by: Gwenneg Lepage <glepage@redhat.com>

--- engine/src/test/java/com/redhat/cloud/notifications/templates/TestOcmTemplate.java
+++ engine/src/test/java/com/redhat/cloud/notifications/templates/TestOcmTemplate.java
@@ -54,12 +54,23 @@ public class TestOcmTemplate extends EmailTemplatesInDbHelper  {
         assertTrue(result.contains("What can you expect"));
         assertTrue(result.contains("Thank you for choosing Red Hat OpenShift Dedicated Trial."));
         assertFalse(result.contains("Check these resources for more information"));
+        assertTrue(result.contains("What should you do to minimize impact"));
 
         action = OcmTestHelpers.createOcmAction("Batcave", "OSDTrial", "<b>Batmobile</b> need a revision", "Awesome subject", "Upgrade scheduled", Optional.of(Map.of("template_sub_type", "upgrade-scheduled-template", "doc_references", List.of("https://docs.openshift.com/rosa/ocm/ocm-overview.html", "https://console.redhat.com/openshift"))));
         result = generateEmailBody(CLUSTER_UPDATE, action);
         assertTrue(result.contains("Check these resources for more information"));
         assertTrue(result.contains("https://docs.openshift.com/rosa/ocm/ocm-overview.html"));
         assertTrue(result.contains("https://console.redhat.com/openshift"));
+
+        action = OcmTestHelpers.createOcmAction("Batcave", "OSDTrial", "<b>Batmobile</b> need a revision", "Awesome subject", "Upgrade scheduled", Optional.of(Map.of("template_sub_type", "upgrade-scheduled-template-rosa-hcp")));
+        result = generateEmailBody(CLUSTER_UPDATE, action);
+        assertTrue(result.contains(TestHelpers.HCC_LOGO_TARGET));
+        assertTrue(result.contains("Upgrade scheduled"));
+        assertTrue(result.contains(((Map<String, String>) action.getEvents().get(0).getPayload().getAdditionalProperties().get("global_vars")).get("log_description")));
+        assertTrue(result.contains("What can you expect"));
+        assertTrue(result.contains("Thank you for choosing Red Hat OpenShift Dedicated Trial."));
+        assertTrue(result.contains("The machine pools hosting the applications will not be upgraded. The machine pools must be upgraded separately."));
+        assertFalse(result.contains("What should you do to minimize impact"));
     }
 
     @Test
