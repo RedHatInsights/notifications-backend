70fc96df19bee914db1ad788a48932a805e063e7
Guillaume Duval <gduval@redhat.com>
2024-04-16 14:43:56 +0200

    [RHCLOUD-31862] Improve test case

--- engine/src/test/java/com/redhat/cloud/notifications/templates/TestOcmTemplate.java
+++ engine/src/test/java/com/redhat/cloud/notifications/templates/TestOcmTemplate.java
@@ -69,6 +69,7 @@ public class TestOcmTemplate extends EmailTemplatesInDbHelper  {
         assertTrue(result.contains(((Map<String, String>) action.getEvents().get(0).getPayload().getAdditionalProperties().get("global_vars")).get("log_description")));
         assertTrue(result.contains("What can you expect"));
         assertTrue(result.contains("Thank you for choosing Red Hat OpenShift Dedicated Trial."));
+        assertTrue(result.contains("The machine pools hosting the applications will not be upgraded. The machine pools must be upgraded separately."));
         assertFalse(result.contains("What should you do to minimize impact"));
     }
 
