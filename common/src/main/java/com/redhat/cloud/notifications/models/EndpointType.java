24c62be3d0d8ef0370303f7fe518ab96fb8e23e0
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2024-05-07 19:29:08 +0200

    refactor: move the JSON annotations to DTO
    RHCLOUD-31165

--- common/src/main/java/com/redhat/cloud/notifications/models/EndpointType.java
+++ common/src/main/java/com/redhat/cloud/notifications/models/EndpointType.java
@@ -1,19 +1,10 @@
 package com.redhat.cloud.notifications.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
-@Schema(enumeration = { "webhook", "email_subscription", "camel", "ansible", "drawer" })
 public enum EndpointType {
-    @JsonProperty("webhook")
     WEBHOOK(false, false),
-    @JsonProperty("email_subscription")
     EMAIL_SUBSCRIPTION(false, true),
-    @JsonProperty("camel")
     CAMEL(true, false),
-    @JsonProperty("ansible")
     ANSIBLE(false, false),
-    @JsonProperty("drawer")
     DRAWER(false, true);
 
     public final boolean requiresSubType;
--- common/src/main/java/com/redhat/cloud/notifications/models/dto/v1/endpoint/EndpointTypeDTO.java
+++ common/src/main/java/com/redhat/cloud/notifications/models/dto/v1/endpoint/EndpointTypeDTO.java
@@ -1,12 +1,20 @@
 package com.redhat.cloud.notifications.models.dto.v1.endpoint;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+@Schema(enumeration = { "webhook", "email_subscription", "camel", "ansible", "drawer" })
 public enum EndpointTypeDTO {
+    @JsonProperty("ansible")
     ANSIBLE(false, false),
+    @JsonProperty("camel")
     CAMEL(true, false),
+    @JsonProperty("drawer")
     DRAWER(false, true),
+    @JsonProperty("email_subscription")
     EMAIL_SUBSCRIPTION(false, true),
+    @JsonProperty("webhook")
     WEBHOOK(false, false);
 
     public final boolean requiresSubType;
