c7daa721a97ea96129acc4d035158eff91c8080f
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2024-05-10 18:57:24 +0200

    refactor: remove redundant JsonProperty annotations
    The JsonValue annotation already adds the lowercase names to the
    enumerations, so having the JsonProperties doing that is redundant.
    
    RHCLOUD-31165

--- common/src/main/java/com/redhat/cloud/notifications/models/dto/v1/endpoint/EndpointTypeDTO.java
+++ common/src/main/java/com/redhat/cloud/notifications/models/dto/v1/endpoint/EndpointTypeDTO.java
@@ -1,20 +1,14 @@
 package com.redhat.cloud.notifications.models.dto.v1.endpoint;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Schema(enumeration = { "webhook", "email_subscription", "camel", "ansible", "drawer" })
 public enum EndpointTypeDTO {
-    @JsonProperty("ansible")
     ANSIBLE(false, false),
-    @JsonProperty("camel")
     CAMEL(true, false),
-    @JsonProperty("drawer")
     DRAWER(false, true),
-    @JsonProperty("email_subscription")
     EMAIL_SUBSCRIPTION(false, true),
-    @JsonProperty("webhook")
     WEBHOOK(false, false);
 
     public final boolean requiresSubType;
@@ -25,6 +19,10 @@ public enum EndpointTypeDTO {
         this.isSystemEndpointType = isSystemEndpointType;
     }
 
+    /**
+     * Transforms the enum values to lowercase.
+     * @return the enum value in lowercase.
+     */
     @JsonValue
     public String toLowerCase() {
         return this.toString().toLowerCase();
