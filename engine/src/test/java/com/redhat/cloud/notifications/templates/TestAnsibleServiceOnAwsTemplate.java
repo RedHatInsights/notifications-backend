b1f2ebac74d376b16746451dbb6dbe447cd238e1
Zack Kayyali <zkayyali@redhat.com>
2024-07-03 11:45:52 -0400

    Add SaaS email notification (#2794)
    * WIP - Format AaaS email
    
    * Wording updates
    
    * Update notifyCustomerProvisionSuccessEmailBodyV2.html
    
    * Include env URL and remove button
    
    Fix email title as well

--- engine/src/test/java/com/redhat/cloud/notifications/templates/TestAnsibleServiceOnAwsTemplate.java
+++ engine/src/test/java/com/redhat/cloud/notifications/templates/TestAnsibleServiceOnAwsTemplate.java
@@ -34,19 +34,21 @@ public class TestAnsibleServiceOnAwsTemplate extends EmailTemplatesInDbHelper {
 
     @Test
     public void testInstantEmailTitle() {
-        Action action = TestHelpers.createAnsibleAction(NOTIFY_CUSTOMER_PROVISION_SUCCESS, null);
+        Action action = TestHelpers.createAnsibleAction(NOTIFY_CUSTOMER_PROVISION_SUCCESS, null, null);
 
         String result = generateEmailSubject(NOTIFY_CUSTOMER_PROVISION_SUCCESS, action);
-        assertEquals("Instant notification - Notify customer environment is ready - Ansible Automation Platform Service on AWS - Ansible Automation Platform", result);
+        assertEquals("Environment is ready - Ansible Automation Platform Service on AWS", result);
     }
 
     @Test
     public void testInstantEmailBody() {
-        String envName  = RandomStringUtils.randomAlphanumeric(10);
-        Action action = TestHelpers.createAnsibleAction(NOTIFY_CUSTOMER_PROVISION_SUCCESS, envName);
+        String envName  = "cus-" + RandomStringUtils.randomAlphanumeric(5).toLowerCase();
+        String bitwardenURL = "https://www.example.com";
+        Action action = TestHelpers.createAnsibleAction(NOTIFY_CUSTOMER_PROVISION_SUCCESS, envName, bitwardenURL);
 
         String result = generateEmailBody(NOTIFY_CUSTOMER_PROVISION_SUCCESS, action);
         assertTrue(result.contains(envName));
+        assertTrue(result.contains(bitwardenURL));
         assertTrue(result.contains(TestHelpers.HCC_LOGO_TARGET));
     }
 }
