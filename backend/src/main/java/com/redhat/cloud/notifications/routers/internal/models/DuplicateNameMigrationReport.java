90fa500c8d1905a276cbee6bce15428893b07d3e
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2024-07-05 17:03:42 +0200

    feature: remove duplicate name migration code
    Once the migration has run in production we have to remove the code
    which is related to it so that we can merge the database constraints.
    If we try to merge the database constraints first the tests for the
    migration will rightfully fail.
    
    RHCLOUD-33636

--- backend/src/main/java/com/redhat/cloud/notifications/routers/internal/models/DuplicateNameMigrationReport.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.cloud.notifications.routers.internal.models;
-
-public class DuplicateNameMigrationReport {
-    public int updatedIntegrations = 0;
-    public int updatedBehaviorGroups = 0;
-}
