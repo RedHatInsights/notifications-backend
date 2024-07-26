cfd61938e381a61d32af04833431c9f5e12527ac
Cameron Rodriguez <carodrig@redhat.com>
2024-07-26 13:41:22 -0400

    [RHCLOUD-33769] update PD data extractor

--- connector-pagerduty/src/main/java/com/redhat/cloud/notifications/connector/pagerduty/ExchangeProperty.java
+++ connector-pagerduty/src/main/java/com/redhat/cloud/notifications/connector/pagerduty/ExchangeProperty.java
@@ -2,7 +2,6 @@ package com.redhat.cloud.notifications.connector.pagerduty;
 
 // TODO: rewrite for PagerDuty
 public class ExchangeProperty {
-
     public static final String ACCOUNT_ID = "accountId";
     public static final String TARGET_URL_NO_SCHEME = "targetUrlNoScheme";
     public static final String TRUST_ALL = "trustAll";
--- connector-pagerduty/src/main/java/com/redhat/cloud/notifications/connector/pagerduty/PagerDutyCloudEventDataExtractor.java
+++ connector-pagerduty/src/main/java/com/redhat/cloud/notifications/connector/pagerduty/PagerDutyCloudEventDataExtractor.java
@@ -6,48 +6,80 @@ import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
+import org.apache.camel.http.common.HttpMethods;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.http.ProtocolException;
+
+import java.util.MissingResourceException;
 
 import static com.redhat.cloud.notifications.connector.ExchangeProperty.TARGET_URL;
 import static com.redhat.cloud.notifications.connector.pagerduty.ExchangeProperty.ACCOUNT_ID;
 import static com.redhat.cloud.notifications.connector.pagerduty.ExchangeProperty.TARGET_URL_NO_SCHEME;
-import static com.redhat.cloud.notifications.connector.pagerduty.ExchangeProperty.TRUST_ALL;
 import static org.apache.commons.validator.routines.UrlValidator.ALLOW_LOCAL_URLS;
 
-// TODO: rewrite for PagerDuty
 @ApplicationScoped
 public class PagerDutyCloudEventDataExtractor extends CloudEventDataExtractor {
 
     public static final String NOTIF_METADATA = "notif-metadata";
+    public static final String AUTHENTICATION = "authentication";
+    public static final String METHOD = "method";
+    public static final String TRUST_ALL = "trustAll";
+    public static final String URL = "url";
 
-    private static final UrlValidator HTTP_URL_VALIDATOR = new UrlValidator(new String[] {"http"}, ALLOW_LOCAL_URLS);
-    private static final UrlValidator HTTPS_URL_VALIDATOR = new UrlValidator(new String[] {"https"}, ALLOW_LOCAL_URLS);
+    public static final String PAYLOAD = "payload";
+    public static final String CUSTOM_DETAILS = "custom_details";
+    public static final String EVENT_ACTION = "event_action";
+
+    private static final UrlValidator HTTPS_URL_VALIDATOR = new UrlValidator(new String[]{"https"}, ALLOW_LOCAL_URLS);
+    private static final UrlValidator HTTP_URL_VALIDATOR = new UrlValidator(new String[]{"http"}, ALLOW_LOCAL_URLS);
 
     @Inject
     AuthenticationDataExtractor authenticationDataExtractor;
 
     @Override
-    public void extract(Exchange exchange, JsonObject cloudEventData) throws Exception {
+    public void extract(Exchange exchange, JsonObject cloudEventData) throws IllegalArgumentException, ProtocolException {
 
-        exchange.setProperty(ACCOUNT_ID, cloudEventData.getString("account_id"));
+        validatePayload(cloudEventData);
+
+        JsonObject customDetails = cloudEventData.getJsonObject(PAYLOAD).getJsonObject(CUSTOM_DETAILS);
+        exchange.setProperty(ACCOUNT_ID, customDetails.getString("account_id"));
 
         JsonObject metadata = cloudEventData.getJsonObject(NOTIF_METADATA);
-        exchange.setProperty(TARGET_URL, metadata.getString("url"));
+        exchange.setProperty(TARGET_URL, metadata.getString(URL));
+        validateTargetUrl(exchange);
+        exchange.setProperty(TARGET_URL_NO_SCHEME, exchange.getProperty(TARGET_URL, String.class).replace("https://", ""));
         exchange.setProperty(TRUST_ALL, Boolean.valueOf(metadata.getString("trustAll")));
 
-        JsonObject authentication = metadata.getJsonObject("authentication");
+        JsonObject authentication = metadata.getJsonObject(AUTHENTICATION);
         authenticationDataExtractor.extract(exchange, authentication);
 
         cloudEventData.remove(NOTIF_METADATA);
 
-        validateTargetUrl(exchange);
-        exchange.setProperty(TARGET_URL_NO_SCHEME, exchange.getProperty(TARGET_URL, String.class).replace("https://", ""));
-
         exchange.getIn().setBody(cloudEventData.encode());
+        exchange.getIn().setHeader(Exchange.HTTP_METHOD, HttpMethods.valueOf(metadata.getString(METHOD)));
     }
 
-    private void validateTargetUrl(Exchange exchange) throws Exception {
+    private void validatePayload(JsonObject cloudEventData) {
+        if (cloudEventData.getJsonObject(PAYLOAD) == null) {
+            throw new MissingResourceException("The '" + PAYLOAD + "' field is required", PagerDutyCloudEventDataExtractor.class.getName(), PAYLOAD);
+        } else {
+            if (cloudEventData.getJsonObject(PAYLOAD).getString("summary") == null) {
+                throw new MissingResourceException("The alert summary field is required", PagerDutyCloudEventDataExtractor.class.getName(), PAYLOAD + ".summary");
+            }
+            if (cloudEventData.getJsonObject(PAYLOAD).getString("severity") == null) {
+                throw new MissingResourceException("The alert severity field is required", PagerDutyCloudEventDataExtractor.class.getName(), PAYLOAD + ".severity");
+            }
+            if (cloudEventData.getJsonObject(PAYLOAD).getString("source") == null) {
+                throw new MissingResourceException("The alert source field is required", PagerDutyCloudEventDataExtractor.class.getName(), PAYLOAD + ".source");
+            }
+        }
+
+        if (cloudEventData.getString(EVENT_ACTION) == null) {
+            throw new MissingResourceException("The '" + EVENT_ACTION + "' field is required", PagerDutyCloudEventDataExtractor.class.getName(), EVENT_ACTION);
+        }
+    }
+
+    private void validateTargetUrl(Exchange exchange) throws IllegalArgumentException, ProtocolException {
         String targetUrl = exchange.getProperty(TARGET_URL, String.class);
         if (HTTP_URL_VALIDATOR.isValid(targetUrl)) {
             throw new ProtocolException("HTTP protocol is not supported");
