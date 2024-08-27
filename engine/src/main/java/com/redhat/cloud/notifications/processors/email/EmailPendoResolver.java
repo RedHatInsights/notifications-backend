d292a2326b41ea1459c2249489aa6d3708a18bb6
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2024-08-27 11:02:57 +0200

    Update engine/src/main/java/com/redhat/cloud/notifications/processors/email/EmailPendoResolver.java
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- engine/src/main/java/com/redhat/cloud/notifications/processors/email/EmailPendoResolver.java
+++ engine/src/main/java/com/redhat/cloud/notifications/processors/email/EmailPendoResolver.java
@@ -12,7 +12,7 @@ public class EmailPendoResolver {
     public static String GENERAL_PENDO_MESSAGE = "You can also receive notifications on tools like Slack, Microsoft Teams, Google chat, and more! Go to <b><a href=\"%s/settings/integrations\" target=\"_blank\">integrations</a></b> to set those up.";
     public static String GENERAL_PENDO_TITLE = "Did you know?";
 
-    public static final String OUTAGE_PENDO_MESSAGE = "Between Aug 22, 07:21 AM UTC and Aug 26, 12:47 PM UTC, the notifications email service experienced an outage. This email was delayed from that time period.";
+    public static final String OUTAGE_PENDO_MESSAGE = "Between Aug 22, 07:19 AM UTC and Aug 26, 12:47 PM UTC, the notifications email service experienced an outage. This email was delayed from that time period.";
     public static final String OUTAGE_PENDO_TITLE = "Outage notice";
 
     @Inject
