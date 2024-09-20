1776829ee996ef0807644b7a0070dffe94bb7af0
Guillaume Duval <gduval@redhat.com>
2024-09-20 16:40:18 +0200

    [RHCLOUD-34488] Update direct links between endpoints and event types trough behaviour group Apis

--- database/src/main/resources/db/migration/V1.103.0__RHCLOUD-34487_add_endpoint_event_type_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE endpoint_event_type (
-         event_type_id UUID NOT NULL,
-         endpoint_id UUID NOT NULL,
-         CONSTRAINT pk_endpoint_event_type PRIMARY KEY (event_type_id, endpoint_id),
-         CONSTRAINT fk_endpoint_event_type_event_type_id FOREIGN KEY (event_type_id) REFERENCES event_type (id) ON DELETE CASCADE,
-         CONSTRAINT fk_endpoint_event_type_endpoint_id FOREIGN KEY (endpoint_id) REFERENCES endpoints (id) ON DELETE CASCADE
-);
