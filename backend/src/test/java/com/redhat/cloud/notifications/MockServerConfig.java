a36a48fa1332ab7ef03c8a149fe47217bbac72cb
Mikel Alejo Barcina Ribera <mbarcina@redhat.com>
2024-10-02 18:23:35 +0200

    feature: fetch workspace IDs from RBAC
    In order to be able to check on users' permissions with Kessel, we need
    to know which workspace they are trying to act upon. And for now, RBAC
    is going to provide that information.
    
    RHCLOUD-35479

--- backend/src/test/java/com/redhat/cloud/notifications/MockServerConfig.java
+++ backend/src/test/java/com/redhat/cloud/notifications/MockServerConfig.java
@@ -1,14 +1,20 @@
 package com.redhat.cloud.notifications;
 
+import com.redhat.cloud.notifications.auth.rbac.workspace.WorkspaceUtils;
+import jakarta.ws.rs.core.MediaType;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
 import org.mockserver.model.ClearType;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.model.Parameter;
+import org.mockserver.verify.VerificationTimes;
 
 import java.io.InputStream;
 
 import static com.redhat.cloud.notifications.Constants.X_RH_IDENTITY_HEADER;
 import static com.redhat.cloud.notifications.MockServerLifecycleManager.getClient;
+import static com.redhat.cloud.notifications.TestConstants.DEFAULT_ORG_ID;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockserver.model.HttpRequest.request;
@@ -65,6 +71,101 @@ public class MockServerConfig {
             .withSecure(secure)
             .when(request)
             .respond(response);
+    }
+
+    /**
+     * Adds a path in the MockServer for the {@link com.redhat.cloud.notifications.auth.rbac.RbacServer#getWorkspaces(String, String, String, String, Integer, Integer)}
+     * method, which simulates RBAC returning more than one workspace in the
+     * response.
+     */
+    public static void addMultipleReturningMultipleWorkspacesRbacEndpoint() {
+        getClient()
+            .when(
+                request()
+                    .withHeader("x-rh-rbac-psk", "development-psk-value")
+                    .withHeader("x-rh-rbac-client-id", WorkspaceUtils.APPLICATION_KEY)
+                    .withHeader("x-rh-rbac-org-id", DEFAULT_ORG_ID)
+                    .withQueryStringParameter(Parameter.param("offset", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_OFFSET)))
+                    .withQueryStringParameter(Parameter.param("limit", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_LIMIT)))
+                    .withPath("/api/rbac/v2/workspaces/")
+            ).respond(
+                response()
+                    .withHeader("Content-Type", MediaType.APPLICATION_JSON)
+                    .withStatusCode(HttpStatus.SC_OK)
+                    .withBody(getFileAsString("rbac-examples/workspaces/multiple-workspaces-response.json"))
+            );
+    }
+
+    /**
+     * Adds a path in the MockServer for the {@link com.redhat.cloud.notifications.auth.rbac.RbacServer#getWorkspaces(String, String, String, String, Integer, Integer)}
+     * method, which simulates RBAC returning a default workspace in the
+     * response.
+     */
+    public static void addMultipleReturningSingleDefaultWorkspaceRbacEndpoint() {
+        getClient()
+            .when(
+                request()
+                    .withHeader("x-rh-rbac-psk", "development-psk-value")
+                    .withHeader("x-rh-rbac-client-id", WorkspaceUtils.APPLICATION_KEY)
+                    .withHeader("x-rh-rbac-org-id", DEFAULT_ORG_ID)
+                    .withQueryStringParameter(Parameter.param("offset", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_OFFSET)))
+                    .withQueryStringParameter(Parameter.param("limit", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_LIMIT)))
+                    .withPath("/api/rbac/v2/workspaces/")
+            ).respond(
+                response()
+                    .withHeader("Content-Type", MediaType.APPLICATION_JSON)
+                    .withStatusCode(HttpStatus.SC_OK)
+                    .withBody(getFileAsString("rbac-examples/workspaces/single-default-workspace-response.json"))
+            );
+    }
+
+    /**
+     * Adds a path in the MockServer for the {@link com.redhat.cloud.notifications.auth.rbac.RbacServer#getWorkspaces(String, String, String, String, Integer, Integer)}
+     * method, which simulates RBAC returning a "root" workspace instead of the
+     * expected "default" workspace.
+     */
+    public static void addReturningSingleRootWorkspaceRbacEndpoint() {
+        getClient()
+            .when(
+                request()
+                    .withHeader("x-rh-rbac-psk", "development-psk-value")
+                    .withHeader("x-rh-rbac-client-id", WorkspaceUtils.APPLICATION_KEY)
+                    .withHeader("x-rh-rbac-org-id", DEFAULT_ORG_ID)
+                    .withQueryStringParameter(Parameter.param("offset", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_OFFSET)))
+                    .withQueryStringParameter(Parameter.param("limit", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_LIMIT)))
+                    .withPath("/api/rbac/v2/workspaces/")
+            ).respond(
+                response()
+                    .withHeader("Content-Type", MediaType.APPLICATION_JSON)
+                    .withStatusCode(HttpStatus.SC_OK)
+                    .withBody(getFileAsString("rbac-examples/workspaces/single-root-workspace-response.json"))
+            );
+    }
+
+    /**
+     * Verifies that the default workspace for an organization has been only
+     * fetched once from RBAC, since Notifications should have cached it.
+     */
+    public static void verifyDefaultWorkspaceFetchedOnlyOnce() {
+        getClient()
+            .verify(
+                request()
+                    .withHeader("x-rh-rbac-psk", "development-psk-value")
+                    .withHeader("x-rh-rbac-client-id", WorkspaceUtils.APPLICATION_KEY)
+                    .withHeader("x-rh-rbac-org-id", DEFAULT_ORG_ID)
+                    .withQueryStringParameter(Parameter.param("offset", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_OFFSET)))
+                    .withQueryStringParameter(Parameter.param("limit", String.valueOf(WorkspaceUtils.REQUEST_DEFAULT_LIMIT)))
+                    .withPath("/api/rbac/v2/workspaces/"),
+                VerificationTimes.once()
+            );
+    }
+
+    public static void clearRbacWorkspaces() {
+        getClient()
+            .clear(
+                request()
+                .withPath("/api/rbac/v2/workspaces/")
+            );
     }
 
     public static void clearRbac() {
---
+++ backend/src/test/java/com/redhat/cloud/notifications/auth/rbac/workspace/WorkspaceUtilsTest.java
@@ -0,0 +1,70 @@
+package com.redhat.cloud.notifications.auth.rbac.workspace;
+
+import com.redhat.cloud.notifications.MockServerConfig;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static com.redhat.cloud.notifications.TestConstants.DEFAULT_ORG_ID;
+
+@QuarkusTest
+public class WorkspaceUtilsTest {
+    @Inject
+    WorkspaceUtils workspaceUtils;
+
+    /**
+     * Make sure we clear the workspace path from the MockServer even if tests
+     * fail.
+     */
+    @AfterAll
+    public static void clearRbacWorkspaceRoutes() {
+        MockServerConfig.clearRbacWorkspaces();
+    }
+
+    /**
+     * Tests that a default workspace is fetched from RBAC and then cached for
+     * a given organization.
+     */
+    @Test
+    void testFetchDefaultWorkspace() {
+        MockServerConfig.addMultipleReturningSingleDefaultWorkspaceRbacEndpoint();
+
+        for (int i = 0; i < 5; i++) {
+            final UUID workspaceId = this.workspaceUtils.getDefaultWorkspaceId(DEFAULT_ORG_ID);
+            Assertions.assertNotNull(workspaceId, "a workspace ID should have been returned from the function under test");
+        }
+
+        MockServerConfig.verifyDefaultWorkspaceFetchedOnlyOnce();
+        MockServerConfig.clearRbacWorkspaces();
+    }
+
+    /**
+     * Tests that when more than one workspace is received, or the single
+     * received workspace is not the default one, an exception is raised.
+     */
+    @Test
+    void testInvalidNonDefaultWorkspacesReturnedThrowsException() {
+        MockServerConfig.addMultipleReturningMultipleWorkspacesRbacEndpoint();
+
+        final IllegalStateException multipleWorkspacesException = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.workspaceUtils.getDefaultWorkspaceId(DEFAULT_ORG_ID)
+        );
+        Assertions.assertEquals(WorkspaceUtils.ERROR_USER_FACING, multipleWorkspacesException.getMessage(), "unexpected user facing error in the received exception");
+
+        MockServerConfig.clearRbacWorkspaces();
+        MockServerConfig.addReturningSingleRootWorkspaceRbacEndpoint();
+
+        final IllegalStateException nonDefaultWorkspaceException = Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> this.workspaceUtils.getDefaultWorkspaceId(DEFAULT_ORG_ID)
+        );
+        Assertions.assertEquals(WorkspaceUtils.ERROR_USER_FACING, nonDefaultWorkspaceException.getMessage(), "unexpected user facing error in the received exception");
+
+        MockServerConfig.clearRbacWorkspaces();
+    }
+}
