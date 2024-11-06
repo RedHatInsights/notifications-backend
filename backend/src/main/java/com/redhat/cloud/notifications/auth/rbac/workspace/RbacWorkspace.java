a0980e210d68986b216308fc828878a8790a0bf9
Guillaume Duval <gduval@redhat.com>
2024-11-06 13:58:37 +0100

    Update rbac workspace uuid to id

--- backend/src/main/java/com/redhat/cloud/notifications/auth/rbac/workspace/RbacWorkspace.java
+++ backend/src/main/java/com/redhat/cloud/notifications/auth/rbac/workspace/RbacWorkspace.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 /**
  * Represents the Kessel's Workspace entity as defined by RBAC.
- * @param uuid the identifier of the workspace.
+ * @param id the identifier of the workspace.
  * @param parentId the identifier of the parent workspace.
  * @param workspaceType the type of the workspace.
  * @param name the name of the workspace.
@@ -16,7 +16,7 @@ import java.util.UUID;
  * @param modified the date when the workspace was modified.
  */
 public record RbacWorkspace(
-    @JsonProperty("uuid") UUID uuid,
+    @JsonProperty("id") UUID id,
     @JsonProperty("parent_id") UUID parentId,
     @JsonProperty("type") WorkspaceType workspaceType,
     @JsonProperty("name") String name,
