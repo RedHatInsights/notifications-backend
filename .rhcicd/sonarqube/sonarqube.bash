d6e0c4a2482a975d99e948fd39429952accbfc46
Guillaume Duval <gduval@redhat.com>
2024-11-13 21:34:11 +0100

    Add new sonar certificate

--- .rhcicd/sonarqube/sonarqube.bash
+++ .rhcicd/sonarqube/sonarqube.bash
@@ -21,6 +21,7 @@ docker build \
   --build-arg cacerts_keystore_password="${CACERTS_KEYSTORE_PASSWORD}" \
   --build-arg rh_it_root_ca_cert_url="${RH_IT_ROOT_CA_CERT_URL}" \
   --build-arg rh_it_root_ca_cert_secondary_url="${RH_IT_ROOT_CA_CERT_SECONDARY_URL}" \
+  --build-arg rhcs-root-ca-cert-url="${RHCS_ROOT_CA_CERT_URL}" \
   --file .rhcicd/sonarqube/Dockerfile \
   --tag notifications-sonarqube:latest \
   .
@@ -35,6 +36,7 @@ if [ -n "${GIT_BRANCH:-}" ] && [ "${GIT_BRANCH}" == "origin/master" ]; then
     --env GIT_BRANCH="${GIT_BRANCH}" \
     --env RH_IT_ROOT_CA_CERT_URL="${RH_IT_ROOT_CA_CERT_URL}" \
     --env RH_IT_ROOT_CA_CERT_SECONDARY_URL="${RH_IT_ROOT_CA_CERT_SECONDARY_URL}" \
+    --env RHCS_ROOT_CA_CERT_URL="${RHCS_ROOT_CA_CERT_URL}" \
     --env SONARQUBE_HOST_URL="${SONARQUBE_HOST_URL}" \
     --env SONARQUBE_TOKEN="${SONARQUBE_TOKEN}" \
     --rm \
@@ -47,6 +49,7 @@ else
     --env GITHUB_PULL_REQUEST_ID="${ghprbPullId}" \
     --env RH_IT_ROOT_CA_CERT_URL="${RH_IT_ROOT_CA_CERT_URL}" \
     --env RH_IT_ROOT_CA_CERT_SECONDARY_URL="${RH_IT_ROOT_CA_CERT_SECONDARY_URL}" \
+    --env RHCS_ROOT_CA_CERT_URL="${RHCS_ROOT_CA_CERT_URL}" \
     --env SONARQUBE_HOST_URL="${SONARQUBE_HOST_URL}" \
     --env SONARQUBE_TOKEN="${SONARQUBE_TOKEN}" \
     --rm \
