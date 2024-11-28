27ee0f76a6a541ac3dcc8f771ce70a4c77f9a9f1
Guillaume Duval <gduval@redhat.com>
2024-11-28 10:33:46 +0100

    Fix status Api

--- backend/src/main/java/com/redhat/cloud/notifications/routers/StatusResource.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/StatusResource.java
@@ -1,10 +1,10 @@
 package com.redhat.cloud.notifications.routers;
 
 import com.redhat.cloud.notifications.oapi.OApiFilter;
+import io.vertx.core.json.JsonObject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import static com.redhat.cloud.notifications.Constants.API_NOTIFICATIONS_V_1_0;
@@ -12,6 +12,8 @@ import static com.redhat.cloud.notifications.Constants.API_NOTIFICATIONS_V_2_0;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class StatusResource {
+
+    static final JsonObject STATUS = new JsonObject("{\"status\":\"UP\"}");
 
     @Path(API_NOTIFICATIONS_V_1_0 + "/status")
     public static class V1 extends StatusResource {
@@ -24,7 +26,7 @@ public class StatusResource {
     @GET
     @Produces(APPLICATION_JSON)
     @Tag(name = OApiFilter.PRIVATE)
-    public Response getCurrentStatus() {
-        return Response.ok().build();
+    public JsonObject getCurrentStatus() {
+        return STATUS;
     }
 }
