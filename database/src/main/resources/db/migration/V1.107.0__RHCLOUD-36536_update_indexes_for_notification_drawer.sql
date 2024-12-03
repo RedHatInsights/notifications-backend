1f9bf2e8b28dfbf7c908086e8fe2244cdbbedb54
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2024-12-03 17:04:19 +0100

    Increase backend health timeout + drop index before creating a new one to save disc space (#3163)

--- database/src/main/resources/db/migration/V1.107.0__RHCLOUD-36536_update_indexes_for_notification_drawer.sql
+++ database/src/main/resources/db/migration/V1.107.0__RHCLOUD-36536_update_indexes_for_notification_drawer.sql
@@ -1,2 +1,2 @@
-CREATE INDEX ix_drawer_notification_org_id_user_id_read_created_event_id ON drawer_notification (org_id, user_id, read, created DESC, event_id);
 DROP INDEX ix_drawer_notification_org_id_user_id_created_read_event_id;
+CREATE INDEX ix_drawer_notification_org_id_user_id_read_created_event_id ON drawer_notification (org_id, user_id, read, created DESC, event_id);
