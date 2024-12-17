7cf808e3f145ff9ce773b8e518fa2f5edd9547ad
Guillaume Duval <gduval@redhat.com>
2024-12-17 14:09:38 +0100

    Update recipients-resolver config for Kessel relations Api

--- recipients-resolver/src/main/java/com/redhat/cloud/notifications/recipients/config/RecipientsResolverConfig.java
+++ recipients-resolver/src/main/java/com/redhat/cloud/notifications/recipients/config/RecipientsResolverConfig.java
@@ -10,6 +10,7 @@ import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.Startup;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.project_kessel.clients.authn.AuthenticationConfig;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -36,6 +37,10 @@ public class RecipientsResolverConfig {
     private static final String MBOP_ENV = "notifications.recipients-resolver.mbop.env";
     private static final String KESSEL_TARGET_URL = "notifications.recipients-resolver.kessel.target-url";
     private static final String KESSEL_USE_SECURE_CLIENT = "relations-api.is-secure-clients";
+    private static final String KESSEL_CLIENT_ID = "relations-api.authn.client.id";
+    private static final String KESSEL_CLIENT_SECRET = "relations-api.authn.client.secret";
+    private static final String KESSEL_CLIENT_ISSUER = "relations-api.authn.client.issuer";
+    private static final String KESSEL_CLIENT_MODE = "relations-api.authn.mode";
 
     /*
      * Unleash configuration
@@ -82,6 +87,18 @@ public class RecipientsResolverConfig {
 
     @ConfigProperty(name = KESSEL_TARGET_URL, defaultValue = "localhost:9000")
     String kesselTargetUrl;
+
+    @ConfigProperty(name = KESSEL_CLIENT_ID)
+    Optional<String> kesselClientId;
+
+    @ConfigProperty(name = KESSEL_CLIENT_SECRET)
+    Optional<String> kesselClientSecret;
+
+    @ConfigProperty(name = KESSEL_CLIENT_ISSUER)
+    Optional<String> kesselClientIssuer;
+
+    @ConfigProperty(name = KESSEL_CLIENT_MODE)
+    AuthenticationConfig.AuthMode kesselClientMode;
 
     /**
      * Is the gRPC client supposed to connect to a secure, HTTPS endpoint?
@@ -210,5 +227,21 @@ public class RecipientsResolverConfig {
 
     public Optional<String> getQuarkusItServicePassword() {
         return quarkusItServicePassword;
+    }
+
+    public Optional<String> getKesselClientId() {
+        return kesselClientId;
+    }
+
+    public Optional<String> getKesselClientSecret() {
+        return kesselClientSecret;
+    }
+
+    public Optional<String> getKesselClientIssuer() {
+        return kesselClientIssuer;
+    }
+
+    public AuthenticationConfig.AuthMode getKesselClientMode() {
+        return kesselClientMode;
     }
 }
