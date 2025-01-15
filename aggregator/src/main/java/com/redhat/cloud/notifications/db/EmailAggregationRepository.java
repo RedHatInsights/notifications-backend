7d7a5478f975fbe7a99ef0db54e067d857f19aba
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2025-01-15 23:31:07 +0100

    [RHCLOUD-37252] Update new aggregation logic (#3233)
    Co-authored-by: Gwenneg Lepage <gwenneg@users.noreply.github.com>

--- aggregator/src/main/java/com/redhat/cloud/notifications/db/EmailAggregationRepository.java
+++ aggregator/src/main/java/com/redhat/cloud/notifications/db/EmailAggregationRepository.java
@@ -53,9 +53,19 @@ public class EmailAggregationRepository {
         LocalDateTime currentTimeTwoDaysAgo = now.minusDays(2);
         String query = "SELECT DISTINCT ev.orgId, ev.bundleId, ev.applicationId, acp.lastRun, bu.name, ap.name FROM Event ev " +
             "join Application ap on ev.applicationId = ap.id join Bundle bu on ev.bundleId = bu.id " +
-            "join AggregationOrgConfig acp on ev.orgId = acp.orgId " +
-            "WHERE EXISTS (SELECT 1 FROM EventTypeEmailSubscription es where ev.orgId = es.id.orgId and es.id.subscriptionType='DAILY' and es.subscribed = true) " +
+            "join AggregationOrgConfig acp on ev.orgId = acp.orgId WHERE " +
+
+            // check than a aggregation template exists for the event application
+            "EXISTS (SELECT 1 FROM AggregationEmailTemplate WHERE application.id = ev.applicationId and subscriptionType = 'DAILY') " +
+            // After prod validation phase, the previous AggregationEmailTemplate check should be remove in favor of:
+            // check than at least one user of the org subscribed for daily digest with this event type
+            //"EXISTS (SELECT 1 FROM EventTypeEmailSubscription es where ev.orgId = es.id.orgId and es.id.subscriptionType='DAILY' and es.eventType = ev.eventType and es.subscribed is true) " + warning: need an new index on EventTypeEmailSubscription before being enabled
+
+            // check for linked email integration linked to this event type (to honor legacy mechanism)
+            "AND EXISTS (SELECT 1 FROM Endpoint ep, EndpointEventType eet where ev.orgId = ep.orgId and ep.compositeType.type = 'EMAIL_SUBSCRIPTION' and eet.eventType = ev.eventType and eet.endpoint = ep) " +
+            // filter on new events since the latest run of this org aggregation, and not older than two days
             "AND ev.created > acp.lastRun AND ev.created > :twoDaysAgo AND ev.created <= :now " +
+            // filter on org scheduled execution time
             "AND :nowTime = acp.scheduledExecutionTime";
         Query hqlQuery = entityManager.createQuery(query)
             .setParameter("nowTime", now.toLocalTime())
--- aggregator/src/test/java/com/redhat/cloud/notifications/helpers/ResourceHelpers.java
+++ aggregator/src/test/java/com/redhat/cloud/notifications/helpers/ResourceHelpers.java
@@ -1,15 +1,28 @@
 package com.redhat.cloud.notifications.helpers;
 
+import com.redhat.cloud.notifications.models.AggregationEmailTemplate;
 import com.redhat.cloud.notifications.models.AggregationOrgConfig;
+import com.redhat.cloud.notifications.models.Application;
 import com.redhat.cloud.notifications.models.EmailAggregation;
 import com.redhat.cloud.notifications.models.EmailAggregationKey;
+import com.redhat.cloud.notifications.models.Endpoint;
+import com.redhat.cloud.notifications.models.EndpointType;
+import com.redhat.cloud.notifications.models.Event;
+import com.redhat.cloud.notifications.models.EventType;
+import com.redhat.cloud.notifications.models.SubscriptionType;
+import com.redhat.cloud.notifications.models.Template;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.transaction.Transactional;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 @ApplicationScoped
 public class ResourceHelpers extends com.redhat.cloud.notifications.models.ResourceHelpers {
@@ -74,5 +87,79 @@ public class ResourceHelpers extends com.redhat.cloud.notifications.models.Resou
                 .setParameter("applicationName", key.getApplication())
                 .setParameter("created", lastUsedTime)
                 .executeUpdate();
+    }
+
+    @Transactional
+    public Endpoint getOrCreateEmailEndpointAndLinkItToEventType(final String orgId, final EventType eventType) {
+        Endpoint emailEndpoint;
+        try {
+            final String query = "FROM Endpoint WHERE orgId = :orgId AND compositeType.type = :type";
+            emailEndpoint = entityManager.createQuery(query, Endpoint.class)
+                .setParameter("orgId", orgId)
+                .setParameter("type", EndpointType.EMAIL_SUBSCRIPTION)
+                .getSingleResult();
+        } catch (NoResultException e) {
+            emailEndpoint = new Endpoint();
+            emailEndpoint.setType(EndpointType.EMAIL_SUBSCRIPTION);
+            emailEndpoint.setOrgId(orgId);
+            emailEndpoint.setName(RandomStringUtils.secure().nextAlphabetic(10));
+            emailEndpoint.setDescription(RandomStringUtils.randomAlphabetic(10));
+            entityManager.persist(emailEndpoint);
+        }
+
+        Set<EventType> linkedEventTypes = emailEndpoint.getEventTypes();
+        if (linkedEventTypes == null) {
+            linkedEventTypes = new HashSet<>();
+        }
+        linkedEventTypes.add(eventType);
+        emailEndpoint.setEventTypes(linkedEventTypes);
+        return entityManager.merge(emailEndpoint);
+    }
+
+    @Transactional
+    public AggregationEmailTemplate getOrCreateAggregationTemplate(Application application) {
+        try {
+            String query = "FROM AggregationEmailTemplate WHERE application.id = :appId";
+            return entityManager.createQuery(query, AggregationEmailTemplate.class)
+                .setParameter("appId", application.getId())
+                .getSingleResult();
+        } catch (NoResultException e) {
+            Template emailTemplate = new Template();
+            emailTemplate.setName(RandomStringUtils.randomAlphabetic(10));
+            emailTemplate.setData(RandomStringUtils.randomAlphabetic(10));
+            emailTemplate.setDescription(RandomStringUtils.randomAlphabetic(10));
+            entityManager.persist(emailTemplate);
+
+            AggregationEmailTemplate aggregationEmailTemplate = new AggregationEmailTemplate();
+            aggregationEmailTemplate.setApplication(application);
+            aggregationEmailTemplate.setApplicationId(application.getId());
+            aggregationEmailTemplate.setSubscriptionType(SubscriptionType.DAILY);
+            aggregationEmailTemplate.setBodyTemplate(emailTemplate);
+            aggregationEmailTemplate.setBodyTemplateId(emailTemplate.getId());
+            aggregationEmailTemplate.setSubjectTemplate(emailTemplate);
+            aggregationEmailTemplate.setSubjectTemplateId(emailTemplate.getId());
+            aggregationEmailTemplate.setApplication(application);
+            entityManager.persist(aggregationEmailTemplate);
+            return aggregationEmailTemplate;
+        }
+    }
+
+    public Event addEventEmailAggregation(String orgId, String bundleName, String applicationName, LocalDateTime created, String eventPayload) {
+        Application application = findOrCreateApplication(bundleName, applicationName);
+        EventType eventType = findOrCreateEventType(application.getId(), "event_type_test");
+        findOrCreateEventTypeEmailSubscription(orgId, "obiwan", eventType, SubscriptionType.DAILY);
+
+        getOrCreateEmailEndpointAndLinkItToEventType(orgId, eventType);
+        getOrCreateAggregationTemplate(application);
+
+        Event event = new Event();
+        event.setId(UUID.randomUUID());
+        event.setOrgId(orgId);
+        eventType.setApplication(application);
+        event.setEventType(eventType);
+        event.setCreated(created);
+        event.setPayload(eventPayload);
+
+        return createEvent(event);
     }
 }
