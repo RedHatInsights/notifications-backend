0c74b43c54e7f702c11e2f7430f54b8ccb1f25a1
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2025-01-22 10:53:25 +0100

    RHCLOUD-37414 Pause Kafka messages consumption from Unleash (#3246)

---
+++ engine/src/main/java/com/redhat/cloud/notifications/config/KafkaChannelConfig.java
@@ -0,0 +1,15 @@
+package com.redhat.cloud.notifications.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(SnakeCaseStrategy.class)
+public class KafkaChannelConfig {
+
+    public String hostName;
+    public String channel;
+    public Boolean paused;
+}
