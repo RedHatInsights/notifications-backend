c2594e16479b0efda8fc02640b7b86c98a083c3d
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2025-03-24 12:15:17 +0100

    [RHCLOUD-38977] Fix maintenance mode (#3358)

--- backend/src/main/java/com/redhat/cloud/notifications/routers/handlers/status/StatusResource.java
+++ backend/src/main/java/com/redhat/cloud/notifications/routers/handlers/status/StatusResource.java
@@ -1,7 +1,9 @@
 package com.redhat.cloud.notifications.routers.handlers.status;
 
+import com.redhat.cloud.notifications.config.BackendConfig;
 import com.redhat.cloud.notifications.oapi.OApiFilter;
 import io.vertx.core.json.JsonObject;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -13,12 +15,19 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path(API_NOTIFICATIONS_V_1_0 + "/status")
 public class StatusResource {
 
-    static final JsonObject STATUS = new JsonObject("{\"status\":\"UP\"}");
+    static final JsonObject STATUS_UP = new JsonObject("{\"status\":\"UP\"}");
+    static final JsonObject STATUS_MAINTENANCE = new JsonObject("{\"status\":\"MAINTENANCE\"}");
+
+    @Inject
+    BackendConfig backendConfig;
 
     @GET
     @Produces(APPLICATION_JSON)
     @Tag(name = OApiFilter.PRIVATE)
     public JsonObject getCurrentStatus() {
-        return STATUS;
+        if (backendConfig.isMaintenanceModeEnabled("GET_status")) {
+            return STATUS_MAINTENANCE;
+        }
+        return STATUS_UP;
     }
 }
