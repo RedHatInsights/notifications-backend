339318766d7678df22f01f40acd8e043549cbeab
Guillaume Duval <gduval@redhat.com>
2025-04-04 15:19:55 +0200

    change folder paths

--- common-template/src/main/java/com/redhat/cloud/notifications/qute/templates/mapping/OpenShift.java
+++ common-template/src/main/java/com/redhat/cloud/notifications/qute/templates/mapping/OpenShift.java
@@ -10,22 +10,22 @@ public class OpenShift {
     static final String BUNDLE_NAME = "openshift";
 
     static final String ADVISOR_APP_NAME = "advisor";
-    static final String ADVISOR_FOLDER_NAME = "AdvisorOpenShift";
+    static final String ADVISOR_FOLDER_NAME = "AdvisorOpenShift/";
 
     static final String COST_MANAGEMENT_APP_NAME = "cost-management";
-    static final String COST_MANAGEMENT_FOLDER_NAME = "CostManagement";
+    static final String COST_MANAGEMENT_FOLDER_NAME = "CostManagement/";
 
     public static final Map<TemplateDefinition, String> templatesMap = Map.ofEntries(
         // Advisor
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, ADVISOR_APP_NAME, "new-recommendation"), ADVISOR_FOLDER_NAME + "/newRecommendationBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, ADVISOR_APP_NAME, "new-recommendation"), ADVISOR_FOLDER_NAME + "newRecommendationBody.md"),
 
         // Cost Management
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "missing-cost-model"), COST_MANAGEMENT_FOLDER_NAME + "/MissingCostModelBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-create"), COST_MANAGEMENT_FOLDER_NAME + "/CostModelCreateBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-update"), COST_MANAGEMENT_FOLDER_NAME + "/CostModelUpdateBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-remove"), COST_MANAGEMENT_FOLDER_NAME + "/CostModelRemoveBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-stale"), COST_MANAGEMENT_FOLDER_NAME + "/CmOperatorStaleBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-data-received"), COST_MANAGEMENT_FOLDER_NAME + "/CmOperatorDataReceivedBody.md"),
-        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-data-processed"), COST_MANAGEMENT_FOLDER_NAME + "/CmOperatorDataProcessedBody.md")
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "missing-cost-model"), COST_MANAGEMENT_FOLDER_NAME + "MissingCostModelBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-create"), COST_MANAGEMENT_FOLDER_NAME + "CostModelCreateBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-update"), COST_MANAGEMENT_FOLDER_NAME + "CostModelUpdateBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cost-model-remove"), COST_MANAGEMENT_FOLDER_NAME + "CostModelRemoveBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-stale"), COST_MANAGEMENT_FOLDER_NAME + "CmOperatorStaleBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-data-received"), COST_MANAGEMENT_FOLDER_NAME + "CmOperatorDataReceivedBody.md"),
+        entry(new TemplateDefinition(DRAWER, BUNDLE_NAME, COST_MANAGEMENT_APP_NAME, "cm-operator-data-processed"), COST_MANAGEMENT_FOLDER_NAME + "CmOperatorDataProcessedBody.md")
     );
 }
