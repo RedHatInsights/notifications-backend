15b9324730b8ed2a530d743dbe9347c7891f80c7
Jessica Rodriguez <jrodri@redhat.com>
2025-05-12 15:25:47 -0400

    [RHCLOUD-38780] Link to OCM cluster in application_url (#3411)
    Logic implemented in both common-template and InsightsUrlsBuilder

---
+++ common-template/src/test/java/ms_teams/TestOcmTemplate.java
@@ -0,0 +1,54 @@
+package ms_teams;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redhat.cloud.notifications.qute.templates.TemplateDefinition;
+import com.redhat.cloud.notifications.qute.templates.TemplateService;
+import helpers.OcmTestHelpers;
+import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.JsonObject;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.redhat.cloud.notifications.qute.templates.IntegrationType.MS_TEAMS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+class TestOcmTemplate {
+
+    private static final JsonObject MESSAGE = OcmTestHelpers.createOcmMessage("Atlantic", "OSDTrial", "<b>Altlantic</b> server is experiencing flooding issues", "Subject line!");
+
+    private static final String CLUSTER_MANAGER_DEFAULT_EVENT_URL = "http://localhost/openshift/details/s/2XqNHRdLNEAzshh7MkkOql6fx6I?from=notifications&integration=teams";
+
+    @Inject
+    TemplateService templateService;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    @Test
+    void testRenderedOcmTemplates() {
+        String result = renderTemplate(null, MESSAGE);
+        checkResult(null, result);
+    }
+
+    String renderTemplate(final String eventType, final JsonObject message) {
+        TemplateDefinition templateConfig = new TemplateDefinition(MS_TEAMS, "openshift", "cluster-manager", eventType);
+        Map<String, Object> messageAsMap;
+        try {
+            messageAsMap = objectMapper.readValue(message.encode(), Map.class);
+            return templateService.renderTemplate(templateConfig, messageAsMap);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("OCM notification data transformation failed", e);
+        }
+    }
+
+    private void checkResult(String eventType, String result) {
+        switch (eventType) {
+            case null -> assertEquals("{\"text\":\"1 event triggered from Cluster Manager - OpenShift. [Open Cluster Manager](" + CLUSTER_MANAGER_DEFAULT_EVENT_URL + ")\"}", result);
+            default -> throw new IllegalArgumentException(eventType + "is not a valid event type");
+        }
+    }
+}
--- engine/src/test/java/com/redhat/cloud/notifications/processors/camel/teams/TeamsProcessorTest.java
+++ engine/src/test/java/com/redhat/cloud/notifications/processors/camel/teams/TeamsProcessorTest.java
@@ -1,6 +1,15 @@
 package com.redhat.cloud.notifications.processors.camel.teams;
 
 import com.redhat.cloud.notifications.TestLifecycleManager;
+import com.redhat.cloud.notifications.events.EventWrapperAction;
+import com.redhat.cloud.notifications.ingress.Action;
+import com.redhat.cloud.notifications.ingress.Metadata;
+import com.redhat.cloud.notifications.ingress.Payload;
+import com.redhat.cloud.notifications.models.Application;
+import com.redhat.cloud.notifications.models.Bundle;
+import com.redhat.cloud.notifications.models.Endpoint;
+import com.redhat.cloud.notifications.models.Event;
+import com.redhat.cloud.notifications.models.EventType;
 import com.redhat.cloud.notifications.processors.camel.CamelProcessor;
 import com.redhat.cloud.notifications.processors.camel.CamelProcessorTest;
 import com.redhat.cloud.notifications.templates.models.EnvironmentTest;
@@ -10,6 +19,10 @@ import io.smallrye.reactive.messaging.ce.CloudEventMetadata;
 import io.vertx.core.json.JsonObject;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Message;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static com.redhat.cloud.notifications.TestConstants.DEFAULT_ORG_ID;
 import static com.redhat.cloud.notifications.events.EndpointProcessor.TEAMS_ENDPOINT_SUBTYPE;
@@ -27,6 +40,8 @@ public class TeamsProcessorTest extends CamelProcessorTest {
 
     private static final String TEAMS_EXPECTED_MSG_WITH_HOST_URL = "\"text\": \"[my-computer](" + CONTEXT_HOST_URL + "?from=notifications&integration=teams) " +
             "triggered 1 event from Policies - Red Hat Enterprise Linux. [Open Policies](" + EnvironmentTest.expectedTestEnvUrlValue + "/insights/policies?from=notifications&integration=teams)\"";
+
+    private static final String TEAMS_EXPECTED_OCM_MSG_WITH_SUBSCRIPTION_ID = "{\"text\":\"1 event triggered from Cluster Manager - OpenShift. [Open Cluster Manager](" + EnvironmentTest.expectedTestEnvUrlValue + "/openshift/details/s/64503ec1-a365-4a1b-8c8b-0a6c519ec5fb?from=notifications&integration=teams)\"}";
 
     @Inject
     TeamsProcessor teamsProcessor;
@@ -69,4 +84,68 @@ public class TeamsProcessorTest extends CamelProcessorTest {
         assertTrue(notification.getString("message").contains(getExpectedMessage(withHostUrl)));
     }
 
+
+    /**
+     * An additional test to validate that a {@code application_url} will use the provided {@code subscription_id}, if
+     * present.
+     */
+    @Test
+    void testProcessWithOCMPayload() {
+        // Build the required data.
+        final Event event = buildOCMEvent();
+        final Endpoint endpoint = this.buildEndpoint();
+
+        // Send the event.
+        getCamelProcessor().process(event, List.of(endpoint));
+
+        // Retrieve the message to be sent, and run basic validation.
+        await().until(() -> inMemorySink.received().size() == 1);
+        Message<JsonObject> message = inMemorySink.received().getFirst();
+
+        CloudEventMetadata cloudEventMetadata = message.getMetadata(CloudEventMetadata.class).get();
+        assertNotNull(cloudEventMetadata.getId());
+        assertEquals(getExpectedCloudEventType(), cloudEventMetadata.getType());
+
+        // Retrieve the OCM notification and validate that the correct URL is provided.
+        JsonObject notification = message.getPayload();
+        assertEquals(TEAMS_EXPECTED_OCM_MSG_WITH_SUBSCRIPTION_ID, notification.getString("message"));
+    }
+
+    private static Event buildOCMEvent() {
+        Event event = buildEvent(false);
+        Action action = ((EventWrapperAction) event.getEventWrapper()).getEvent();
+        action.setContext(null);
+        action.setBundle("openshift");
+        action.setApplication("cluster-manager");
+        action.setEventType("cluster-update");
+        action.setEvents(
+                List.of(
+                        new com.redhat.cloud.notifications.ingress.Event.EventBuilder()
+                                .withMetadata(new Metadata.MetadataBuilder().build())
+                                .withPayload(new Payload.PayloadBuilder()
+                                        .withAdditionalProperty(
+                                                "global_vars",
+                                                Map.of("subscription_id", "64503ec1-a365-4a1b-8c8b-0a6c519ec5fb"))
+                                        .withAdditionalProperty("foo", "bar")
+                                        .build()
+                                ).build()
+                )
+        );
+
+        event.setEventWrapper(new EventWrapperAction(action));
+        event.setBundleDisplayName("OpenShift");
+        event.setApplicationDisplayName("Cluster Manager");
+        Bundle bundle = new Bundle("openshift", "OpenShift");
+        Application application = new Application();
+        application.setBundle(bundle);
+        application.setName("cluster-manager");
+        application.setDisplayName("Cluster Manager");
+        EventType eventType = new EventType();
+        eventType.setApplication(application);
+        eventType.setName("cluster-update");
+        eventType.setDisplayName("Cluster Update");
+        event.setEventType(eventType);
+
+        return event;
+    }
 }
