5aeed225f67fbbc4c0b06c8f028083d1ae49be86
Guillaume Duval <gduval@redhat.com>
2025-06-18 14:20:47 +0200

    [RHCLOUD-40471] Fix openApi default value for Query.limit property

--- backend/src/main/java/com/redhat/cloud/notifications/db/Query.java
+++ backend/src/main/java/com/redhat/cloud/notifications/db/Query.java
@@ -19,7 +19,7 @@ public class Query {
 
     private static final Pattern SORT_BY_PATTERN = Pattern.compile("^[a-z0-9_-]+(:(asc|desc))?$", CASE_INSENSITIVE);
 
-    private static final int DEFAULT_RESULTS_PER_PAGE = 20;
+    public static final int DEFAULT_RESULTS_PER_PAGE = 20;
 
     @QueryParam("limit")
     @DefaultValue(DEFAULT_RESULTS_PER_PAGE + "")
