f288b91ad0a35da7196e716a37cf3e52731501e0
Jessica Rodriguez <jrodri@redhat.com>
2025-08-21 20:11:06 -0400

    Revert "[RHCLOUD-40134] Move email template tests (#3613)" (#3647)
    This reverts commit caf07f923fdf2dc5afc1b6a94cdd427d2cad9f87.

--- common-template/src/test/java/helpers/OcmTestHelpers.java
+++ common-template/src/test/java/helpers/OcmTestHelpers.java
@@ -1,6 +1,5 @@
 package helpers;
 
-import com.redhat.cloud.notifications.ingress.Action;
 import com.redhat.cloud.notifications.ingress.Context;
 import com.redhat.cloud.notifications.ingress.Event;
 import com.redhat.cloud.notifications.ingress.Metadata;
@@ -17,54 +16,6 @@ import java.util.Optional;
 import static helpers.TestHelpers.DEFAULT_ORG_ID;
 
 public class OcmTestHelpers {
-
-    public static Action createOcmAction(String clusterDisplayName, String subscriptionPlan, String logDescription, String subject) {
-        return createOcmAction(clusterDisplayName, subscriptionPlan, logDescription, subject, null, Optional.empty());
-    }
-
-    public static Action createOcmAction(String clusterDisplayName, String subscriptionPlan, String logDescription, String subject, String title, Optional<Map<String, Object>> specificGlobalVars) {
-        Action emailActionMessage = new Action();
-        emailActionMessage.setBundle("openshift");
-        emailActionMessage.setApplication("cluster-manager");
-        emailActionMessage.setTimestamp(LocalDateTime.now());
-        emailActionMessage.setEventType("testEmailSubscriptionInstant");
-
-        emailActionMessage.setContext(
-            new Context.ContextBuilder()
-                .withAdditionalProperty("system_check_in", "2021-07-13T15:22:42.199046")
-                .withAdditionalProperty("tags", List.of())
-                .build()
-        );
-        Map<String, Object> globalVars = new HashMap<>();
-        globalVars.put("cluster_display_name", clusterDisplayName);
-        globalVars.put("subscription_id", "2XqNHRdLNEAzshh7MkkOql6fx6I");
-        globalVars.put("subscription_plan", subscriptionPlan);
-        globalVars.put("log_description", logDescription);
-        globalVars.put("internal_cluster_id", "fekelklflef");
-
-        if (specificGlobalVars.isPresent()) {
-            globalVars.putAll(specificGlobalVars.get());
-        }
-
-        emailActionMessage.setEvents(List.of(
-            new Event.EventBuilder()
-                .withMetadata(new Metadata.MetadataBuilder().build())
-                .withPayload(
-                    new Payload.PayloadBuilder()
-                        .withAdditionalProperty("global_vars", globalVars)
-                        .withAdditionalProperty("subject", subject)
-                        .withAdditionalProperty("title", title)
-                        .build()
-                )
-                .build()
-        ));
-
-        emailActionMessage.setOrgId(DEFAULT_ORG_ID);
-
-        return emailActionMessage;
-    }
-
-
 
     public static JsonObject createOcmMessage(String clusterDisplayName, String subscriptionPlan, String logDescription, String subject) {
         return createOcmMessage(clusterDisplayName, subscriptionPlan, logDescription, subject, null, Optional.empty());
