82ea077b041faaa2150eb37135431f52cad8c713
Gwenneg Lepage <glepage@redhat.com>
2025-09-01 16:37:16 +0200

    Migrate to the new unleash-client-java API

--- common-unleash/src/main/java/com/redhat/cloud/notifications/unleash/ToggleChangedLogger.java
+++ common-unleash/src/main/java/com/redhat/cloud/notifications/unleash/ToggleChangedLogger.java
@@ -1,7 +1,6 @@
 package com.redhat.cloud.notifications.unleash;
 
-import io.getunleash.FeatureToggle;
-import io.getunleash.repository.ToggleCollection;
+import io.getunleash.FeatureDefinition;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
@@ -9,7 +8,9 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @ApplicationScoped
 public class ToggleChangedLogger {
@@ -22,14 +23,16 @@ public class ToggleChangedLogger {
 
     private final Map<String, Boolean> toggleValues = new HashMap<>();
 
-    void process(@Observes ToggleCollection toggleCollection) {
+    void process(@Observes List<FeatureDefinition> featureDefinitions) {
         if (enabled) {
             for (String toggleName : toggleRegistry.getLoggedToggles()) {
-                FeatureToggle toggle = toggleCollection.getToggle(toggleName);
-                if (toggle != null) {
-                    Boolean toggleEnabled = toggleValues.put(toggle.getName(), toggle.isEnabled());
-                    if (toggleEnabled == null || toggleEnabled != toggle.isEnabled()) {
-                        Log.infof("Feature toggle changed [name=%s, enabled=%s]", toggle.getName(), toggle.isEnabled());
+                Optional<FeatureDefinition> toggle = featureDefinitions.stream()
+                    .filter(featureDefinition -> toggleName.equals(featureDefinition.getName()))
+                    .findFirst();
+                if (toggle.isPresent()) {
+                    Boolean toggleEnabled = toggleValues.put(toggle.get().getName(), toggle.get().environmentEnabled());
+                    if (toggleEnabled == null || toggleEnabled != toggle.get().environmentEnabled()) {
+                        Log.infof("Feature toggle changed [name=%s, enabled=%s]", toggle.get().getName(), toggle.get().environmentEnabled());
                     }
                 } else {
                     toggleValues.remove(toggleName);
