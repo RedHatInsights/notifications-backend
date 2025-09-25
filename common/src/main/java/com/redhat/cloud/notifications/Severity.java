ab85d5d941822698452a775579509f7cd001f71f
Jessica Rodriguez <jrodri@redhat.com>
2025-09-25 14:57:50 -0400

    Add comments to Severity enum
    Sorting is important, and explain UNDEFINED value

--- common/src/main/java/com/redhat/cloud/notifications/Severity.java
+++ common/src/main/java/com/redhat/cloud/notifications/Severity.java
@@ -1,7 +1,11 @@
 package com.redhat.cloud.notifications;
 
 /**
- * Recognized levels for {@link com.redhat.cloud.notifications.ingress.Action#severity}
+ * Recognized levels for {@link com.redhat.cloud.notifications.ingress.Action#severity Action#severity}. Values must be kept in
+ * order of decreasing severity to be properly sorted.
+ *
+ * @see <a href="https://access.redhat.com/security/updates/classification">Red Hat severity ratings</a>
+ * @see <a href="https://www.patternfly.org/patterns/status-and-severity/#severity-icons">PatternFly severity icons</a>
  */
 public enum Severity {
     CRITICAL,
@@ -9,5 +13,7 @@ public enum Severity {
     MODERATE,
     LOW,
     NONE,
+
+    /** A severity level was not provided, or could not be parsed. */
     UNDEFINED
 }
