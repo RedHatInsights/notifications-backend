8ae8e9279880b3c5f8278e6f34badb581277c919
Guillaume Duval <gduval@redhat.com>
2025-10-12 21:39:25 +0200

    Refactor using CE mapping

--- connector-common-http-v2/src/main/java/com/redhat/cloud/notifications/connector/v2/http/HttpOutgoingCloudEventBuilder.java
+++ connector-common-http-v2/src/main/java/com/redhat/cloud/notifications/connector/v2/http/HttpOutgoingCloudEventBuilder.java
@@ -4,6 +4,7 @@ import com.redhat.cloud.notifications.connector.v2.MessageContext;
 import com.redhat.cloud.notifications.connector.v2.OutgoingCloudEventBuilder;
 import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Message;
 
 import static com.redhat.cloud.notifications.connector.v2.http.CommonHttpConstants.HTTP_ERROR_TYPE;
 import static com.redhat.cloud.notifications.connector.v2.http.CommonHttpConstants.HTTP_STATUS_CODE;
@@ -15,21 +16,24 @@ import static com.redhat.cloud.notifications.connector.v2.http.HttpErrorType.HTT
 public class HttpOutgoingCloudEventBuilder extends OutgoingCloudEventBuilder {
 
     @Override
-    public JsonObject build(MessageContext context) throws Exception {
-        JsonObject cloudEvent = super.build(context);
+    public Message<String> build(MessageContext context) throws Exception {
+        Message<String> cloudEventMessage = super.build(context);
 
         HttpErrorType httpErrorType = context.getProperty(HTTP_ERROR_TYPE, HttpErrorType.class);
         if (httpErrorType != null) {
-            JsonObject data = new JsonObject(cloudEvent.getString("data"));
+            // Extract the current payload and add error information
+            JsonObject data = new JsonObject(cloudEventMessage.getPayload());
             JsonObject error = new JsonObject();
             error.put("error_type", httpErrorType);
             if (httpErrorType == HTTP_3XX || httpErrorType == HTTP_4XX || httpErrorType == HTTP_5XX) {
                 error.put("http_status_code", context.getProperty(HTTP_STATUS_CODE));
             }
             data.put("error", error);
-            cloudEvent.put("data", data.encode());
+
+            // Create a new message with the updated payload, preserving the CloudEvent metadata
+            return cloudEventMessage.withPayload(data.encode());
         }
 
-        return cloudEvent;
+        return cloudEventMessage;
     }
 }
