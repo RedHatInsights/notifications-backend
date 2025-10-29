fd9020e54ac858e8fc37552cf5ef0f824dcea6b7
Guillaume Duval <gduval@redhat.com>
2025-10-29 13:02:42 +0100

    [RHCLOUD-41812] Migrate recipients-resolver module

--- recipients-resolver/src/test/java/com/redhat/cloud/notifications/recipients/resolver/rbac/OidcServerMockResource.java
+++ recipients-resolver/src/test/java/com/redhat/cloud/notifications/recipients/resolver/rbac/OidcServerMockResource.java
@@ -1,45 +1,39 @@
 package com.redhat.cloud.notifications.recipients.resolver.rbac;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.model.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockserver.integration.ClientAndServer.startClientAndServer;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 public class OidcServerMockResource implements QuarkusTestResourceLifecycleManager {
 
     public static final String TEST_ACCESS_TOKEN = "test-access-token-12345";
 
-    private static final String LOG_LEVEL_KEY = "mockserver.logLevel";
-    private static ClientAndServer clientAndServer;
+    private static WireMockServer wireMockServer;
 
     @Override
     public Map<String, String> start() {
 
-        setMockServerLogLevel();
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig()
+            .dynamicPort());
+        wireMockServer.start();
 
-        clientAndServer = startClientAndServer();
-        String serverUrl = "http://localhost:" + clientAndServer.getPort();
+        String serverUrl = "http://localhost:" + wireMockServer.port();
 
         setupMockExpectations(serverUrl);
 
         Map<String, String> config = new HashMap<>();
         config.put("quarkus.oidc-client.auth-server-url", serverUrl);
 
-        System.out.println("OIDC server mock started");
+        System.out.println("OIDC server mock started on port: " + wireMockServer.port());
 
         return config;
-    }
-
-    private static void setMockServerLogLevel() {
-        if (System.getProperty(LOG_LEVEL_KEY) == null) {
-            System.setProperty(LOG_LEVEL_KEY, "OFF");
-            System.out.println("MockServer log is disabled. Use '-D" + LOG_LEVEL_KEY + "=WARN|INFO|DEBUG|TRACE' to enable it.");
-        }
     }
 
     private static void setupMockExpectations(String serverUrl) {
@@ -47,46 +41,40 @@ public class OidcServerMockResource implements QuarkusTestResourceLifecycleManag
         // Mock OIDC server endpoints
 
         // Mock OIDC discovery endpoint
-        clientAndServer.when(
-            request()
-                .withMethod("GET")
-                .withPath("/.well-known/openid-configuration")
-        ).respond(
-            response()
-                .withStatusCode(200)
-                .withContentType(MediaType.APPLICATION_JSON)
-                .withBody(String.format("""
-                    {
-                      "issuer": "%s",
-                      "token_endpoint": "%s/token",
-                      "grant_types_supported": ["client_credentials"]
-                    }
-                    """, serverUrl, serverUrl))
+        wireMockServer.stubFor(
+            get(urlEqualTo("/.well-known/openid-configuration"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(String.format("""
+                        {
+                          "issuer": "%s",
+                          "token_endpoint": "%s/token",
+                          "grant_types_supported": ["client_credentials"]
+                        }
+                        """, serverUrl, serverUrl)))
         );
 
         // Mock OIDC token endpoint
-        clientAndServer.when(
-            request()
-                .withMethod("POST")
-                .withPath("/token")
-        ).respond(
-            response()
-                .withStatusCode(200)
-                .withContentType(MediaType.APPLICATION_JSON)
-                .withBody(String.format("""
-                    {
-                      "access_token": "%s",
-                      "token_type": "Bearer",
-                      "expires_in": 3600
-                    }
-                    """, TEST_ACCESS_TOKEN))
+        wireMockServer.stubFor(
+            post(urlEqualTo("/token"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(String.format("""
+                        {
+                          "access_token": "%s",
+                          "token_type": "Bearer",
+                          "expires_in": 3600
+                        }
+                        """, TEST_ACCESS_TOKEN)))
         );
     }
 
     @Override
     public void stop() {
-        if (clientAndServer != null) {
-            clientAndServer.stop();
+        if (wireMockServer != null) {
+            wireMockServer.stop();
             System.out.println("OIDC server mock stopped");
         }
     }
