a2ae3e3d7c834e89aea25b19e65b3138566dfb12
Guillaume Duval <gduval@redhat.com>
2025-10-29 19:24:21 +0100

    Use only an auto generated cert

--- connector-common-http/src/test/java/com/redhat/cloud/notifications/connector/http/TestHttpComponentConfigurator.java
+++ connector-common-http/src/test/java/com/redhat/cloud/notifications/connector/http/TestHttpComponentConfigurator.java
@@ -10,15 +10,16 @@ import org.apache.camel.support.jsse.SSLContextParameters;
 import org.apache.camel.support.jsse.TrustManagersParameters;
 
 import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 /**
  * Test-only override of HttpComponentConfigurator that configures HTTPS components
- * to trust all SSL certificates and skip hostname verification.
+ * to trust localhost self-signed certificates and our custom WireMock test certificate.
  * This is necessary for testing with WireMock which uses self-signed certificates.
  *
  * SECURITY NOTE: This class is only used during tests and will NOT be included in production builds.
- * The production HttpComponentConfigurator does NOT trust all certificates.
+ * Only trusts certificates issued for localhost/127.0.0.1/::1 or our custom certificate (CN=Notifications WireMock cert).
  */
 @Mock
 @Dependent
@@ -35,28 +36,44 @@ public class TestHttpComponentConfigurator extends HttpComponentConfigurator {
         // Additionally configure SSL for HTTPS components in tests
         for (String httpComponent : connectorConfig.getHttpComponents()) {
             if ("https".equals(httpComponent)) {
-                Log.infof("Test mode: Configuring HTTPS component to trust all certificates for WireMock testing");
+                Log.infof("Test mode: Configuring HTTPS component to trust localhost certificates for WireMock testing");
                 HttpComponent component = context.getComponent(httpComponent, HttpComponent.class);
-                configureTrustAllSSL(component);
+                configureLocalhostSSL(component);
             }
         }
     }
 
-    private void configureTrustAllSSL(HttpComponent component) {
+    private void configureLocalhostSSL(HttpComponent component) {
         try {
             SSLContextParameters sslContextParameters = new SSLContextParameters();
             TrustManagersParameters trustManagersParameters = new TrustManagersParameters();
 
-            // Trust all certificates - ONLY for testing with WireMock self-signed certs
+            // Trust localhost self-signed certificates only - for testing with WireMock
             trustManagersParameters.setTrustManager(new X509TrustManager() {
                 @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType) {
-                    // Accept all client certificates in test mode
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    // We don't accept client certificates in test mock server
+                    throw new CertificateException("Client certificates not accepted in test mock server");
                 }
 
                 @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType) {
-                    // Accept all server certificates in test mode
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    if (chain == null || chain.length == 0) {
+                        throw new CertificateException("No certificates provided");
+                    }
+                    // Only accept certificates with localhost or our custom WireMock test certificate
+                    X509Certificate cert = chain[0];
+                    String dn = cert.getSubjectX500Principal().getName();
+
+                    // Check if it's a localhost certificate or our custom Notifications WireMock test certificate
+                    boolean isValidTestCert = dn.contains("CN=localhost") ||
+                                             dn.contains("CN=127.0.0.1") ||
+                                             dn.contains("CN=::1") ||
+                                             dn.contains("CN=Notifications WireMock cert");
+
+                    if (!isValidTestCert) {
+                        throw new CertificateException("Certificate not issued for localhost or Notifications WireMock. DN: " + dn);
+                    }
                 }
 
                 @Override
@@ -68,10 +85,14 @@ public class TestHttpComponentConfigurator extends HttpComponentConfigurator {
             sslContextParameters.setTrustManagers(trustManagersParameters);
             component.setSslContextParameters(sslContextParameters);
 
-            // Disable hostname verification for testing with self-signed certificates
-            component.setX509HostnameVerifier(org.apache.http.conn.ssl.NoopHostnameVerifier.INSTANCE);
+            // Only accept localhost hostnames for testing with self-signed certificates
+            component.setX509HostnameVerifier((hostname, session) ->
+                "localhost".equals(hostname) ||
+                "127.0.0.1".equals(hostname) ||
+                "::1".equals(hostname)
+            );
 
-            Log.debugf("Test mode: HTTPS component configured to trust all certificates");
+            Log.debugf("Test mode: HTTPS component configured to trust localhost certificates only");
         } catch (Exception e) {
             Log.error("Failed to configure HTTPS component SSL for testing: " + e.getMessage(), e);
             throw new RuntimeException("SSL configuration failed in test mode", e);
