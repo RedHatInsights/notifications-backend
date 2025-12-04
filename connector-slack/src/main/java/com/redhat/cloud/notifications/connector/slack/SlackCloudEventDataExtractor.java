7243b8921129914493c4ceaf092c8249a263e470
Guillaume Duval <gduval@redhat.com>
2025-12-04 15:01:32 +0100

    [RHCLOUD-43721] Add Unleash useBetaTemplate flag on chat connectors

--- connector-slack/src/main/java/com/redhat/cloud/notifications/connector/slack/SlackCloudEventDataExtractor.java
+++ connector-slack/src/main/java/com/redhat/cloud/notifications/connector/slack/SlackCloudEventDataExtractor.java
@@ -1,6 +1,7 @@
 package com.redhat.cloud.notifications.connector.slack;
 
 import com.redhat.cloud.notifications.connector.CloudEventDataExtractor;
+import com.redhat.cloud.notifications.connector.slack.config.SlackConnectorConfig;
 import com.redhat.cloud.notifications.qute.templates.IntegrationType;
 import com.redhat.cloud.notifications.qute.templates.TemplateDefinition;
 import com.redhat.cloud.notifications.qute.templates.TemplateService;
@@ -10,6 +11,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
 
+import static com.redhat.cloud.notifications.connector.ExchangeProperty.ORG_ID;
 import static com.redhat.cloud.notifications.connector.ExchangeProperty.TARGET_URL;
 
 @ApplicationScoped
@@ -18,16 +20,25 @@ public class SlackCloudEventDataExtractor extends CloudEventDataExtractor {
     @Inject
     TemplateService templateService;
 
+    @Inject
+    SlackConnectorConfig connectorConfig;
+
     @Override
     public void extract(Exchange exchange, JsonObject cloudEventData) {
         CamelNotification notification =  cloudEventData.mapTo(CamelNotification.class);
         exchange.setProperty(TARGET_URL, notification.webhookUrl);
 
+        final String bundle = notification.eventData.get("bundle").toString();
+        final String application = notification.eventData.get("application").toString();
+        final String eventType = notification.eventData.get("event_type").toString();
+        boolean useBetaTemplate = connectorConfig.isUseBetaTemplatesEnabled(exchange.getProperty(ORG_ID, String.class), bundle, application, eventType);
+
         TemplateDefinition templateDefinition = new TemplateDefinition(
             IntegrationType.SLACK,
-            notification.eventData.get("bundle").toString(),
-            notification.eventData.get("application").toString(),
-            notification.eventData.get("event_type").toString());
+            bundle,
+            application,
+            eventType,
+            useBetaTemplate);
         String sourceMessage = templateService.renderTemplate(templateDefinition, notification.eventData);
 
         JsonObject message = new JsonObject();
