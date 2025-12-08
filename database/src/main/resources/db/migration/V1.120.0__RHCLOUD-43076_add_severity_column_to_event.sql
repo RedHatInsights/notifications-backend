89cf6eaf472eea330aa214b037fbf52216a3e165
Dalia Cervantes <dcervant@redhat.com>
2025-12-08 10:05:14 -0600

    fixing db

--- database/src/main/resources/db/migration/V1.120.0__RHCLOUD-43076_add_severity_column_to_event.sql
+++ database/src/main/resources/db/migration/V1.120.0__RHCLOUD-43076_add_severity_column_to_event.sql
@@ -1,3 +1,3 @@
--- Create index for better query performance when filtering by severity
-CREATE INDEX idx_event_severity ON event(severity);
+-- Add severity column to event table
+ALTER TABLE event ADD COLUMN severity VARCHAR(20);
 
