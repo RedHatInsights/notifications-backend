e332a63df59ecc47d178005ee41018e139429047
Guillaume Duval <gduval@redhat.com>
2026-01-26 13:11:12 +0100

    [RHCLOUD-44508] Build install Mandrel from UBI9 + update log config

--- common-unleash/src/main/java/com/redhat/cloud/notifications/unleash/LogConfig.java
+++ common-unleash/src/main/java/com/redhat/cloud/notifications/unleash/LogConfig.java
@@ -2,11 +2,13 @@ package com.redhat.cloud.notifications.unleash;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(SnakeCaseStrategy.class)
+@RegisterForReflection
 public class LogConfig {
 
     public String hostName;
