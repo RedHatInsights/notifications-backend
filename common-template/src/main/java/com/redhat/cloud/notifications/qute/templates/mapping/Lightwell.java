c59d0ff0fa01f6e0540b9da232418f0c05730596
Guillaume Duval <gduval@redhat.com>
2026-08-06 16:22:13 +0200

    RHCLOUD-49576: Add Lightwell email title template and unit tests
    Registers the EMAIL_TITLE template for the Lightwell java_remediated
    event type and adds unit test coverage for it.

--- common-template/src/main/java/com/redhat/cloud/notifications/qute/templates/mapping/Lightwell.java
+++ common-template/src/main/java/com/redhat/cloud/notifications/qute/templates/mapping/Lightwell.java
@@ -5,6 +5,7 @@ import com.redhat.cloud.notifications.qute.templates.TemplateDefinition;
 import java.util.Map;
 
 import static com.redhat.cloud.notifications.qute.templates.IntegrationType.EMAIL_BODY;
+import static com.redhat.cloud.notifications.qute.templates.IntegrationType.EMAIL_TITLE;
 import static java.util.Map.entry;
 
 public class Lightwell {
@@ -17,6 +18,7 @@ public class Lightwell {
 
     public static final Map<TemplateDefinition, String> templatesMap = Map.ofEntries(
 
-        entry(new TemplateDefinition(EMAIL_BODY, BUNDLE_NAME, LIGHTWELL_APP_NAME, null), LIGHTWELL_FOLDER_NAME + "lightwellDefaultEmailBody.html")
+        entry(new TemplateDefinition(EMAIL_BODY, BUNDLE_NAME, LIGHTWELL_APP_NAME, null), LIGHTWELL_FOLDER_NAME + "lightwellDefaultEmailBody.html"),
+        entry(new TemplateDefinition(EMAIL_TITLE, BUNDLE_NAME, LIGHTWELL_APP_NAME, null), LIGHTWELL_FOLDER_NAME + "lightwellDefaultEmailTitle.txt")
     );
 }
